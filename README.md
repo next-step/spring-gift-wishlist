62390e6dbe0247be8081f94e162e0e025670cf48
user <cyg0828@gmail.com>
2025-06-30 22:14:35 +0200

    feat: 상품 관리 기능 코드 이전 및 리소스 정리

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spring-gift-wishlist
+step0: 상품 관리 기능 코드 옮기기
---
+++ src/main/java/gift/dto/ProductRequest.java
@@ -0,0 +1,52 @@
+package gift.dto;
+
+public class ProductRequest {
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+
+    public ProductRequest(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public ProductRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
