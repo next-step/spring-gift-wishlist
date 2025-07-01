39cfbc164c03c5c717d93ea8e99fd2536f1eaee3
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 23:02:18 +0900

    test: ProductViewControllerTest 클래스 생성 및 테스트 환경 설정
    - @SpringBootTest, @AutoConfigureMockMvc, @Transactional 적용
    - MockMvc를 이용한 컨트롤러 테스트 기반 구성 완료

---
+++ src/test/java/gift/ProductViewControllerTest.java
@@ -0,0 +1,17 @@
+package gift;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class ProductViewControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+}
