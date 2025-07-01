aaef70584268ff6dfa28e3d8f88fbcdc4d92f391
seongwon02 <seongwon2303@gmail.com>
2025-07-01 22:00:08 +0900

    feat: 기존 유효성 검사에서 상품 이름에 대한 유효성 검사 추가 (최대 길이,  특수문자 검사)

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -1,12 +1,13 @@
 package gift.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public class ProductRequestDto {
 
     @NotBlank(message = "상품명은 필수입니다.")
+    @Size(max = 15, message = "상품명은 공백 포함 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$",
+            message = "상품명으로 허용되지 않는 특수 문자가 존재합니다. 가능한 특수문자: (), [], +, -, &, /, _")
     private final String name;
 
     @NotNull(message = "가격은 필수입니다.")
