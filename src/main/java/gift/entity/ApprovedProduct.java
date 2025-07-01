619bea808579060096e1fc8cbd161d665e176051
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 18:49:58 +0900

    feat: ApprovedProduct 엔티티 클래스 생성

---
+++ src/main/java/gift/entity/ApprovedProduct.java
@@ -0,0 +1,18 @@
+package gift.entity;
+
+public class ApprovedProduct {
+
+    private Long id;
+    private String name;
+
+    public ApprovedProduct() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
