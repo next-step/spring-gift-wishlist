0c9825930db1ea575a001d3a2bb79ec917cae0c2
YoonGangHoon <yghun021007@naver.com>
2025-07-02 02:21:43 +0900

    feat: API 컨트롤러에서 유효성 검사 실패 시 JSON 형식 에러 반환

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package gift.exception;
 
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -18,5 +21,20 @@ public class GlobalExceptionHandler {
         model.put("errorMessage", "상품이 존재하지 않습니다.");
 
         return new ModelAndView("error/product-not-found", model);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.computeIfAbsent(fieldName, key -> new ArrayList<>()).add(errorMessage);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
