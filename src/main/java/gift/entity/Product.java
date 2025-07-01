51093385506035f580ad85f43cbe204655d8134a
sylimi2r2 <sylimi2r2@naver.com>
2025-07-01 16:22:48 +0900

    refactor: 1차 과제 코드 복사

---
+++ src/main/java/gift/entity/Product.java
@@ -0,0 +1,66 @@
+package gift.entity;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public static Product createWithId(Product productWithoutId, Long newId) {
+        return new Product(
+                newId,
+                productWithoutId.getName(),
+                productWithoutId.getPrice(),
+                productWithoutId.getImageUrl()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table products (
+     id bigint auto_increment primary key,
+     name varchar(255),
+     price int,
+     imageUrl varchar(512)
+);
