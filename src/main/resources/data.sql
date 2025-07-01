81dde170dedc9a73d0faa607dd84d4e40108381b
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 15:29:47 +0900

    chore: data.sql 초기 데이터 이미지 URL 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO products (name, price, image_url)
 VALUES
-    ('초콜릿', 1000, 'https://i.namu.wiki/i/p5SG0f_Sux-XgAa3uNUsJLG_VeWFakukCtXW4kAiWZoiTqjYq0xVDJGpWqga8_LNZGQoCLf-E-gCev-GLYdibGQgQFny_R44zfBbtwU52rpJ1d_sOkzZb8CaBO7DTi3mTrTkRW_SN7wBX9DlAa0zBg.webp'),
-    ('새우깡', 1500, 'https://i.namu.wiki/i/v12co4dPL1DydzL3X4YFYFVNLLYe07BO2zSofx2LmrUG7g1PNWaBOC5Gwa2ip1r6EUxYIYzRze-9qRZVTQeq0lVjv91X_cWPVPb0HXitAg7NoCHDqpvbPZUHE3WyyAE2uead00JqL5w6uIxQQYTDPA.webp');
+    ('초콜릿', 1000, 'https://i.namu.wiki/i/omn0E--U8ws6U4WXKk2-ITRzSE_YO4Qf0CXoxABZ3Eiwj4lUoqwK5PcTALo3D2o_QxHKl_Xmba9PTXS169yGqAsjTX3DEgCemLSv0aNLhx0HNWv9YLjUeVnUx3DtdzOEUkvB-9fhami0ttZkEeYcfw.webp'),
+    ('새우깡', 1500, 'https://i.namu.wiki/i/HgNlAcOFWmo5y9T8Jmp6QiMKBnoXn-wLdhZOdV7vwlNIPaQM3uEFLiKN9u36O2fbpyV_6Zw2xxhMzk5SwjReIYrRYz6KyOFjTVaEGWX2XwFjJSAVTooaJGlhvSfiv7CVZb69RwbqC_xWEOLs-uAREQ.webp');
