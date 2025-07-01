ae96048f469fdd7f032da4eb10669332f1b00ddb
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 16:30:08 +0900

    feat: ProductViewRequestDto의 상품명에 공백을 포함한 허용된 특수 문자 검증 기능 추가
    - 허용된 특수 문자 : ( ), [ ], +, -, &, /, _

--- src/main/java/gift/dto/view/ProductViewRequestDto.java
+++ src/main/java/gift/dto/view/ProductViewRequestDto.java
@@ -6,6 +6,10 @@ public class ProductViewRequestDto {
 
     @NotBlank(message = "상품명을 입력해주세요.")
     @Size(max = 15, message = "최대 15자까지 가능합니다.")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$",
+            message = "유효한 특수문자 ( '( )', '[ ]', '+', '-', '&', '/', '_' ) 가 아닙니다."
+    )
     private String name;
 
     @NotNull(message = "가격을 입력해주세요.")
