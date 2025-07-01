a22dfca1d07c52757674941d62e4b5a00c966e73
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-02 00:14:39 +0900

    Feat: kakaoMD 의 허락이 필요한 상황의 요구사항을 구별하기 위한 Column 추가 및 TABLE 구조 변경

--- src/main/java/gift/entity/Gift.java
+++ src/main/java/gift/entity/Gift.java
@@ -1,11 +1,21 @@
 package gift.entity;
 
+
+import jakarta.validation.constraints.Size;
+
 public class Gift {
     private Long id;
+
     private Long giftId;
+
+    @Size(min = 1, max = 15, message = "상품 명은 공백 포함 최대 15자까지 가능합니다.")
     private String giftName;
+
     private Integer giftPrice;
+
     private String giftPhotoUrl;
+
+    private boolean isKakaoMDAccepted = true;
 
     public Gift(Long giftId, String giftName, Integer giftPrice, String giftPhotoUrl) {
         this.giftId = giftId;
@@ -14,8 +24,7 @@ public class Gift {
         this.giftPhotoUrl = giftPhotoUrl;
     }
 
-    public Gift() {
-    }
+    public Gift() {}
 
     public Long getId() {
         return id;
@@ -37,6 +46,10 @@ public class Gift {
         return giftPhotoUrl;
     }
 
+    public boolean getIsKakaoMDAccepted() {
+        return isKakaoMDAccepted;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -55,5 +68,21 @@ public class Gift {
 
     public void setGiftPhotoUrl(String giftPhotoUrl) {
         this.giftPhotoUrl = giftPhotoUrl;
+    }
+
+    public void setKakaoMDAccepted(boolean kakaoMDAccepted) {
+        isKakaoMDAccepted = kakaoMDAccepted;
+    }
+
+    public boolean isGiftNameValid(){
+        String pattern = "^[a-zA-Z0-9가-힣 ()\\[\\]\\+\\-\\&\\/\\_]*$";
+        return giftName.matches(pattern);
+    }
+
+    public void isKakaoMessageInclude(){
+        String censorshipWord = "카카오";
+        if(giftName.contains(censorshipWord)){
+            this.isKakaoMDAccepted = false;
+        }
     }
 }
