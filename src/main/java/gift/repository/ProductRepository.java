fc72db5329e35c14e0c4c455373615318b641512
YUNJAEGOONER <coygyj@gmail.com>
2025-07-01 20:06:57 +0900

    refactor: repository계층 생성 및 ProductRepository클래스 생성

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,26 @@
+package gift.repository;
+
+
+public class ProductRepository {
+    //repository -> crud동작 수행
+    public void add(){
+
+    }
+
+    public void findById(){
+
+    }
+
+    public void findProducts(){
+
+    }
+
+    public void modifyProduct(){
+
+    }
+
+    public void removeProduct(){
+
+    }
+
+}
