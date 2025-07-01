d836296f0354ba788dc0254e7d45e4d0fd9ec9d9
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 18:47:00 +0900

    feat: '카카오' 문구가 포함된 상품명을 등록할 수 있는 승인 리스트 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE products
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
+
+-- "카카오" 문구가 포함된 상품명을 등록할 수 있는 승인 리스트 테이블
+CREATE TABLE approved_products
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
