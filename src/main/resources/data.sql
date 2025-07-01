e9f375bcb5757008d72b2d1f1f22b58ff20fd7e0
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 22:09:11 +0900

    chore: 승인 상품명 '카카오 프렌즈 필통'으로 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ VALUES
     ('초콜릿', 1000, 'https://i.namu.wiki/i/omn0E--U8ws6U4WXKk2-ITRzSE_YO4Qf0CXoxABZ3Eiwj4lUoqwK5PcTALo3D2o_QxHKl_Xmba9PTXS169yGqAsjTX3DEgCemLSv0aNLhx0HNWv9YLjUeVnUx3DtdzOEUkvB-9fhami0ttZkEeYcfw.webp'),
     ('새우깡', 1500, 'https://i.namu.wiki/i/HgNlAcOFWmo5y9T8Jmp6QiMKBnoXn-wLdhZOdV7vwlNIPaQM3uEFLiKN9u36O2fbpyV_6Zw2xxhMzk5SwjReIYrRYz6KyOFjTVaEGWX2XwFjJSAVTooaJGlhvSfiv7CVZb69RwbqC_xWEOLs-uAREQ.webp');
 
-INSERT INTO approved_products (name) VALUES ('카카오 초콜릿');
+INSERT INTO approved_products (name) VALUES ('카카오 프렌즈 필통');
 INSERT INTO approved_products (name) VALUES ('카카오 프렌즈 인형');
