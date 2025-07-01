df30f44f238fcb4501b96d2cfdaf8db52b25a41c
SwallowLee <lmh2157@outlook.kr>
2025-07-01 14:48:59 +0900

    feat: Testing을 위한 기본 추상 클래스 작성

---
+++ src/test/java/gift/AbstractControllerTest.java
@@ -0,0 +1,18 @@
+package gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractControllerTest {
+    @LocalServerPort
+    protected int port;
+
+    protected RestClient restClient = RestClient.builder().build();
+
+    protected String getBaseUrl() {
+        return "http://localhost:" + port;
+    }
+}
