4e0cfdb3bdd004fc3fffb77f7e2d7ea358e1736e
YUNJAEGOONER <coygyj@gmail.com>
2025-07-01 20:56:56 +0900

    refactor: 사용되지 않는 문장과 메서드 삭제

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -3,13 +3,9 @@ package gift.controller;
 import gift.dto.ProductRequestDto;
 import gift.entity.Product;
 import gift.service.ProductService;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -123,24 +119,5 @@ public class AdminController {
         return true;
     }
 
-    public Optional<Product> findProductById(Long id) {
-        String sql = "select * from products where id = ?";
-        //List<Product> productList = jdbcTemplate.query(sql, productRowMapper(), id);
-        //return productList.stream().findAny();
-        return null;
-    }
-
-    private RowMapper<Product> productRowMapper() {
-        return new RowMapper<Product>() {
-            @Override
-            public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Long id = rs.getLong("id");
-                String name = rs.getString("name");
-                Integer price = rs.getInt("price");
-                String imageUrl = rs.getString("image_url");
-                return new Product(id, name, price, imageUrl);
-            }
-        };
-    }
 }
 
