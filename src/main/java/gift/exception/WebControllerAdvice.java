0b9d53cbf82fc5417acf77ec1bde16ff6d86cd61
Lmmhhhh <lmh10087215@gmail.com>
2025-07-02 05:05:28 +0900

    feat: 에러 메시지 응답

--- src/main/java/gift/exception/WebControllerAdvice.java
+++ src/main/java/gift/exception/WebControllerAdvice.java
@@ -2,8 +2,13 @@ package gift.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class WebControllerAdvice {
@@ -11,5 +16,16 @@ public class WebControllerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFound(ProductNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()){
+            errors.put(error.getField(),error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
