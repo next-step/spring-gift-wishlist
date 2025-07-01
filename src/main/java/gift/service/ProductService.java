ed17d8aea131361e104d60bdc34b54b0ec243a02
YUNJAEGOONER <coygyj@gmail.com>
2025-07-01 20:02:44 +0900

    refactor: Service계층 생성 및 ProductService클래스 생성

---
+++ src/main/java/gift/service/ProductService.java
@@ -0,0 +1,35 @@
+package gift.service;
+
+import gift.dto.ProductRequestDto;
+import gift.entity.Product;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+public class ProductService {
+
+    //상품 추가
+    public void add(ProductRequestDto requestDto){
+
+    }
+
+    //상품 검색
+    public Product findOne(Long id){
+
+    }
+
+    //전체 상품 검색
+    public List<Product> findAll(){
+
+    }
+
+    //상품 수정
+    public void modify(Long id, ProductRequestDto requestDto){
+
+    }
+
+    //상품 삭제
+    public void remove(Long id){
+
+    }
+
+}
