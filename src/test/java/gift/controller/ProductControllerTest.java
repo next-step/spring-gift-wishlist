bccbce92bdb01bf36c729d5754eb3e97fd47b78f
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-01 13:42:26 +0900

    feat: Test Code Template 추가

---
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package gift.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductControllerTest {
+    
+    @BeforeEach
+    void setUp() {
+    }
+    
+    @Test
+    void test1() {
+    
+    }
+}
