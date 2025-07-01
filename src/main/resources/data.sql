1d4ed06a65684cc54e728b0f5027d6ee5a55387c
uvrvuoon <0524yar@gmail.com>
2025-07-01 15:30:26 +0900

    feat: 상품 관리 코드 이동

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into product (name, imageUrl) values ('테스트 상품', 'https://example.com/image.jpg');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table product
+(
+    id bigint AUTO_INCREMENT,
+    name varchar(100),
+    imageUrl varchar(255),
+    primary key (id)
+);
