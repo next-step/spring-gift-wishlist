cde4c82a1f68e90d7fc2dca06f943479899b119b
5seonjae <5seonjae.dev@gmail.com>
2025-07-01 23:43:00 +0900

    feat: 테스트용 ApprovedProduct(String name) 생성자 추가

--- src/main/java/gift/entity/ApprovedProduct.java
+++ src/main/java/gift/entity/ApprovedProduct.java
@@ -7,6 +7,10 @@ public class ApprovedProduct {
 
     public ApprovedProduct() {}
 
+    public ApprovedProduct(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
