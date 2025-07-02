8643d6b896a5a1d7b23e75de7bbaea953637ffdf
ji-mim <194115@jnu.ac.kr>
2025-07-02 23:32:20 +0900

    Refactor(Repository): JDBC Template에서 JDBC Client로 변경

--- src/main/java/gift/repository/ProductJdbcRepository.java
+++ src/main/java/gift/repository/ProductJdbcRepository.java
@@ -4,27 +4,25 @@ import gift.domain.Product;
 import gift.dto.CreateProductRequest;
 import gift.dto.UpdateProductRequest;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class ProductJdbcRepository implements ProductRepository {
 
-    private final NamedParameterJdbcTemplate template;
+    private final JdbcClient client;
     private final SimpleJdbcInsert jdbcInsert;
 
     public ProductJdbcRepository(DataSource dataSource) {
-        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.client = JdbcClient.create(dataSource);
         this.jdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName("product")
                 .usingGeneratedKeyColumns("id");
@@ -41,51 +39,50 @@ public class ProductJdbcRepository implements ProductRepository {
     @Override
     public Optional<Product> findById(Long id) {
         String sql = "select id, name, price, image_url from product where id = :id";
-        Map<String, Long> param = Map.of("id", id);
-        try {
-            Product product = template.queryForObject(sql, param, productRowMapper());
-            return Optional.of(product);
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
+        return client.sql(sql)
+                .param("id", id)
+                .query(Product.class)
+                .optional();
     }
 
     @Override
     public List<Product> findAll() {
         String sql = "select id, name, price, image_url from product";
-        return template.query(sql, productRowMapper());
+        return client.sql(sql)
+                .query(Product.class)
+                .list();
     }
 
     @Override
     public Product update(Long id, UpdateProductRequest request) {
         String sql = "update product set name=:name, price=:price, image_url=:imageUrl where id = :id";
-        MapSqlParameterSource param = new MapSqlParameterSource()
-                .addValue("name", request.name())
-                .addValue("price", request.price())
-                .addValue("imageUrl", request.imageUrl())
-                .addValue("id", id);
-        template.update(sql, param);
+        client.sql(sql)
+                .param("name", request.name())
+                .param("price", request.price())
+                .param("imageUrl", request.imageUrl())
+                .param("id", id)
+                .update();
         return new Product(id, request.name(), request.price(), request.imageUrl());
     }
 
     @Override
     public void delete(Long id) {
         String sql = "delete from product where id = :id";
-        MapSqlParameterSource param = new MapSqlParameterSource()
-                .addValue("id", id);
-        template.update(sql, param);
+        client.sql(sql)
+                .param("id", id)
+                .update();
     }
-
-    private RowMapper<Product> productRowMapper() {
-        return (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int price = rs.getInt("price");
-            String imageUrl = rs.getString("image_url");
-
-            return new Product(id, name, price, imageUrl);
-        };
-    }
+//
+//    private RowMapper<Product> productRowMapper() {
+//        return (rs, rowNum) -> {
+//            long id = rs.getLong("id");
+//            String name = rs.getString("name");
+//            int price = rs.getInt("price");
+//            String imageUrl = rs.getString("image_url");
+//
+//            return new Product(id, name, price, imageUrl);
+//        };
+//    }
 
 
 
