b3fbb283ba34b1edcf7ba1ba9811049ad90a7390
LSH <dltlgos@naver.com>
2025-07-02 14:34:57 +0900

    RequestDto에 글자수 제한, 특수문자 제한 유효성 검사 추가 Service 레이어에서 제품명에 특정 문구 포함 여부 검사 추가

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -1,8 +1,11 @@
 package gift.dto;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record ProductRequestDto(
-        @NotNull(message = "이름은 필수입니다.") String name,
+        @NotNull(message = "이름은 필수입니다.")
+        @Size(max=15, message = "상품 이름은 최대 15자까지 입력 가능합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "상품 이름의 특수문자는 ( ), [ ], +, -, &, /, _ 이외에는 허용되지 않습니다.")
+        String name,
         @NotNull(message = "가격은 필수입니다.") Long price,
         @NotNull(message = "이미지링크는 필수입니다.") String imageUrl
 ) {}
--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -3,18 +3,31 @@ package gift.exception;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 // 예외 관리 핸들러
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({ProductNotFoundException.class, EmptyResultDataAccessException.class}) // 자체 예외 및 queryForObject 예외
+    // 자체 예외 및 queryForObject 예외
+    @ExceptionHandler({ProductNotFoundException.class, EmptyResultDataAccessException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFound(RuntimeException e) {
         return e.getMessage();
     }
 
+    // queryForObject로 2개 이상의 제품을 찾은 경우 예외
     @ExceptionHandler(IncorrectResultSizeDataAccessException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleIncorrectResultSizeException(RuntimeException e) { return e.getMessage(); }
+    public String handleInternalServerError(RuntimeException e) { return e.getMessage(); }
+
+    // MD가 아닌데 특정 문구를 포함한 제품명을 입력한 경우
+    @ExceptionHandler(InvalidProductNameException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleForbidden(RuntimeException e) { return e.getMessage(); }
+
+    // ProductRequestDto에 지정된 Validation 검사에서 예외가 발생한 경우
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequest(MethodArgumentNotValidException e) { return e.getBindingResult().getAllErrors().getFirst().getDefaultMessage(); }
 }
