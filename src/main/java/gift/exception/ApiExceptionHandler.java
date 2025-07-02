5aa8f4b605ae177ca3aa4d827a90510d8138684d
KIYEONG <rldud1237@naver.com>
2025-07-02 16:38:09 +0900

    feat: apiController의 적용 범위 한정

--- src/main/java/gift/exception/ApiExceptionHandler.java
+++ src/main/java/gift/exception/ApiExceptionHandler.java
@@ -1,11 +1,15 @@
 package gift.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "gift.controller.api")
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
@@ -16,6 +20,17 @@ public class ApiExceptionHandler {
     @ExceptionHandler(DuplicateItemException.class)
     public ResponseEntity<String> handleDuplicateItemException(DuplicateItemException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(Exception.class)
