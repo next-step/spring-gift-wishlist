bccecca699b9b63cba87335cc59a73579fa3d955
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-07-02 15:35:25 +0900

    feat: validation 적용 및 Request DTO 분리

--- src/main/java/gift/dto/request/ProductSaveReqDTO.java
+++ src/main/java/gift/dto/request/ProductSaveReqDTO.java
@@ -1,32 +1,15 @@
 package gift.dto.request;
 
-import jakarta.validation.constraints.Max;
+import gift.common.annotation.NameValidation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
 
-public record ProductReqDTO(
+public record ProductSaveReqDTO(
     @NotBlank(
         message = "상품 이름을 입력해주세요."
     )
-    @Size(
-        min = 1,
-        max = 15,
-        message = "상품 이름은 공백을 포함하여 최대 15글자까지 입력할 수 있습니다."
-    )
-    @Pattern.List({
-        @Pattern(
-            regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9\\(\\)\\[\\]\\+\\-\\&/_ ]*$",
-            message = "상품 이름은 한글, 영어, 숫자, 특수문자(( ), [ ], +, -, &, /, _) 외 "
-                + "다른 문자가 들어갈 수 없습니다."
-        ),
-        @Pattern(
-            regexp = "^(?!.*카카오).*$",
-            message = "'카카오'가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다."
-        )
-    })
+    @NameValidation
     String name,
 
     @NotNull(
@@ -34,10 +17,6 @@ public record ProductReqDTO(
     )
     @PositiveOrZero(
         message = "올바른 상품 가격을 입력해주세요."
-    )
-    @Max(
-        value = 2147483647,
-        message = "상품 가격은 2,147,483,647원을 초과할 수 없습니다."
     )
     Integer price,
 
--- src/main/java/gift/dto/request/ProductUpdateReqDTO.java
+++ src/main/java/gift/dto/request/ProductUpdateReqDTO.java
@@ -1,4 +1,18 @@
 package gift.dto.request;
 
-public record ProductUpdateReqDTO() {
+import gift.common.annotation.NameValidation;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ProductUpdateReqDTO(
+    @NameValidation
+    String name,
+
+    @PositiveOrZero(
+        message = "올바른 상품 가격을 입력해주세요."
+    )
+    Integer price,
+
+    String imageURL
+) {
+
 }
