1f8d5089063ef98dd0be89b4a7b521a47a4680b2
KIYEONG <rldud1237@naver.com>
2025-07-02 16:32:24 +0900

    feat: 중복 아이디에 대한 커스텀 예외 추가 및 전역 예외 처리기 구현

---
+++ src/main/java/gift/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package gift.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<String> handleItemNotFoundException(ItemNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateItemException.class)
+    public ResponseEntity<String> handleDuplicateItemException(DuplicateItemException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류가 발생했습니다.");
+    }
+}
