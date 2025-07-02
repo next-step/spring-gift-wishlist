1ffe9256c5da6dbbf50fe4171ce4b67fd5ff7c94
HyeonJun <osumaniaddict527@gmail.com>
2025-07-02 18:00:05 +0900

    Feat(ErrorCode): 바인딩 실패도 유효성 검사 실패로 판단하여 변경 오류를 4xx와 5xx로 구분하는게 에러의 원인을 인식하기에 명확함. 도메인 오류 요구사항 반영. 구체적인 에러 정보를 클라이언트에 제공하는 것에 의구심을 가졌음 그래서 사용자 친화적인 메세지를 전달하기로 함.

--- src/main/java/gift/domain/product/dto/ProductRequest.java
+++ src/main/java/gift/domain/product/dto/ProductRequest.java
@@ -1,11 +1,14 @@
 package gift.domain.product.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public class ProductRequest {
     @NotBlank(message = "상품 이름은 비어 있을 수 없습니다.")
+    @Size(max = 15, message = "상품명은 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s()\\[\\]+&/_-]*$",
+            message = "상품명에는 허용되지 않은 특수문자가 포함되어 있습니다. 사용 가능한 특수문자: ( ), [ ], +, -, &, /, _")
+    @Pattern(regexp = "^(?!.*카카오).*$",
+            message = "\"카카오\"가 포함된 상품명은 담당 MD와 협의 후 사용 가능합니다.")
     private String name;
 
     @NotNull(message = "상품 가격은 비어 있을 수 없습니다.")
