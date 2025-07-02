0c8ff51d7f6c29cc46807432ba307207376bf861
Dohyun <sks020k@naver.com>
2025-07-02 16:10:36 +0900

    feat : ErrorCode 명시를 위해 enum클래스 추가

--- src/main/java/gift/exception/ErrorCode.java
+++ src/main/java/gift/exception/ErrorCode.java
@@ -1,5 +1,24 @@
 package gift.exception;
 
-public class ErrorCode {
+public enum ErrorCode {
 
+  INVALID_PRODUCT_NAME("INVALID_PRODUCT_NAME", "상품명이 올바르지 않습니다."),
+  INVALID_PRODUCT_PRICE("INVALID_PRODUCT_PRICE", "상품 가격이 올바르지 않습니다."),
+  INVALID_IMAGE_URL("INVALID_IMAGE_URL", "이미지 URL이 올바르지 않습니다.");
+
+  private final String code;
+  private final String message;
+
+  ErrorCode(String code, String message) {
+    this.code = code;
+    this.message = message;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
