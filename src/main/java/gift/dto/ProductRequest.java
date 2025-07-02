c6f44cc6a56b5854a75044b31a7fe256fdbb9de6
Hyun <hyun_7586@naver.com>
2025-07-02 22:55:40 +0900

    refactor: request DTO에 검증 어노테이션 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -5,6 +5,20 @@ package gift.dto;
  이후에 기능 확장, 필드 추가 등을 고려해 미리 분리해 놓는 게 낫다고 판단했습니다.
 */
 
-public record ProductRequest(Long id, String name, Integer price, String imageUrl) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-}
+public record ProductRequest(
+    Long id,
+
+    @NotNull(message = "상품명은 필수입니다.")
+    @Size(max = 15, message = "상품명은 최대 15자까지 가능합니다.")
+    @Pattern(regexp = "^((?!카카오).)*$", message = "'카카오'가 들어간 상품명 등록은 관리자에게 문의해 주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_\\s]+$", message="사용가능한 특수문자: (), [], +, -, &, /, _")
+    String name,
+    Integer price,
+    String imageUrl
+) {}
