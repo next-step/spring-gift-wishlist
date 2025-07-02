e6d906e9cf0b3df18631776355348d371189cb6c
lwj0831 <wjl0831@gmail.com>
2025-07-02 15:16:01 +0900

    feat(product): 상품 등록, 수정 DTO 유효성 검증 수정
    - CreateProductReqDto, UpdateProductReqDto의 필드에 요구사항에 맞는 Bean Validation 어노테이션 적용
    - 상품명에 '카카오' 포함 여부를 검증하는 Custom Constraint 구현 및 적용

--- src/main/java/gift/product/dto/UpdateProductReqDto.java
+++ src/main/java/gift/product/dto/UpdateProductReqDto.java
@@ -1,5 +1,6 @@
 package gift.product.dto;
 
+import gift.product.validation.NoKakao;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -8,6 +9,12 @@ import jakarta.validation.constraints.Size;
 
 public record UpdateProductReqDto(
     @NotBlank(message = "상품명은 필수값입니다")
+    @Size(max = 15)
+    @Pattern(
+        regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-&/_가-힣]*$",
+        message = "상품 이름에는 허용되지 않은 특수문자가 포함되어 있습니다."
+    )
+    @NoKakao
     String name,
     @PositiveOrZero(message = "가격은 0원 이상이어야 합니다")
     @NotNull(message = "가격은 필수값입니다")
