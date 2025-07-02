9215de9b0d32f56235fa43142f2e7e74d064f4fa
jnujh <jnu21jihoon@gmail.com>
2025-07-02 16:17:16 +0900

    feat: ProductRequest에 유효성 검사를 위한 어노테이션 추가 및 build.gradle에 validation 의존성 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -1,3 +1,23 @@
 package gift.dto;
 
-public record ProductRequest(Long categoryId, String name, int price, String imageUrl) {}
+import jakarta.validation.constraints.*;
+
+public record ProductRequest(
+
+        Long categoryId,
+
+        @NotBlank(message = "상품 이름은 필수입니다.")
+        @Size(max = 15, message = "상품 이름은 최대 15자까지 입력 가능합니다.")
+        @Pattern(
+                regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]\\+\\-\\&/_]*$",
+                message = "상품 이름에 허용되지 않는 특수 문자가 포함되어 있습니다."
+        )
+        String name,
+
+        @Min(value = 0, message = "상품 가격은 0원 이상이어야 합니다.")
+        int price,
+
+        @NotBlank(message = "이미지 URL은 필수입니다.")
+        String imageUrl
+
+) {}
