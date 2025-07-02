4cbb239fa4a0b3c330bb9f0b646d5f6f1c43de30
seoyoungjin23 <135246488+seoyoungjin23@users.noreply.github.com>
2025-07-03 00:35:58 +0900

    강원대 서영진 1단계 - 유효성 검사 및 예외 처리 (#145)
    * feat(validation): 상품 이름 유효성 검사 추가
    
    최대 문자 길이 15자까지 제한, 가능한 특수문자 제한((), [], +, -, &, /, _), '카카오' 키워드 MD와 협의한 경우에만 사용할 수 있도록 제약
    
    * test: 상품 관리 API E2E 테스트 추가
    
    * feat(admin): 관리자 페이지 '카카오' 키워드 유효성 검사 추가
    
    카카오 키워드 유효성 검사는 @Valid로 자동 처리되지 않기 때문에, 검사 후 BindingResult에 수동으로 등록하여 오류 메시지를 폼에 표시한다.
    
    * docs(readme): 유효성 검사 및 예외 처리 readme 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -2,10 +2,14 @@ package gift.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ProductRequest(
 
     @NotBlank(message = "상품명은 필수입니다.")
+    @Size(max = 15, message = "상품명은 15자 까지만 입력 가능합니다.")
+    @Pattern(regexp = "^[\\s\\w가-힣-+()&/\\[\\]]+$", message = "지원하지 않는 문자가 포함되어있습니다.")
     String name,
     @NotNull(message = "가격은 필수입니다.")
     Integer price,
