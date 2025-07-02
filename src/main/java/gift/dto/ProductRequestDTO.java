d3e2c93096cb7af20ec85914f5ceb7836614655e
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-02 16:50:55 +0900

    feat(validation): 상품명 길이 및 특수 문자 유효성 검사 추가

--- src/main/java/gift/dto/ProductRequestDTO.java
+++ src/main/java/gift/dto/ProductRequestDTO.java
@@ -1,11 +1,16 @@
 package gift.dto;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigInteger;
 
 public record ProductRequestDTO (
-        @Size(max = 255, message = "상품명은 255자를 초과할 수 없습니다.")
+        @Size(max = 15, message = "상품 이름은 최대 15자까지 입력할 수 있습니다.")
+        @Pattern(
+                regexp = "^[\\p{L}\\p{N}\\s()\\[\\]+\\-&/_]*$",
+                message = "(),[],+,-,&,/,_ 외의 특수 문자는 사용이 불가합니다."
+        )
         String name,
         BigInteger price,
         @Size(max = 1000, message = "이미지 URL은 1000자를 초과할 수 없습니다.")
