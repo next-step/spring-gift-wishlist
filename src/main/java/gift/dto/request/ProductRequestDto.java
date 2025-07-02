09e13b72182c7fccc8b4e460b155fec7873f4fcb
jihwan38 <sss203800@gmail.com>
2025-07-02 12:39:39 +0900

    feat: 상품의 이름에 허용된 특수문자 외 사용 불가능한 유효성 검사 추가

--- src/main/java/gift/dto/request/ProductRequestDto.java
+++ src/main/java/gift/dto/request/ProductRequestDto.java
@@ -1,15 +1,16 @@
 package gift.dto.request;
 
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.URL;
 
 public record ProductRequestDto(
         @NotBlank(message = "상품 이름을 입력해 주세요.")
         @Size(max = 15, message = "상품 이름은 최대 15자까지 입력 가능합니다." )
+        @Pattern(
+                regexp = "^[a-zA-Z0-9가-힣\\s()\\[\\]+\\-&/_]*$",
+                message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다."
+        )
         String name,
 
         @NotNull(message = "가격을 입력해 주세요.")
