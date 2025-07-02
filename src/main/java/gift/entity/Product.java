b95dfc50c281274b095b740745d6f9cd56c9e24f
seongwon02 <seongwon2303@gmail.com>
2025-07-02 14:23:21 +0900

    feat: Product Entitiy에 approved 필드 추가

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -6,18 +6,21 @@ public class Product {
     private String name;
     private Long price;
     private String imageUrl;
+    private Boolean approved;
 
-    public Product(Long id, String name, Long price, String imageUrl) {
+    public Product(Long id, String name, Long price, String imageUrl, boolean approved) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.approved = approved;
     }
 
-    public void updateProduct(String name, Long price, String imageUrl) {
+    public void updateProduct(String name, Long price, String imageUrl, Boolean approved) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.approved = approved;
     }
 
     public Long getId() {
@@ -35,4 +38,6 @@ public class Product {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    public boolean getApproved() { return approved; }
 }
