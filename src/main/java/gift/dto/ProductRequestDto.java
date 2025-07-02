94fef5a7d14554de4f063902bbbdd372130fe370
hopencode <newalfo1006@gmail.com>
2025-07-02 16:28:42 +0900

    중간에 공백 패턴을 지워서 공백 다시 포함시킴

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -7,7 +7,7 @@ public class ProductRequestDto {
 
     @Size(min = 1, max = 15, message = "상품 이름은 최대 15자까지 입력할 수 있습니다.")
     @Pattern(
-            regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_]*$",
+            regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$",
             message = "문자 및 숫자 또는 유효한 특수문자 ( '( )', '[ ]', '+', '-', '&', '/', '_' ) 만 사용할 수 있습니다."
     )
     private String name;
