45c713f741a637160c49632cfc1dfeb27fe8e434
gosurim <su020900@gmail.com>
2025-07-02 17:04:03 +0900

    feat: service 카카오 유효성 검사 추가

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -4,9 +4,11 @@ import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
 import gift.repository.ProductRepository;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -18,8 +20,11 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-
-    public ProductResponseDto saveProduct(ProductRequestDto requestDto) {
+    public ProductResponseDto saveProduct(@Valid @RequestBody ProductRequestDto requestDto) {
+        if (requestDto.name().contains("카카오")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다.");
+        }
         Product product = new Product(requestDto.name(), requestDto.price(), requestDto.imageUrl());
         Product savedProduct = productRepository.saveProduct(product);
 
@@ -37,12 +42,13 @@ public class ProductService {
     }
 
     public ProductResponseDto updateProduct(Long id, String name, Integer price, String url) {
+        if (name.contains("카카오")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다.");
+        }
         Product product = productRepository.findProductById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        if (product == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
-        }
         product.update(name, price, url);
         productRepository.updateProduct(product);
 
