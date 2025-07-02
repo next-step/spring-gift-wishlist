0f80758b5bff205de19c65c9a07f2554c2473d2c
seongwon02 <seongwon2303@gmail.com>
2025-07-02 14:19:13 +0900

    feat: MD 승인 여부를 담는 column 추가 및 초기 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,5 +5,6 @@ create table products
     id          bigint auto_increment primary key,
     name        varchar(255) not null,
     price       bigint not null,
-    imageUrl    varchar(1024)
+    imageUrl    varchar(1024) not null,
+    approved    boolean default true not null
 );
