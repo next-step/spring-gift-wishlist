f4ec941ce737d4061bac2fa661037ff191097781
treasure-sky <ych0486666@gmail.com>
2025-07-02 15:56:32 +0900

    feat: 상품명 입력값 특정 문자열 제한 validation 추가
    - 상품명에 대한 입력값에 "카카오" 라는 문자열이 포함되면 제한하도록 구현하였음.
    
    - 제한 문자열이 추가되는 경우를 고려하여 커스텀 어노테이션을 통해 확장성 있게 구현하였음.

--- src/main/java/gift/item/dto/ItemUpdateDto.java
+++ src/main/java/gift/item/dto/ItemUpdateDto.java
@@ -1,6 +1,7 @@
 package gift.item.dto;
 
 import gift.common.validation.OnlyPermittedSymbols;
+import gift.common.validation.RestrictedWords;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -8,6 +9,7 @@ public record ItemUpdateDto(
     @NotBlank(message = "상품명은 필수입니다.")
     @Size(max = 15, message = "상품명은 공백포함 최대 15자까지 입력할 수 있습니다.")
     @OnlyPermittedSymbols
+    @RestrictedWords(words = {"카카오"})
     String name,
     Integer price,
     String imageUrl
