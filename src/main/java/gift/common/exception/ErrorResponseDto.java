95c152069e81701f8fb3c892b2fca92d7570185a
treasure-sky <ych0486666@gmail.com>
2025-07-02 14:15:56 +0900

    feat: 예외 응답 양식 수정(RFC 9457)
    - RFC 9457의 표준에 맞추어 에러 응답을 보내도록 변경하였음.
    
    - ProblemDetail은 제공해주는 생성자가 제한적이므로 이를 상속받아 새로운 생성자를 구현하였음.
    
    - type 필드에는 해당 에러의 설명에 관한 URI가 들어가야 하지만 아직 구현된 바가 없으므로 `about:blank` 로 두었음.
    
    - type 필드가 `about:blank` 인 경우 title을 해당 에러의 HTTP status phrase로 하라는 권고사항이 있으므로 이를 적용하였음.

--- src/main/java/gift/common/exception/ErrorResponseDto.java
+++ src/main/java/gift/common/exception/ErrorResponseDto.java
@@ -1,21 +1,25 @@
 package gift.common.exception;
 
-public class ErrorResponseDto {
+import java.net.URI;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 
-    private final int httpStatusCode;
-    private final String message;
+public class ErrorResponseDto extends ProblemDetail {
 
+    /**
+     * type을 설정하지 않은 ProblemDetail을 생성
+     *
+     * @param status   http 상태 코드
+     * @param detail   상세 설명
+     * @param instance 예외 발생한 URI
+     */
+    public ErrorResponseDto(HttpStatus status, String detail, URI instance) {
+        super(status.value());
+        this.setDetail(detail);
+        this.setInstance(instance);
 
-    public ErrorResponseDto(int httpStatusCode, String message) {
-        this.httpStatusCode = httpStatusCode;
-        this.message = message;
+        // RFC 9457의 권고에 따라 type이 `about:blank`일 때 title을 HTTP status phrase와 동일하게 지정
+        this.setTitle(status.getReasonPhrase());
     }
 
-    public int getHttpStatusCode() {
-        return httpStatusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
--- src/main/java/gift/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package gift.common.exception;
 
 import gift.item.exception.ItemNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleItemNotFoundException(ItemNotFoundException e) {
-        ErrorResponseDto errorResponse = new ErrorResponseDto(HttpStatus.NOT_FOUND.value(),
-            e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    public ResponseEntity<ErrorResponseDto> handleItemNotFoundException(
+        ItemNotFoundException e,
+        HttpServletRequest request
+    ) {
+        ErrorResponseDto dto = new ErrorResponseDto(
+            HttpStatus.NOT_FOUND,
+            e.getMessage(),
+            URI.create(request.getRequestURI())
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
     }
 
 }
