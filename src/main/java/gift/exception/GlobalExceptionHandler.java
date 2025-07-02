c5541344a4ce9b6396edd60e021d4d3357fb8bac
Hyun <hyun_7586@naver.com>
2025-07-02 22:56:47 +0900

    refactor: 상품 생성/수정 유효성 검증에 따른 예외처리 메서드 추가

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package gift.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +17,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleInvalidProductException(InvalidProductException e) {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
             .body(e.getErrorCode().getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleConstraintViolationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = "invalid in "+ ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(errors);
     }
 
     @ExceptionHandler(Exception.class)
