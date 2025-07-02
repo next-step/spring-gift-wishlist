c4f90297dab30e0c4588ed4b47e483f1c6e3317b
Dohyun <sks020k@naver.com>
2025-07-02 16:57:03 +0900

    특수문자 요구사항 테스트 성공

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -2,11 +2,16 @@ package gift.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ProductRequestDto(
     @NotBlank(message = "상품명은 입력되어야 합니다.")
     @Size(max = 15, message = "상품명의 최대길이는 15자 입니다.")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_\\s]*$",
+        message = "상품명에 (), [], +, -, &, /, _ 는 가능합니다."
+    )
     String name,
 
     @Min(value = 0, message = "최소가격은 0입니다.")
