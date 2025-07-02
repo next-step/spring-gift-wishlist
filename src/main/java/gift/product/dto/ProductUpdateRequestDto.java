66731586dd836ac8ec09c2c5b94aa4039dcbb49b
Junyoung Yang <y@jun0.dev>
2025-07-02 15:27:44 +0900

    feat: 상품명에 대한 validation 추가
    - 상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있다.
    - 특수 문자 가능: ( ), [ ], +, -, &, /, _
    - 그 외 특수 문자 사용 불가

--- src/main/java/gift/product/dto/ProductUpdateRequestDto.java
+++ src/main/java/gift/product/dto/ProductUpdateRequestDto.java
@@ -1,8 +1,17 @@
 package gift.product.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import org.hibernate.validator.constraints.Length;
+
 public record ProductUpdateRequestDto(
+        @Length(max = 15, message = "상품 이름은 최대 15자까지만 입력 가능합니다.")
+        @Pattern(regexp = ".*\\S.*", message = "공백만 입력할 수 없습니다.")
+        @Pattern(regexp = "[0-9a-zA-Zㄱ-ㅎ가-힣 ()\\[\\]+\\-&/_]+", message = "상품 이름은 한글, 영문, 숫자, 공백, 특수문자((), [], +, -, &, /, _)만 사용할 수 있습니다.")
         String name,
+        @PositiveOrZero(message = "상품 가격은 0 이상이어야 합니다.")
         Long price,
         String imageUrl
 ) {
+
 }
