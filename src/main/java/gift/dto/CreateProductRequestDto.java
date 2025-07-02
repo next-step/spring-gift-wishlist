8b44b11a0e4e764fa0aac03aa54fba5b19dfcab7
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-02 14:10:48 +0900

    fix: dto 내부 생성자 검증 삭제

--- src/main/java/gift/dto/CreateProductRequestDto.java
+++ src/main/java/gift/dto/CreateProductRequestDto.java
@@ -14,10 +14,4 @@ public record CreateProductRequestDto(
         @NotBlank(message = "이름을 입력해주세요") @Size(max = 15, message = "최대 15자 까지 입력 가능합니다") @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$]", message = "( ), [ ], +, -, &, /, _ 외의 특수 문자는 사용 불가능 합니다") String name,
         @NotNull(message = "가격을 입력해주세요") @Positive(message = "가격은 음수가 될 수 없습니다") Long price,
         @NotBlank(message = "이미지 Url을 입력해주세요") @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$]", message = "( ), [ ], +, -, &, /, _ 외의 특수 문자는 사용 불가능 합니다") String imageUrl) {
-
-    public CreateProductRequestDto {
-        if (name == null || price == null || imageUrl == null || price < 0L) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-    }
 }
