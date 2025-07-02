16e3a20e91f4c6af2a3377cf490e7cd299d039e6
lwj0831 <wjl0831@gmail.com>
2025-07-02 16:39:39 +0900

    feat(exception): 검증 실패 시 응답 개선
    - API 응답시 풍부한 정보를 위해 ErrorResponse에 추가 정보 전달용 extras 필드 추가
    - MethodArgumentNotValidException 발생 시 FieldError 정보 담도록
      수정
    - ConstraintViolationException 발생 시 violation 정보 담도록 수정

--- src/main/java/gift/global/exception/dto/ErrorResponse.java
+++ src/main/java/gift/global/exception/dto/ErrorResponse.java
@@ -1,14 +1,23 @@
 package gift.global.exception.dto;
 
 import gift.global.exception.ErrorCode;
+import java.util.Collections;
+import java.util.Map;
 
-public record ErrorResponse(String errorCode, String errorMessage) {
-
-  public static ErrorResponse from(ErrorCode errorCode) {
-    return new ErrorResponse(
-        errorCode.getErrorCode(),
-        errorCode.getErrorMessage()
-    );
+public record ErrorResponse(
+  String errorCode,
+  String errorMessage,
+  Map<String, Object> extras
+) {
+  public ErrorResponse(String errorCode, String errorMessage) {
+    this(errorCode, errorMessage, Collections.emptyMap());
   }
 
+  public static ErrorResponse from(ErrorCode errorCode) {
+    return new ErrorResponse(errorCode.getErrorCode(), errorCode.getErrorMessage());
+  }
+
+  public static ErrorResponse from(ErrorCode errorCode, Map<String, Object> extras) {
+    return new ErrorResponse(errorCode.getErrorCode(), errorCode.getErrorMessage(), extras);
+  }
 }
