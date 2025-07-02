ce56e4a6cfd1c178b5b5c14db6b248f537046b7b
doorcs <itsdoorcs@gmail.com>
2025-07-03 01:47:23 +0900

    refactor: 유효성 검사 실패시 원인을 알려줄 수 있도록 수정

--- src/main/java/gift/dto/CreateProductRequest.java
+++ src/main/java/gift/dto/CreateProductRequest.java
@@ -9,16 +9,19 @@ import jakarta.validation.constraints.Pattern;
 
 public record CreateProductRequest(
 
-    @NotEmpty
-    @Length(max = 15)
-    @Pattern(regexp = "^[0-9a-zA-Z가-힣()\\s\\[\\]+\\-&/_]*$")
+    @NotEmpty(message = "상품명이 입력되지 않았습니다.")
+    @Length(max = 15, message = "상품명은 최대 15자까지 입력 가능합니다.")
+    @Pattern(
+        regexp = "^[0-9a-zA-Z가-힣()\\s\\[\\]+\\-&/_]*$",
+        message = "( ), [ ], +, -, &, /, _ 외의 특수문자는 사용하실 수 없습니다."
+    )
     String name,
 
-    @NotNull
-    @Min(value = 0)
+    @NotNull(message = "상품 가격이 입력되지 않았습니다.")
+    @Min(value = 0, message = "상품 가격은 음수가 될 수 없습니다.")
     Integer price,
 
-    @NotEmpty
+    @NotEmpty(message = "상품 이미지가 입력되지 않았습니다.")
     String imageUrl
 ) {
 }
