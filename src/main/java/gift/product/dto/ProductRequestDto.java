eda2a906881877bd978e304a0602460acb38069f
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-02 16:34:33 +0900

    feat: 특수문자 필터 적용

--- src/main/java/gift/product/dto/ProductRequestDto.java
+++ src/main/java/gift/product/dto/ProductRequestDto.java
@@ -2,13 +2,16 @@ package gift.product.dto;
 
 import gift.product.entity.Product;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class ProductRequestDto {
 
     private Long id;
     @Size(min = 1 , max = 15,message = "이름은 1~15자 여야합니다")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$", message = "특수문자는 (), [], +, -, &, /, _ 만 허용됩니다.")
     private String name;
+
     @NotNull(message = "비울 수 없습니다")
     private Integer price;
     private String imageUrl;
