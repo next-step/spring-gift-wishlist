3c5c3ecd27f4ddc2f31e019833f247b00addf76b
sohyeon <studyhyeon1004@naver.com>
2025-07-02 17:48:45 +0900

    feat: 상품 유효성 검사 및 예외 처리 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -1,10 +1,19 @@
 package gift.dto;
 
 import gift.entity.Product;
+import gift.validator.ValidProductName;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public record ProductRequest(
+        @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
+        @ValidProductName
         String name,
+
+        @Positive(message = "상품 가격은 0보다 커야 합니다.")
         Integer price,
+
+        @NotBlank(message = "이미지 URL은 비워둘 수 없습니다.")
         String imageUrl
 ) {
     public Product toEntity() {
