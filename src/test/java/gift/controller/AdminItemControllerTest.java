a2205a7396c7613f6ac78e1afb77ed36a4b78257
KIYEONG <rldud1237@naver.com>
2025-07-02 16:05:11 +0900

    test: AdminItemController 상품 조회, 삭제 기능 테스트 코드 추가

--- src/test/java/gift/controller/AdminItemControllerTest.java
+++ src/test/java/gift/controller/AdminItemControllerTest.java
@@ -1,11 +1,15 @@
 package gift.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import gift.entity.Item;
+import gift.repository.ItemRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +26,16 @@ class AdminItemControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    private Item testItem;
+
+    @BeforeEach
+    void setUp() {
+        testItem = itemRepository.save(new Item(null, "사전 등록 상품", 20000, "before.jpg"));
+    }
 
     @Test
     @DisplayName("상품 등록 성공 테스트")
@@ -46,5 +60,23 @@ class AdminItemControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("admin/items/form"))
             .andExpect(model().hasErrors());
+    }
+
+    @Test
+    @DisplayName("상품 상세 조회 성공 테스트")
+    void detailItem_Success() throws Exception {
+        mockMvc.perform(get("/admin/items/" + testItem.getId()))
+            .andExpect(status().isOk())
+            .andExpect(view().name("admin/items/detail"))
+            .andExpect(model().attributeExists("item"));
+    }
+
+    @Test
+    @DisplayName("상품 삭제 성공 테스트")
+    void deleteItem_Success() throws Exception {
+        mockMvc.perform(post("/admin/items/" + testItem.getId())
+                .param("_method", "delete"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/admin/items"));
     }
 }
