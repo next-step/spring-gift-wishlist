63f5934c996c77ad8c71d8616910b1217abde8d3
YoonGangHoon <yghun021007@naver.com>
2025-07-02 14:08:17 +0900

    feat: 상품명에 "카카오"가 포함된 경우 유효성 검사 추가

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -1,5 +1,6 @@
 package gift.dto;
 
+import gift.validation.ForbiddenWord;
 import jakarta.validation.constraints.*;
 
 public class ProductRequestDto {
@@ -12,6 +13,7 @@ public class ProductRequestDto {
             regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-\\&/_\\s]*$",
             message = "특수문자는 ()[]+-&/_ 만 사용할 수 있어요."
     )
+    @ForbiddenWord(word = "카카오", message = "상품명에 <카카오>가 포함된 상품은 담당 MD에게 문의해주세요.")
     private String name;
 
     @NotNull(message = "가격은 필수 입력값입니다.")
