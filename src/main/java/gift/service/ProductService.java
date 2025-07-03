e9bd46e9cc0501378546918917c11f1b219af86b
gosurim <su020900@gmail.com>
2025-07-03 18:26:51 +0900

    refactor: Service의 @Valid, @RequestBody 제거

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -4,11 +4,9 @@ import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
 import gift.repository.ProductRepository;
-import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -20,7 +18,7 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public ProductResponseDto save(@Valid @RequestBody ProductRequestDto requestDto) {
+    public ProductResponseDto save(ProductRequestDto requestDto) {
         if (requestDto.name().contains("카카오")) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다.");
