d0330c0308d5660be32b1d4f2718451a373ea561
seoyoungjin23 <ascd050@gmail.com>
2025-07-03 15:32:52 +0900

    refactor(validation): 확장성을 고려하여 FORBIDDEN_KEYWORD로 변경
    FORBIDDEN_KEYWORD_KAKAO에서 추후 추가될 수 있는 권한 관련 키워드들을 고려하여 FORBIDDEN_KEYWORD로 변경한다.

--- src/main/java/gift/common/validator/ProductRequestValidator.java
+++ src/main/java/gift/common/validator/ProductRequestValidator.java
@@ -2,6 +2,7 @@ package gift.common.validator;
 
 import gift.common.code.CustomResponseCode;
 import gift.dto.ProductRequest;
+import java.util.List;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -9,20 +10,29 @@ import org.springframework.validation.Validator;
 @Component
 public class ProductRequestValidator implements Validator {
 
-    private static final String FORBIDDEN_KEYWORD = "카카오";
+    private static final List<String> FORBIDDEN_KEYWORDS = List.of("카카오");
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return clazz.isAssignableFrom(ProductRequest.class);
+        return ProductRequest.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
         ProductRequest request = (ProductRequest) target;
 
-        if (request.name() != null && request.name().contains(FORBIDDEN_KEYWORD)) {
-            errors.rejectValue("name", "forbidden-keyword",
-                CustomResponseCode.FORBIDDEN_KEYWORD_KAKAO.getMessage());
+        String name = request.name();
+        if (name == null) {
+            return;
+        }
+
+        for (String keyword : FORBIDDEN_KEYWORDS) {
+            if (name.contains(keyword)) {
+                String message = String.format(CustomResponseCode.FORBIDDEN_KEYWORD.getMessage(),
+                    keyword);
+                errors.rejectValue("name", "forbidden-keyword", message);
+                break;
+            }
         }
     }
 }
