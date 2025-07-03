5f38df98f7e96ad99db224d3875240d65cf88ecf
ming0511 <alsrbdhkalsw@naver.com>
2025-07-03 17:26:04 +0900

    chore: 불필요한 주석 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,8 @@
 CREATE TABLE products
 (
-    productId BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name      VARCHAR(255)  NOT NULL,
-    price     INT           NOT NULL,
-    imageUrl  VARCHAR(1000) NOT NULL,
-    mdConfirmed BOOLEAN NOT NULL DEFAULT FALSE
+    productId   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255)  NOT NULL,
+    price       INT           NOT NULL,
+    imageUrl    VARCHAR(1000) NOT NULL,
+    mdConfirmed BOOLEAN       NOT NULL DEFAULT FALSE
 );
-
--- CREATE TABLE approved_products
--- (
---     id   BIGINT PRIMARY KEY AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL
--- );
---
--- INSERT INTO approved_products(id, name) VALUES (1, "카카오");
