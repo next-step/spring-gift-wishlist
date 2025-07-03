9247600d30cd001a69ee558424e864842854c0cb
IT-Gentleman <gentleman@kakao.com>
2025-07-03 18:46:48 +0900

    feat: handle exception not only NotValid but also 404/500/etc.

--- src/main/java/gift/handler/ApiExceptionHandler.java
+++ src/main/java/gift/handler/ApiExceptionHandler.java
@@ -1,14 +1,16 @@
 package gift.handler;
 
+import gift.controller.ProductController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice(assignableTypes = ProductController.class)
+public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
