32d88d84a190445a5479ecc8dc5d3dcfd924d5bb
milk-stone <ywshero@naver.com>
2025-07-03 18:03:22 +0900

    refactor: 2가지 변경 사항
    1. 가격에 대한 변경 사항
    - 최솟값을 1원으로 설정 (무료인 상품은 없으므로..)
    - 최댓값을 2억원으로 설정
    
    2. 상품 이름에 대한 변경 사항
    - 빈 값이 들어오지 않도록 하는 어노테이션만 유지함.
    - Product 객체 생성 시점에 유효성을 검증할 수 있도록 Product 클래스 내부에 검증 메서드 작성

--- src/main/java/gift/product/dto/ProductRequest.java
+++ src/main/java/gift/product/dto/ProductRequest.java
@@ -4,20 +4,11 @@ import jakarta.validation.constraints.*;
 
 public record ProductRequest(
         @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
-        @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
-        @Pattern(
-                regexp = "^[a-zA-Z0-9가-힣\\s\\(\\)\\[\\]\\+\\-\\&\\/_]*$",
-                message = "상품 이름에 허용되지 않는 특수문자가 포함되어 있습니다. ( ), [ ], +, -, &, /, _ 만 사용 가능합니다."
-        )
-        @Pattern(
-                regexp = "^(?!.*카카오).*$",
-                message = "상품 이름에 '카카오'는 담당 MD와 협의된 경우에만 사용할 수 있습니다."
-        )
         String name,
 
         @NotNull(message = "가격은 비워둘 수 없습니다.")
-        @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
-        @Max(value = 2147483647, message = "가격은 2147483647원 이하이어야 합니다.")
+        @Min(value = 1, message = "가격은 1원 이상이어야 합니다.")
+        @Max(value = 200000000, message = "가격은 2억원 이하이어야 합니다.")
         Long price,
 
         @NotBlank(message = "이미지 URL은 비워둘 수 없습니다.")
