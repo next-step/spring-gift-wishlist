646587802febfe69b8ad686fe50fdbbed37cf7c0
Lee Si Woong <silverttthin@naver.com>
2025-07-03 15:50:56 +0900

    feat: 400번대 에러를 분기 - valid의 경우 깔끔한 디버깅 메시지를 위한 후처리가 있기 때문에 MethodArgumentNotValid 예외는 분기

--- src/main/java/gift/product/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/product/exception/GlobalExceptionHandler.java
@@ -8,19 +8,34 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
-// 400번대 모든 에러와 500번 에러에 대해 상태 코드와 디버깅 메시지을 전달하는 전역 핸들러
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 	public record ErrorResponse(int code, String message) {}
 
-	@ExceptionHandler({ MethodArgumentNotValidException.class, IllegalArgumentException.class })
-	public ResponseEntity<ErrorResponse> handleClientErrors(Exception e) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+	// @Valid 예외
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+		String debuggingMessage = e.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), debuggingMessage));
 	}
 
+	// 카카오 키워드 포함 예외
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+	}
+
+
+	// 서버 에러
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handlerServerErrors(Exception e) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+		return ResponseEntity.status(HttpStatus
+			.INTERNAL_SERVER_ERROR)
+			.body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
 	}
 }
