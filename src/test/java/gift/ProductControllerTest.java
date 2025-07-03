4ae9408faa538bdd2ddda23e7949c6ed2b9552cb
5seonjae <5seonjae.dev@gmail.com>
2025-07-03 14:30:31 +0900

    test: ProductControllerTest 기본 설정 추가
    - @AutoConfigureMockMvc, @SpringBootTest, @Transactional 적용
    - MockMvc, ObjectMapper, ProductRepository, ApprovedProductRepository 주입
    - ProductController 테스트 작성을 위한 뼈대 클래스 생성

---
+++ src/test/java/gift/ProductControllerTest.java
@@ -0,0 +1,29 @@
+package gift;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.repository.ApprovedProductRepository;
+import gift.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ApprovedProductRepository approvedProductRepository;
+
+}
