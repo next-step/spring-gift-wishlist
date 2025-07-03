af9602de74ce0d2a58293b45e9108255ef5940f2
doorcs <itsdoorcs@gmail.com>
2025-07-04 02:39:51 +0900

    feat: 관리자 페이지 예외 핸들링을 위한 뷰 구현

---
+++ src/main/java/gift/controller/AdminExceptionHandler.java
@@ -0,0 +1,44 @@
+package gift.controller;
+
+import java.util.List;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import gift.exception.ApprovalRequiredException;
+import gift.exception.ProductCreateException;
+import gift.exception.ProductNotFoundException;
+import gift.exception.ProductUpdateException;
+
+@ControllerAdvice(assignableTypes = AdminController.class)
+public class AdminExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e,
+        Model model
+    ) {
+        List<String> messages = e.getBindingResult().getFieldErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .toList();
+
+        model.addAttribute("errorTitle", "유효성 검사 실패");
+        model.addAttribute("errorMessages", messages);
+        return "error";
+    }
+
+    @ExceptionHandler({
+        ProductNotFoundException.class,
+        ProductCreateException.class,
+        ProductUpdateException.class,
+        ApprovalRequiredException.class
+    })
+    public String handleProductExceptions(RuntimeException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+}
