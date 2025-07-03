048356a22cfa4d903ae95ec1fe350042c8f30c25
milk-stone <ywshero@naver.com>
2025-07-03 17:58:06 +0900

    refactor: 3가지 변경 사항 적용
    1. IllegalArgumentException 핸들러 함수 추가
    
    2. ProblemDetail 대신 에러 메세지만 담는 SingleErrorResponse 레코드 클래스를 작성하여 사용
    
    3. Logger를 추가하여 spring console에 로그가 적히도록 설정

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package gift.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,8 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
         List<CustomFieldError> fieldErrors = e.getBindingResult().getFieldErrors().stream()
@@ -22,10 +25,17 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<SingleErrorResponse> handleIllegalArgumentExceptions(IllegalArgumentException e) {
+        log.warn("비즈니스 로직 유효성 검사 실패: {}", e.getMessage());
+        SingleErrorResponse errorResponse = new SingleErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ProblemDetail> handleExceptions(Exception e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        problemDetail.setTitle("INTERNAL SERVER ERROR");
-        return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<SingleErrorResponse> handleExceptions(Exception e) {
+        log.error("서버 내부에서 처리되지 않은 예외가 발생했습니다.", e);
+        SingleErrorResponse errorResponse = new SingleErrorResponse(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
