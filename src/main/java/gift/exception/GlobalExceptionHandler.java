cbcf2b94bca2b342106465e823c8a893a3e94747
milk-stone <ywshero@naver.com>
2025-07-03 17:27:10 +0900

    refactor: CustomFieldError, ErrorResponse record 클래스 분리, 예기치 못한 에러 처리를 위한 Exception.class 핸들러 함수 작성
    - 불 필요한 주석 삭제
    - 내부 코드 상에서 발생하는 에러 메세지를 담기 위한 ProblemDetail 클래스를 임포트함.

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package gift.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,22 +15,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
-
-        // 1. 예외 객체에서 모든 필드 에러 정보를 가져오기
         List<CustomFieldError> fieldErrors = e.getBindingResult().getFieldErrors().stream()
-                // 2. 각 FieldError를 우리가 정의한 CustomFieldError 객체로 변환.
                 .map(error -> new CustomFieldError(error.getField(), error.getDefaultMessage()))
-                // 3. List 형태로 수집.
                 .collect(Collectors.toList());
-
-        // 4. 최종 에러 응답 객체를 생성.
         ErrorResponse errorResponse = new ErrorResponse("입력 값에 대한 유효성 검사에 실패했습니다.", fieldErrors);
-
-        // 5. HTTP 400 (Bad Request) 상태 코드와 함께 에러 응답을 반환.
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    public record ErrorResponse(String message, List<CustomFieldError> errors) {}
-
-    public record CustomFieldError(String field, String defaultMessage) {}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleExceptions(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setTitle("INTERNAL SERVER ERROR");
+        return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
