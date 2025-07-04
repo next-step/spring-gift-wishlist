99028fef0523fd16850f08bd09156a74b4a80ec0
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-04 15:38:10 +0900

    feat: registerMember 작성

--- src/main/java/gift/repository/member/MemberRepositoryImpl.java
+++ src/main/java/gift/repository/member/MemberRepositoryImpl.java
@@ -3,7 +3,11 @@ package gift.repository.member;
 import gift.dto.api.member.MemberResponseDto;
 import gift.dto.api.product.ProductResponseDto;
 import gift.entity.Member;
+import gift.entity.Product;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -31,6 +35,28 @@ public class MemberRepositoryImpl implements MemberRepository {
     
     @Override
     public MemberResponseDto registerMember(Member newMember) {
-        return null;
+        var sql = """
+            insert into members(email, password, role)
+            values (:email, :password, :role);
+            """;
+        GeneratedKeyHolder generatedKey = new GeneratedKeyHolder();
+        
+        members.sql(sql)
+            .param("email", newMember.getEmail())
+            .param("password", newMember.getPassword())
+            .param("role", newMember.getRole().name())
+            .update(generatedKey);
+        
+        long recentKey = generatedKey.getKey().longValue();
+        
+        String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+        String accessToken = Jwts.builder()
+            .subject(Long.toString(recentKey))
+            .claim("email", newMember.getEmail())
+            .claim("role", newMember.getRole())
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+            .compact();
+        
+        return new MemberResponseDto(accessToken);
     }
 }
