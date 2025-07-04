1214f499ba35778560411e0c266f0a209a2246d4
sweet-mine <dltlgos@naver.com>
2025-07-04 16:22:48 +0900

    코드 피드백 반영 상품 수정 할 때도 금지 상품명 검사하도록 수정
    예외처리 핸들러 주석 업데이트
    
    Service 레이어에 하드 코딩돼있던 금지 단어를 application.properties에 정의, 이에 따른 application.properties 인코딩 방식 UTF-8로 변경

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 # 유효성 검사 및 예외 처리
 1. RequestDto에 글자수 제한, 특수문자 제한 유효성 검사 추가 , Service 레이어에서 제품명에 특정 문구 포함 여부 검사 추가
 2. README.md 업데이트
+3. 코드 피드백 반영
+
+상품 수정 할 때도 금지 상품명 검사하도록 수정
+
+예외처리 핸들러 주석 업데이트
+
+Service 레이어에 하드 코딩돼있던 금지 단어를 application.properties에 정의, 이에 따른 application.properties 인코딩 방식 UTF-8로 변경
--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -9,24 +9,24 @@ import org.springframework.web.bind.annotation.*;
 // 예외 관리 핸들러
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // 자체 예외 및 queryForObject 예외
+    // NOT_FOUND 응답하는 예외처리 핸들러
     @ExceptionHandler({ProductNotFoundException.class, EmptyResultDataAccessException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFound(RuntimeException e) {
         return e.getMessage();
     }
 
-    // queryForObject로 2개 이상의 제품을 찾은 경우 예외
+    // INTERNAL_SERVER_ERROR 응답하는 예외처리 핸들러
     @ExceptionHandler(IncorrectResultSizeDataAccessException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleInternalServerError(RuntimeException e) { return e.getMessage(); }
 
-    // MD가 아닌데 특정 문구를 포함한 제품명을 입력한 경우
+    // FORBIDDEN 응답하는 예외처리 핸들러
     @ExceptionHandler(InvalidProductNameException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public String handleForbidden(RuntimeException e) { return e.getMessage(); }
 
-    // ProductRequestDto에 지정된 Validation 검사에서 예외가 발생한 경우
+    // BAD_REQUEST 응답하는 예외처리 핸들러
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleBadRequest(MethodArgumentNotValidException e) { return e.getBindingResult().getAllErrors().getFirst().getDefaultMessage(); }
