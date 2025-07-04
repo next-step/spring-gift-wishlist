9292df9497990d757a1f96ff25520a29755d877d
thdgmlwns1 <xhrl233@naver.com>
2025-07-04 19:40:55 +0900

    Feat: 예외처리

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -4,8 +4,11 @@ import gift.config.JwtProvider;
 import gift.dto.request.MemberRequsetDto;
 import gift.dto.response.TokenResponseDto;
 import gift.entity.Member;
+import gift.exception.ForbiddenException;
 import gift.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+
+import java.awt.*;
 
 @Service
 public class MemberService {
@@ -20,6 +23,11 @@ public class MemberService {
 
 
     public TokenResponseDto register(MemberRequsetDto dto) {
+
+        if (memberRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new IllegalStateException("이미 사용 중인 이메일입니다.");
+        }
+
         Member member = new Member(null, dto.getEmail(), dto.getPassword());
         memberRepository.save(member);
 
@@ -34,10 +42,10 @@ public class MemberService {
 
     public TokenResponseDto login(MemberRequsetDto dto) {
         Member saved = memberRepository.findByEmail(dto.getEmail())
-                .orElseThrow(() -> new IllegalStateException("가입 되지 않은 회원 입니다."));
+                .orElseThrow(() -> new ForbiddenException("가입 되지 않은 회원 입니다."));
 
         if (!saved.getPassword().equals(dto.getPassword())) {
-            throw new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다.");
+            throw new ForbiddenException("비밀번호가 올바르지 않습니다.");
         }
 
         String token = jwtProvider.generateToken(saved);
