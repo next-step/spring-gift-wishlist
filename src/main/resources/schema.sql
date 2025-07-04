786fbc84b58a403bb88906b283fa4db498bbe243
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-04 15:30:42 +0900

    fix:  name_permission 컬럼 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,5 @@ CREATE TABLE product (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(100) NOT NULL,
      price INT NOT NULL,
-     image_url VARCHAR(255),
-     name_permission Boolean DEFAULT FALSE
+     image_url VARCHAR(255)
 );
