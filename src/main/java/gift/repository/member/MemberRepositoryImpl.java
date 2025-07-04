8a67380a618bde2a25e59d160766b6ae6897a64c
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-04 14:40:29 +0900

    feat: db 설정

--- src/main/java/gift/repository/member/MemberRepositoryImpl.java
+++ src/main/java/gift/repository/member/MemberRepositoryImpl.java
@@ -2,10 +2,17 @@ package gift.repository.member;
 
 import gift.dto.api.member.MemberResponseDto;
 import gift.entity.Member;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberRepositoryImpl implements MemberRepository {
+    
+    private final JdbcClient members;
+    
+    public MemberRepositoryImpl(JdbcClient members) {
+        this.members = members;
+    }
     
     @Override
     public boolean alreadyRegitered(String email) {
