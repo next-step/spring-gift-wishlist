e58100fa659276cec8f4ba2d6d750243b4a8aadf
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-04 15:30:08 +0900

    refactor: pathvariable로 id값을 받고 있으니 필드 삭제

--- src/main/java/gift/product/dto/ProductRequestDto.java
+++ src/main/java/gift/product/dto/ProductRequestDto.java
@@ -1,17 +1,18 @@
 package gift.product.dto;
 
 import gift.product.entity.Product;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class ProductRequestDto {
 
-    private Long id;
     @Size(min = 1 , max = 15,message = "이름은 1~15자 여야합니다")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$", message = "특수문자는 (), [], +, -, &, /, _ 만 허용됩니다.")
     private String name;
 
+    @NotBlank
     @NotNull(message = "비울 수 없습니다")
     private Integer price;
     private String imageUrl;
@@ -20,16 +21,11 @@ public class ProductRequestDto {
 
         ProductRequestDto dto = new ProductRequestDto();
 
-        dto.id = product.getId();
         dto.name = product.getName();
         dto.price = product.getPrice();
         dto.imageUrl = product.getImageUrl();
 
         return dto;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
