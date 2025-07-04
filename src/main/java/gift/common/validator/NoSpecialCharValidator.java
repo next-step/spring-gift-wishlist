d91674f0785177192b7f857cdd03acdca3c15251
oh010 <oh010224@gmail.com>
2025-07-04 15:41:23 +0900

    fix: 이름에 숫자 포함 가능하도록 변경

--- src/main/java/gift/common/validator/NoSpecialCharValidator.java
+++ src/main/java/gift/common/validator/NoSpecialCharValidator.java
@@ -7,7 +7,7 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
 public class NoSpecialCharValidator implements ConstraintValidator<NoSpecialChar, String> {
-    private static final Pattern SPECIAL_CHAR_PATTERN = Pattern.compile("^[a-zA-Z가-힣()\\[\\]\\+\\-&/_ ]*$");
+    private static final Pattern SPECIAL_CHAR_PATTERN = Pattern.compile("^[a-zA-Z가-힣0-9()\\[\\]\\+\\-&/_ ]*$");
 
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return value == null || SPECIAL_CHAR_PATTERN.matcher(value).find();
