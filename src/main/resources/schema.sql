eb731df6843ea1f1a7860bf975ebda7b53157bc2
YoonGangHoon <yghun021007@naver.com>
2025-07-04 16:36:29 +0900

    chore: members 테이블 생성 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,10 @@ create table products (
     price int,
     imageUrl varchar(255)
 );
+
+create table members (
+    id bigint auto_increment primary key,
+    name varchar(100),
+    email varchar(100),
+    password varchar(100)
+);
