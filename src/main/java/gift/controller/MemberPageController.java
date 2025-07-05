35dc177dcafac1f1e87beed39a7b7511b1fa7735
HyunJunCho00 <catlove5478@naver.com>
2025-07-05 13:11:01 +0900

    feat: 회원 관련 프론트엔드 페이지 및 컨트롤러 추가

---
+++ src/main/java/gift/controller/MemberPageController.java
@@ -0,0 +1,28 @@
+package gift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberPageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "member/login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "member/register";
+    }
+
+    @GetMapping("/myinfo")
+    public String myInfoPage() {
+        return "member/myinfo";
+    }
+}
