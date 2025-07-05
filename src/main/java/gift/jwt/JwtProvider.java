0c6db1498daddd3ffdcc24256f6ee301d5c5fda9
YoonGangHoon <yghun021007@naver.com>
2025-07-05 16:54:59 +0900

    feat: JWT에서 Claim 추출

--- src/main/java/gift/jwt/JwtProvider.java
+++ src/main/java/gift/jwt/JwtProvider.java
@@ -1,19 +1,40 @@
 package gift.jwt;
 
 import gift.entity.Member;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtProvider {
+    @Value("${jwt.secret.key}")
+    private String secretKeyString;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(Member member) {
-        String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
         return Jwts.builder()
-                .setSubject(member.getId().toString())
-                .claim("email", member.getName())
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .subject(member.getId().toString())
+                .signWith(key)
                 .compact();
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
