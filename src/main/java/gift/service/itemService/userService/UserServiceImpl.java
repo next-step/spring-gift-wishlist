15478a809ca60d3834bfb05dda790223380e40dc
xodbs126 <dltvmsskd@naver.com>
2025-07-05 13:28:53 +0900

    feat(service):registerUser 생성

--- src/main/java/gift/service/itemService/userService/UserServiceImpl.java
+++ src/main/java/gift/service/itemService/userService/UserServiceImpl.java
@@ -2,13 +2,27 @@ package gift.service.itemService.userService;
 
 import gift.dto.itemDto.userDto.UserRegisterDto;
 import gift.dto.itemDto.userDto.UserResponseDto;
+import gift.entity.User;
+import gift.repository.itemRepository.userRepository.UserRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
     @Override
     public UserResponseDto registerUser(UserRegisterDto dto) {
+        User user = new User(dto.email(), dto.password());
 
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDto(savedUser.email(),savedUser.password());
     }
 }
