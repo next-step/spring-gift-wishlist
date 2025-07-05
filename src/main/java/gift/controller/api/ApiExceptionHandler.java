219c2f43047fa03e2f4e01f03f309086156dc35f
HyunJunCho00 <catlove5478@naver.com>
2025-07-05 13:21:23 +0900

    refactor: ApiExceptionHandler가 패키지 기준으로 동작하도록 개선

--- src/main/java/gift/controller/api/ApiExceptionHandler.java
+++ src/main/java/gift/controller/api/ApiExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@RestControllerAdvice(assignableTypes = MemberApiController.class)
+@RestControllerAdvice("gift.controller.api")
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
