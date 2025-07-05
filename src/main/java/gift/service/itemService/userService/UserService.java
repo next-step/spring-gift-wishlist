0e1d03380531f33142bd52970ddab74c8b24e12b
xodbs126 <dltvmsskd@naver.com>
2025-07-05 14:07:27 +0900

    feat(service):getUserList 생성

--- src/main/java/gift/service/itemService/userService/UserService.java
+++ src/main/java/gift/service/itemService/userService/UserService.java
@@ -3,6 +3,10 @@ package gift.service.itemService.userService;
 import gift.dto.itemDto.userDto.UserRegisterDto;
 import gift.dto.itemDto.userDto.UserResponseDto;
 
+import java.util.List;
+
 public interface UserService {
     UserResponseDto registerUser(UserRegisterDto dto);
+
+    List<UserResponseDto> getUserList(String email);
 }
--- src/main/java/gift/service/itemService/userService/UserServiceImpl.java
+++ src/main/java/gift/service/itemService/userService/UserServiceImpl.java
@@ -6,6 +6,9 @@ import gift.entity.User;
 import gift.repository.itemRepository.userRepository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -24,5 +27,21 @@ public class UserServiceImpl implements UserService{
         User savedUser = userRepository.save(user);
 
         return new UserResponseDto(savedUser.email(),savedUser.password());
+    }
+
+    @Override
+    public List<UserResponseDto> getUserList(String email) {
+        List<User> users;
+        List<UserResponseDto> result = new ArrayList<>();
+        if (email == null) {
+            users = userRepository.getAllUsers();
+        }else
+            users = userRepository.findUserByEmail(email);
+
+        for (User user : users) {
+            result.add(new UserResponseDto(user.email(),user.password()));
+        }
+
+        return result;
     }
 }
