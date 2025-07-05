36bf6bb07c49a51354926f4c94bde5e974781bae
xqqldir <2jihye10@kangwon.ac.kr>
2025-07-06 02:49:34 +0900

    [Feat] 특수문자 제한

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -3,11 +3,13 @@ package gift.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 
 public record ProductRequestDto(
         @NotBlank(message = "상품 이름은 필수입니다.")
         @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15글자까지 입력할 수 있습니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣ㄱ-ㅎ\\s()\\[\\]+\\-&/_]*$", message = "사용할 수 없는 특수문자가 포함되어 있습니다. ( ), [ ], +, -, &, /, _ 만 사용 가능합니다.")
         String name,
 
         Long price,
