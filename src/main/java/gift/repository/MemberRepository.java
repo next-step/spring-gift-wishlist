157fb85ddff1e367fa61690f7e43926daeca44d1
5seonjae <5seonjae.dev@gmail.com>
2025-07-05 17:59:27 +0900

    feat: MemberRepository에 회원 저장(save) 기능 구현
    - `save(Member)` 메서드 추가
    - SQL `INSERT INTO member (email, password)` 쿼리 실행
    - `GeneratedKeyHolder` 이용해 자동 생성된 `id` 조회 및 설정
    - 저장된 정보를 포함해 새로운 `Member` 객체 반환

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -1,13 +1,32 @@
 package gift.repository;
 
+import gift.entity.Member;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberRepository {
 
     private final JdbcClient jdbcClient;
 
     public MemberRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
+    }
+
+    // 회원 저장
+    public Member save(Member member) {
+        String sql = "INSERT INTO member (email, password) VALUES (?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcClient
+            .sql(sql)
+            .param(member.getEmail())
+            .param(member.getPassword())
+            .update(keyHolder, "id");
+
+        Long id = keyHolder.getKey().longValue();
+        return new Member(id, member.getEmail(), member.getPassword());
     }
 
 }
