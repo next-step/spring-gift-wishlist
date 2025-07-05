b81219ecacd093e8ffde173349bad5f943cac441
ji-mim <194115@jnu.ac.kr>
2025-07-05 18:42:25 +0900

    Refactor(BeanValidator): 애노테이션 이름 변경

--- src/main/java/gift/dto/UpdateProductRequest.java
+++ src/main/java/gift/dto/UpdateProductRequest.java
@@ -1,6 +1,6 @@
 package gift.dto;
 
-import gift.config.HaveKakao;
+import gift.config.NotHaveValue;
 import gift.config.SpecialChar;
 import jakarta.validation.constraints.*;
 import jakarta.validation.constraints.NotBlank;
@@ -12,7 +12,7 @@ public record UpdateProductRequest (
         @NotBlank(message = "상품명은 필수입니다.")
         @Size(max = 15, message = "15자를 넘을 수 없습니다.")
         @SpecialChar(message = "(), [], +, -, &, /, _ 이외의 특수문자는 사용할 수 없습니다")
-        @HaveKakao(message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.",value = "카카오")
+        @NotHaveValue(message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.",value = "카카오")
         String name,
         @Positive(message = "가격은 0보다 커야합니다.")
         @NotNull(message = "가격은 필수입니다.")
