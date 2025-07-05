49279f025b32ddc8ea7c4f8d4294d15006c3914b
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-05 17:01:03 +0900

    feat : 카카오 금지어용 validator

---
+++ src/main/java/gift/validation/FobiddenWordKakao.java
@@ -0,0 +1,16 @@
+package gift.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ForbiddenWordKakaoValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ForbiddenWordKakao {
+    String message() default "\"카카오\" 단어는 사용할 수 없습니다.";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+}
