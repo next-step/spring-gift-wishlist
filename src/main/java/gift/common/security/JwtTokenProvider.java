0ccf0ad538156246ea4e2cd9a03681892a81ef52
treasure-sky <ych0486666@gmail.com>
2025-07-07 01:02:32 +0900

    feat: JWT 유틸 구현
    - JWT를 처리하기 위해 필요한 필수적인 메서드(토큰 생성, 검증, 정보 추출, 만료 체크)를 구현하였음.

---
+++ src/main/java/gift/common/security/JwtTokenProvider.java
@@ -0,0 +1,87 @@
+package gift.common.security;
+
+import gift.member.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.time.Instant;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey secretKey;
+
+    private final long accessTokenValidityTime;
+
+    public JwtTokenProvider(
+        @Value("${jwt.secret}") String secretKey,
+        @Value("${jwt.access-token-validity}") long accessTokenValidityTime) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.accessTokenValidityTime = accessTokenValidityTime;
+    }
+
+    /**
+     * 토큰 생성 메서드
+     *
+     * @param member 멤버 도메인 객체
+     * @return JWT 문자열
+     */
+    public String generateToken(Member member) {
+        return Jwts.builder()
+            .claim("id", member.getId().toString())
+            .claim("name", member.getName())
+            .claim("email", member.getEmail())
+            .expiration(Date.from(Instant.now().plusSeconds(accessTokenValidityTime)))
+            .signWith(secretKey)
+            .compact();
+    }
+
+    /**
+     * JWT 검증 메서드
+     *
+     * @param token 검증할 토큰
+     * @return 검증 성공 여부
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 토큰 정보 추출 메서드
+     *
+     * @param token 추출할 토큰
+     * @return 토큰에 담긴 정보 Map
+     */
+    public Claims parseToken(String token) {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+
+    /**
+     * 토큰의 만료 여부 체크 메서드
+     *
+     * @param token 만료 여부를 체크할 토큰
+     * @return 만료 여부 (만료시 true)
+     */
+    public boolean isTokenExpired(String token) {
+        return parseToken(token).getExpiration().before(new Date());
+    }
+
+}
