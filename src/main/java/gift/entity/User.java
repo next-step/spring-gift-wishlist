c9c0fbbf464899534c203eb6c05f2f202ef633c1
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-06 23:28:52 +0900

    Feat: User Entity 추가 ( 이메일, 비밀번호 )

---
+++ src/main/java/gift/entity/User.java
@@ -0,0 +1,33 @@
+package gift.entity;
+
+public class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean validatePassword(String password){
+        return this.password.equals(password);
+    }
+}
