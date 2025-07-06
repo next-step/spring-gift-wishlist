0800ac688c365d7126c2bde33981b58a0671d504
treasure-sky <ych0486666@gmail.com>
2025-07-06 21:32:16 +0900

    feat: member 스키마 정의

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE ITEM
     image_url   VARCHAR(255)    NOT NULL,
     Primary Key (id)
 );
+
+CREATE TABLE MEMBER
+(
+    id          BIGINT          NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(20)     NOT NULL,
+    email       VARCHAR(50)     NOT NULL,
+    password    VARCHAR(255)    NOT NULL,
+    PRIMARY KEY (id)
+)
