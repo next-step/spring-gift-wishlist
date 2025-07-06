2b2428d6d926733c66f3b2ff712d20d5fc311a18
YoonGangHoon <yghun021007@naver.com>
2025-07-06 15:37:56 +0900

    feat: 로그인 테스트 코드 추가

--- src/test/java/gift/MemberE2ETest.java
+++ src/test/java/gift/MemberE2ETest.java
@@ -1,5 +1,7 @@
 package gift;
 
+import gift.dto.MemberLoginRequestDto;
+import gift.dto.MemberLoginResponseDto;
 import gift.dto.MemberRequestDto;
 import gift.dto.MemberResponseDto;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,6 +46,37 @@ public class MemberE2ETest {
         assertThat(response.name()).isEqualTo(name);
         assertThat(response.email()).isEqualTo(email);
         assertThat(response.password()).isEqualTo(password);
+    }
+
+    @Test
+    void 로그인_성공시_JWT_토큰_반환() {
+        // given
+        final String name = "홍길동";
+        final String email = "honggildong@email.com";
+        final String password = "password";
+
+        MemberRequestDto joinRequest = new MemberRequestDto(null, name, email, password);
+
+        MemberResponseDto joinResponse = restClient.post()
+                .uri("/api/members/register")
+                .body(joinRequest)
+                .retrieve()
+                .body(MemberResponseDto.class);
+
+        assertThat(joinResponse.id()).isNotNull();
+
+        // when
+        MemberLoginRequestDto loginRequest = new MemberLoginRequestDto(email, password);
+
+        MemberLoginResponseDto loginResponse = restClient.post()
+                .uri("/api/members/login")
+                .body(loginRequest)
+                .retrieve()
+                .body(MemberLoginResponseDto.class);
+
+        // then
+        assertThat(loginResponse).isNotNull();
+        assertThat(loginResponse.token()).isNotBlank();
     }
 
 }
