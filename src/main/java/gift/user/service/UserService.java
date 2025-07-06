f6720005a64099a094583ee6496bc3694c961235
Dohyun <sks020k@naver.com>
2025-07-07 03:09:14 +0900

    feat : 회원가입 시 token 생성

--- src/main/java/gift/user/service/UserService.java
+++ src/main/java/gift/user/service/UserService.java
@@ -1,5 +1,6 @@
 package gift.user.service;
 
+import gift.user.JwtTokenProvider;
 import gift.user.dto.LoginRequestDto;
 import gift.user.dto.LoginResponseDto;
 import gift.user.dto.RegisterRequestDto;
@@ -10,18 +11,21 @@ import gift.user.repository.UserRepository;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final JwtTokenProvider jwtTokenProvider;
 
-  public UserService(UserRepository userRepository) {
+  public UserService(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
     this.userRepository = userRepository;
+    this.jwtTokenProvider = jwtTokenProvider;
   }
 
   public RegisterResponseDto registerUser(RegisterRequestDto registerRequestDto) {
         User user = userRepository.saveUser(registerRequestDto.email(),registerRequestDto.password());
+
+        String token = jwtTokenProvider.generateToken(user);
+
         return RegisterResponseDto.from(user);
   }
 
   public LoginResponseDto loginUser(LoginRequestDto loginRequestDto) {
     return new LoginResponseDto(loginRequestDto);
   }
-
-}
