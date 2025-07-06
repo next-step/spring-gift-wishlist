520297b3d6da0293fd148b6b561d0620fedc75ef
treasure-sky <ych0486666@gmail.com>
2025-07-06 16:52:09 +0900

    refactor: User -> Member 도메인 이름 변경
    - user의 경우 DB에서 예약어로 사용되므로, 에러를 방지하고자 member로 변경하였음.

--- src/main/java/gift/member/Member.java
+++ src/main/java/gift/member/Member.java
@@ -1,20 +1,20 @@
-package gift.user;
+package gift.member;
 
-public class User {
+public class Member {
 
     private Long id;
     private String name;
     private String email;
     private String password;
 
-    public User(Long id, String name, String email, String password) {
+    public Member(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public User(String name, String email, String password) {
+    public Member(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
