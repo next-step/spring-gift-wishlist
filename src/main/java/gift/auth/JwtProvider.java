5e480a468e3de262e444746dc7ce0b3c3339336b
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-06 12:20:57 +0900

    feat: jwt provider 작성

--- src/main/java/gift/auth/JwtProvider.java
+++ src/main/java/gift/auth/JwtProvider.java
@@ -1,9 +1,23 @@
 package gift.auth;
 
+import gift.entity.Member;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtProvider {
     
     @Value(value = "${jwt.secret}")
     private String secretKey;
+    
+    public String createToken(Member member) {
+        return Jwts.builder()
+            .subject(Long.toString(member.getId()))
+            .claim("email", member.getEmail())
+            .claim("role", member.getRole())
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+            .compact();
+    }
 }
