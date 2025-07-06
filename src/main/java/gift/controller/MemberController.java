a471858100e078aa68b6cd909626af3b898d1b47
seongwon02 <seongwon2303@gmail.com>
2025-07-07 01:52:01 +0900

    feat: 멤버 컨트롤러 구현

---
+++ src/main/java/gift/controller/MemberController.java
@@ -0,0 +1,78 @@
+package gift.controller;
+
+import gift.dto.*;
+import gift.service.MemberService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 회원가입
+    @PostMapping("/register")
+    public ResponseEntity<TokenResponseDto> registerMember (
+            @Valid @RequestBody MemberRequestDto dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(memberService.registerMember(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> logInMember (
+            @Valid @RequestBody MemberRequestDto dto) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(memberService.logInMember(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberResponseDto>> findAllMembers() {
+        List<MemberResponseDto> memberList = memberService.findAllMembers();
+
+        return ResponseEntity.ok(memberList);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<MemberResponseDto> findMember(@PathVariable Long id) {
+
+        MemberResponseDto dto = memberService.findMemberById(id);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberResponseDto> createMember(
+            @Valid @RequestBody MemberRequestDto dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(memberService.saveMember(dto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<MemberResponseDto> updateMember(
+            @Valid @RequestBody RoleRequestDto dto,
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(memberService.updateMember(id, dto));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
