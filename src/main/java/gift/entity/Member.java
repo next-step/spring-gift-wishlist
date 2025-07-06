8cc90043281be75e5f435ad6baaf43c39cac0921
hopencode <newalfo1006@gmail.com>
2025-07-06 23:46:40 +0900

    fix: Member 엔티티에 id 속성 추가

--- src/main/java/gift/entity/Member.java
+++ src/main/java/gift/entity/Member.java
@@ -1,12 +1,17 @@
 package gift.entity;
 
 public class Member {
+    private Long id;
     private String email;
     private String password;
 
     public Member(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    Long getId() {
+        return id;
     }
 
     String getEmail() {
