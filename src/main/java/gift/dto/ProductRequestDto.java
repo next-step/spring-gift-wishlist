4988405b96428b6fe0d99446f1922a9383d190d5
hhseo9519 <hhseo9519@naver.com>
2025-07-06 10:59:59 +0900

    feat : 상품명에 한 글자 이상의 문자가 들어가도록 변경

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -3,12 +3,13 @@ package gift.dto;
 import jakarta.validation.constraints.*;
 public class ProductRequestDto {
     @NotBlank(message = "상품 이름은 필수 입력 사항입니다.")
-    @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Size(max = 15, message = "상품 이름은 공백 포함 최대 15자까지 가능합니다.")
     @Pattern(
-            regexp = "^[\\p{L}\\p{N} ()\\[\\]+\\-\\&/_]*$",
-            message = "상품 이름에는 허용되지 않은 특수문자가 포함되어 있습니다."
+            regexp = "^(?=.*\\p{L})[\\p{L}\\p{N} ()\\[\\]+\\-\\&/_]{1,15}$",
+            message = "상품명은 반드시 최소 한 글자 이상의 문자를 포함해야 하며, 허용되지 않은 특수문자는 사용할 수 없습니다."
     )
     private String name;
+
 
     @Min(value = 0, message = "상품 가격은 0원 이상이어야 합니다.")
     private int price;
