1eb80e27a1023770ba13a5fbbf1c105fa78e9965
YoonGangHoon <yghun021007@naver.com>
2025-07-06 16:34:29 +0900

    feat: 회원 생성 기능 및 화면 추가

--- src/main/java/gift/controller/MemberViewController.java
+++ src/main/java/gift/controller/MemberViewController.java
@@ -1,8 +1,13 @@
 package gift.controller;
 
+import gift.dto.MemberRequestDto;
 import gift.service.MemberService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +28,28 @@ public class MemberViewController {
         Map<String, Object> model = new HashMap<>();
         model.put("members", memberService.findAll());
         return new ModelAndView("member/list", model);
+    }
+
+    @GetMapping("/new")
+    public ModelAndView createForm() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("member", new MemberRequestDto(null, null, null, null));
+        return new ModelAndView("member/create", model);
+    }
+
+    @PostMapping
+    public ModelAndView create(
+            @Valid @ModelAttribute("member") MemberRequestDto requestDto,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            ModelAndView mav = new ModelAndView("member/create");
+            mav.addObject("member", requestDto);
+            return mav;
+        }
+
+        memberService.create(requestDto);
+        return new ModelAndView("redirect:/members");
     }
 
 }
