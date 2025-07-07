0f7454e24e7bb365ad17cefb9b9a727f88dbd3a2
sinsehwan <sk124590@gmail.com>
2025-07-07 17:45:40 +0900

    feat(config, auth): webconfig & argumentresolver impl

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,41 @@
+package gift.config;
+
+import gift.auth.LoginInterceptor;
+import gift.auth.LoginArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginInterceptor loginInterceptor;
+    private final LoginArgumentResolver loginArgumentResolver;
+
+    public WebConfig(LoginInterceptor loginInterceptor, LoginArgumentResolver loginArgumentResolver) {
+        this.loginInterceptor = loginInterceptor;
+        this.loginArgumentResolver = loginArgumentResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/api/members/register",
+                        "/api/members/login",
+                        "/h2-console/**",
+                        "/admin/**", // 추후 제거 필요.
+                        "/api/products/**" // 추후 제거 필요.
+                );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
+    }
+
+}
