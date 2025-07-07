c509ce75b52370ac944200ce477839734b518505
리지혜 <2jihye10@gmail.com>
2025-07-07 16:52:59 +0900

    [Feat] MemberService 구현

--- src/main/java/gift/auth/JwtTokenProvider.java
+++ src/main/java/gift/auth/JwtTokenProvider.java
@@ -1,17 +1,21 @@
 package gift.auth;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Component
 public class JwtTokenProvider {
 
     private final SecretKey secretKey;
     private final long validityInMilliseconds;
+
 
     public JwtTokenProvider(
             @Value("${jwt.secret}") String secretKey,
