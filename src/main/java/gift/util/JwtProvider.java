e39d93f66e224a0b67ae542554be6b355136ba87
KWH <kmwh030@gmail.com>
2025-07-07 21:17:10 +0900

    refactor: jwt raw-secret에 @Value 적용

--- src/main/java/gift/util/JwtProvider.java
+++ src/main/java/gift/util/JwtProvider.java
@@ -4,19 +4,22 @@ import gift.entity.Member;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 
 @Component
 public class JwtProvider {
-    private final String RAW_SECRET = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    @Value("${jwt.secret}")
+    private String rawSecret;
+
     private SecretKey key;
 
     @PostConstruct
     void init() {
         // 문자열 → SecretKey 로 변환
-        this.key = Keys.hmacShaKeyFor(RAW_SECRET.getBytes());
+        this.key = Keys.hmacShaKeyFor(rawSecret.getBytes());
     }
 
     public String createToken(Member member) {
