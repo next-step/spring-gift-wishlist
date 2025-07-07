b583165d32cc88dc8e5f0c2debaf1055f372d344
thstkddnr20 <thstkddnr20@gmail.com>
2025-07-07 12:12:37 +0900

    test: UserApiControllerTest e2e 테스트 작성

--- src/test/java/gift/controller/ProductApiControllerTest.java
+++ src/test/java/gift/controller/ProductApiControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Sql(scripts = "/cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @Sql(scripts = "/dropTable.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
 class ProductApiControllerTest {
