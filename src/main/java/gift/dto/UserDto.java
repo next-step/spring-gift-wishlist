cc2b59b14a5366522bdc8b5f061bce68d2c06892
uvrvuoon <0524yar@gmail.com>
2025-07-07 16:15:24 +0900

    feat: UserDto 클래스 생성

---
+++ src/main/java/gift/dto/UserDto.java
@@ -0,0 +1,18 @@
+package gift.dto;
+
+public class UserDto {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    public Long getId() {return id;}
+
+    public String getEmail() {return email;}
+
+    public String getPassword() {return password;}
+
+    public void setId(Long id) {this.id = id;}
+}
