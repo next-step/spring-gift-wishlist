690ac6db05e57aabda032e318911673688e6026d
KIYEONG <rldud1237@naver.com>
2025-07-07 14:41:41 +0900

    feat: 회원(Member) 도메인 및 Repository 구현

---
+++ src/main/java/gift/entity/Member.java
@@ -0,0 +1,29 @@
+package gift.entity;
+
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+    private Role role;
+
+    public Member(Long id, String email, String password, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public Role getRole() {
+        return role;
+    }
+
+}
