011f355fa4b6d61f6f8ddeb0bc4f8fd3f07fd156
sinsehwan <sk124590@gmail.com>
2025-07-07 16:17:55 +0900

    fix(member): fix member schema, repository grammar

--- src/main/java/gift/member/controller/MemberController.java
+++ src/main/java/gift/member/controller/MemberController.java
@@ -6,7 +6,6 @@ import gift.member.dto.MemberLoginResponse;
 import gift.member.dto.MemberRegisterRequest;
 import gift.member.service.MemberService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
