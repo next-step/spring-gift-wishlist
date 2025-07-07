0a58fa7902e73dae227125e9bc7400966c918875
kimpro4214 <qnfghk555@naver.com>
2025-07-07 16:14:01 +0900

    feat: MemberController에 로그인 API 엔드포인트 추가

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
@@ -20,5 +23,13 @@ public class MemberController {
     public ResponseEntity<String> register(@RequestBody MemberRequestDto dto) {
         memberService.register(dto);
         return new ResponseEntity<>("회원가입 성공", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody MemberRequestDto dto) {
+        String token = memberService.login(dto);
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
