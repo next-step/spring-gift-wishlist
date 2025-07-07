e199e51e9c9378a8574ca62f88f7f10251c92fdb
Junyoung Yang <y@jun0.dev>
2025-07-07 17:37:03 +0900

    refactor: 엔티티 단에서 검증 추가

--- src/main/java/gift/member/entity/Member.java
+++ src/main/java/gift/member/entity/Member.java
@@ -1,17 +1,32 @@
 package gift.member.entity;
 
 import gift.member.dto.MemberRegisterRequestDto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 public class Member {
 
+    @NotNull
     private UUID uuid;
+    
+    @NotBlank
+    @Email
     private String email;
+    
+    @NotBlank
     private String password;
+    
+    @NotBlank
     private String name;
+    
+    @NotNull
     private LocalDateTime createdAt;
+    
+    @NotNull
     private LocalDateTime updatedAt;
 
     private static final Argon2PasswordEncoder PasswordEncoder =
--- src/main/java/gift/token/entity/RefreshToken.java
+++ src/main/java/gift/token/entity/RefreshToken.java
@@ -1,5 +1,7 @@
 package gift.token.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -8,9 +10,16 @@ public class RefreshToken {
 
     public static final Duration TTL = Duration.ofDays(365);
 
+    @NotBlank
     private final String token;
+    
+    @NotNull
     private final UUID memberUuid;
+    
+    @NotNull
     private final LocalDateTime createdAt;
+    
+    @NotNull
     private final LocalDateTime expirationDate;
 
     public RefreshToken(UUID memberUuid) {
