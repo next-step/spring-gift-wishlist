49bbdd14238a227f06eadbc2dec4336518f4aaa2
hydrationn <suhwaparkk1031@gmail.com>
2025-07-07 18:08:37 +0900

    feat: member 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,15 @@
-create table product
+CREATE TABLE product
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price BIGINT NOT NULL,
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(100) NOT NULL,
+    price     BIGINT       NOT NULL,
     image_url VARCHAR(255)
+);
+
+CREATE TABLE member
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(50)  NOT NULL
 );
