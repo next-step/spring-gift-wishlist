c1ac52ffbbf4d80d7a59dffa8f3893c4bfdc940e
Hyun <hyun_7586@naver.com>
2025-07-07 17:20:35 +0900

    refactor: 토큰/로그인 시도 요청 예외의 에러코드 수정

--- src/main/java/gift/global/exception/ErrorCode.java
+++ src/main/java/gift/global/exception/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     INVALID_PRODUCT_UPDATE_REQUEST(HttpStatus.BAD_REQUEST, "수정 요청 객체를 찾을 수 없습니다."),
     INVALID_KAKAO_NAME(HttpStatus.BAD_REQUEST, "'카카오'가 들어간 상품명은 관리자에게 문의해 주세요."),
     INVALID_FORM_REQUEST(HttpStatus.BAD_REQUEST, "허용되지 않은 값을 입력하셨습니다."),
-    INVALID_TOKEN_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 형식의 토큰입니다."),
-    INCORRECT_LOGIN_INFO(HttpStatus.BAD_REQUEST, "이메일, 비밀번호 조합이 잘못되었습니다."),
+    INVALID_TOKEN_REQUEST(HttpStatus.UNAUTHORIZED, "잘못된 형식의 토큰입니다."),
+    INCORRECT_LOGIN_INFO(HttpStatus.FORBIDDEN, "이메일, 비밀번호 조합이 잘못되었습니다."),
     OTHERS(HttpStatus.INTERNAL_SERVER_ERROR, "기타 오류입니다.");
 
 
