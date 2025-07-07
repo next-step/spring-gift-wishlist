8842a7b7986c4ceccc031affa64e08c0fff46ee2
kimpro4214 <qnfghk555@naver.com>
2025-07-07 16:19:24 +0900

    fix: 로그인 허용

--- src/main/java/gift/config/SecurityConfig.java
+++ src/main/java/gift/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig {
         http
                 .csrf().disable() // CSRF 비활성화 (POST 테스트용)
                 .authorizeHttpRequests()
-                .requestMatchers("/api/members/register").permitAll() // 회원가입은 누구나 접근 허용
-                .anyRequest().authenticated() // 그 외는 인증 필요
+                .requestMatchers("/api/members/register", "/api/members/login").permitAll() // 회원가입, 로그인 허용
+                .anyRequest().authenticated() // 나머지는 인증 필요
                 .and()
                 .formLogin().disable() // 폼 로그인 비활성화
                 .httpBasic().disable(); // Basic Auth 비활성화
