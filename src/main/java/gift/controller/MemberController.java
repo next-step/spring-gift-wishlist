84855ab17b8f06ee6d43effb63fb8b7206770869
BSY3510 <baeksy3510@gmail.com>
2025-07-07 17:27:05 +0900

    feat(controller): implement MemberController

---
+++ src/main/java/gift/controller/MemberController.java
@@ -0,0 +1,43 @@
+package gift.controller;
+
+import gift.dto.MemberRequest;
+import gift.dto.TokenResponse;
+import gift.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody MemberRequest request) {
+        try {
+            TokenResponse response = memberService.register(request);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody MemberRequest request) {
+        try {
+            TokenResponse response = memberService.login(request);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+}
