ca9356264a659ff0919c51c800020fec49ad75fb
박신영 <park_new0@naver.com>
2025-07-07 12:51:44 +0900

    feat(auth): token validation logic을 jwtprovider에 추가

--- src/main/java/gift/auth/JwtTokenProvider.java
+++ src/main/java/gift/auth/JwtTokenProvider.java
@@ -1,8 +1,10 @@
 package gift.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtTokenProvider {
 
-    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final String secretKeyString = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    private final SecretKey key = Keys.hmacShaKeyFor(
+            secretKeyString.getBytes(StandardCharsets.UTF_8));
     private final long validityInMilliseconds = 3600000; // 1시간
 
     public String createToken(String email) {
@@ -18,10 +22,32 @@ public class JwtTokenProvider {
         Date validity = new Date(now.getTime() + validityInMilliseconds);
 
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(now)
-                .setExpiration(validity)
+                .subject(email)
+                .issuedAt(now)
+                .expiration(validity)
                 .signWith(key)
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return !claims.getPayload().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getPayload(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 }
