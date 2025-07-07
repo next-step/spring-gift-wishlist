f0d1dfa6207672db53c0bd1237bbd85e292cc4bb
Dohyun <sks020k@naver.com>
2025-07-07 14:34:10 +0900

    chore : Spring Bean annotation 추가

--- src/main/java/gift/user/JwtTokenProvider.java
+++ src/main/java/gift/user/JwtTokenProvider.java
@@ -3,7 +3,9 @@ package gift.user;
 import gift.user.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtTokenProvider {
 
   private final String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
