02028ea4da4106cb6ce34e140707489e06d67677
HJH <jijinny26@gmail.com>
2025-07-07 16:44:02 +0900

    test: 각 테스트에 @DisplayName 추가

--- src/test/java/gift/ProductValidationTest.java
+++ src/test/java/gift/ProductValidationTest.java
@@ -1,5 +1,6 @@
 package gift;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +22,7 @@ class ProductValidationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @DisplayName("상품명 15자 초과 시 유효성 에러 발생")
     @Test
     void createProduct_NameTooLong_ShouldReturnValidationError() throws Exception {
         mockMvc.perform(post("/admin/products-add")
@@ -34,6 +36,7 @@ class ProductValidationTest {
                 .andExpect(view().name("admin/product-add"));
     }
 
+    @DisplayName("상품명에 특수문자 포함 시 유효성 에러 발생")
     @Test
     void createProduct_InvalidSpecialCharacters_ShouldReturnValidationError() throws Exception {
         mockMvc.perform(post("/admin/products-add")
@@ -47,6 +50,7 @@ class ProductValidationTest {
                 .andExpect(view().name("admin/product-add"));
     }
 
+    @DisplayName("MD 승인 없이 '카카오'가 포함된 상품명 등록 시도 시 오류 발생")
     @Test
     void createProduct_KakaoWithoutApproval_ShouldReturnMdApprovalError() throws Exception {
         mockMvc.perform(post("/admin/products-add")
@@ -60,6 +64,7 @@ class ProductValidationTest {
                 .andExpect(view().name("admin/product-add"));
     }
 
+    @DisplayName("MD 승인 후 '카카오'가 포함된 수정 시도 시 정상 리다이렉트")
     @Test
     void editProduct_KakaoWithApproval_ShouldRedirect() throws Exception {
         mockMvc.perform(post("/admin/4")
