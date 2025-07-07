c0d92972e131bc672b96af4a0d56a04f32159122
eastgyu11 <eastgyu11>
2025-07-07 14:07:49 +0900

    style: 필요하지 않은 주석 제거

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")//코드리뷰: 코드중복줄이기
+@RequestMapping("/api/products")
 public class ProductController {
 
   private final ProductService service;
