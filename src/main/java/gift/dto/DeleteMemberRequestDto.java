f67c2ea6cfc1d08b2a0c14189d8bc859b10b176e
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-07 15:06:12 +0900

    feat: 멤버 dto bean validation 추가

--- src/main/java/gift/dto/DeleteMemberRequestDto.java
+++ src/main/java/gift/dto/DeleteMemberRequestDto.java
@@ -1,7 +1,12 @@
 package gift.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record DeleteMemberRequestDto(
+        @Email(message="이메일을 입력해주세요")
         String email,
+        @NotBlank(message = "비밀번호를 입력해주세요")
         String password
 ) {
 }
--- src/main/java/gift/dto/UpdateMemberPasswordRequestDto.java
+++ src/main/java/gift/dto/UpdateMemberPasswordRequestDto.java
@@ -1,8 +1,14 @@
 package gift.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateMemberPasswordRequestDto(
+        @Email(message="이메일을 입력해주세요")
         String email,
+        @NotBlank(message = "이전 비밀번호를 입력해주세요")
         String oldPassword,
+        @NotBlank(message = "변경할 비밀번호를 입력해주세요")
         String newPassword
 ) {
 
