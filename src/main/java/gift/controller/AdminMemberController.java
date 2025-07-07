1b1c7309c121b68bc4ab090336ea5e72205f3b80
eastgyu11 <eastgyu11>
2025-07-07 14:05:33 +0900

    feat: 관리자 화면 controller 구현

---
+++ src/main/java/gift/controller/AdminMemberController.java
@@ -0,0 +1,65 @@
+package gift.controller;
+
+import gift.entity.Member;
+import gift.service.member.MemberService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/members")
+public class AdminMemberController {
+
+  MemberService service;
+  private static final String MEMBERS_LIST_PAGE_PATH = "/admin/members";
+
+
+  public AdminMemberController(MemberService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public String findAllMembers(Model model) {
+    List<Member> memberList = service.findAllMember();
+    model.addAttribute("memberList", memberList);
+    return "memberList";
+  }
+
+  @GetMapping("/new")
+  public String createMemberForm(Model model) {
+    model.addAttribute("member", new Member());
+    return "createMemberForm";
+  }
+
+  @PostMapping("/new")
+  public String createMember(@ModelAttribute Member member) {
+    service.createMember(member);
+    return "redirect:" + MEMBERS_LIST_PAGE_PATH;
+  }
+
+  @GetMapping("/{id}/update")
+  public String updateMemberForm(@PathVariable("id") Long id, Model model) {
+    Member memberById = service.findMemberById(id);
+    model.addAttribute("member", memberById);
+    return "updateMemberForm";
+  }
+
+  @PutMapping("/{id}/update")
+  public String update(@PathVariable("id") Long id, @ModelAttribute Member member) {
+    service.updateMember(id, member);
+    return "redirect:" + MEMBERS_LIST_PAGE_PATH;
+  }
+
+  @DeleteMapping("/{id}/delete")
+  public String delete(@PathVariable("id") Long id) {
+    service.deleteMember(id);
+    return "redirect:" + MEMBERS_LIST_PAGE_PATH;
+  }
+}
