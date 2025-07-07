4ab7afe969115868a0535b7ca6a091a529c63b45
oh010 <oh010224@gmail.com>
2025-07-07 11:20:43 +0900

    refactor: 불필요한 import 문 제거

--- src/main/java/gift/product/controller/api/ProductController.java
+++ src/main/java/gift/product/controller/api/ProductController.java
@@ -7,12 +7,10 @@ import gift.product.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
