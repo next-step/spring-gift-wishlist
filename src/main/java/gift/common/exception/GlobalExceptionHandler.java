f37f32754a70203618409a8344bd196e5db7e82d
oh010 <oh010224@gmail.com>
2025-07-07 11:20:09 +0900

    refactor: validation 적용으로 불필요한 handler 제거

--- src/main/java/gift/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/common/exception/GlobalExceptionHandler.java
@@ -5,15 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<String> handleEmptyResultDataAccess(EmptyResultDataAccessException e) {
