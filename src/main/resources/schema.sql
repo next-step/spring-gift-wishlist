0902bab82de173dc4bf5d88e51e8b03dc4d68037
KKPASII <kkongpasi1005@gmail.com>
2025-07-07 14:53:49 +0900

    fix(db): 상품 스키마의 상품 이름(name) UNIQUE 제약 조건 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product
 (
     id        INT AUTO_INCREMENT PRIMARY KEY,
-    name      VARCHAR(15)   NOT NULL UNIQUE,
+    name      VARCHAR(15)   NOT NULL,
     price     INT           NOT NULL CHECK (price >= 0),
     image_url VARCHAR(1024) NOT NULL
 );
