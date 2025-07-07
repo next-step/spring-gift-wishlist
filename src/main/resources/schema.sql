06e4a1e136b3c7b1b492e68576caf98c22062d21
hayun <psh040826@naver.com>
2025-07-07 15:08:53 +0900

    feat: member 테이블을 schema.sql에 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE product (
                          image_url   VARCHAR(255),
                          PRIMARY KEY (id)
 );
+
+CREATE TABLE member (
+                        id          BIGINT          NOT NULL AUTO_INCREMENT,
+                        email       VARCHAR(255)    NOT NULL UNIQUE,
+                        password    VARCHAR(255)    NOT NULL,
+                        PRIMARY KEY (id)
+);
