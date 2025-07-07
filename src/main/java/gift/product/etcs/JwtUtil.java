d98a2224e88476f9eaca44376c701366e68e89fc
Lee Si Woong <silverttthin@naver.com>
2025-07-08 02:05:16 +0900

    feat: 토큰 파싱 유틸함수 작성 - 토큰을 파싱하지만 리턴값을 사용하지 않고 그대로 리턴하는게 킥임 - 왜냐하면 중간에 parse()가 예외를 터트린다 == 잘못된 토큰 - 무사히 다 끝난다 -> 유효한 토큰임을 true 리턴으로 표시

--- src/main/java/gift/product/etcs/JwtUtil.java
+++ src/main/java/gift/product/etcs/JwtUtil.java
@@ -2,9 +2,13 @@ package gift.product.etcs;
 
 
 import gift.product.entity.User;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -34,6 +38,37 @@ public class JwtUtil {
 			.expiration(expiryDate)
 			.signWith(secretKey, Jwts.SIG.HS256)
 			.compact();
+	}
+
+	public String extractJwtTokenFromHeader(HttpServletRequest request) {
+		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			return authorizationHeader.substring(7);
+		}
+
+		return null;
+	}
+
+	public Boolean validateJwtToken(String token) {
+		try {
+			// jwt를 파싱하는 코드지만 가장 큰 목적은 파싱 도중 잘못된 토큰이면 예외를 뱉어내는 데에 있다
+			Jwts.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parse(token);
+			return true;
+
+		} catch (SecurityException e) {
+			throw new IllegalArgumentException("decryption를 실패했습니다.", e);
+		} catch (MalformedJwtException e) {
+			throw new IllegalArgumentException("신뢰할 수 없는 토큰입니다.", e);
+		} catch (ExpiredJwtException e) {
+			throw new IllegalArgumentException("토큰이 만료됐습니다.", e);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("유효하지 않은 토큰입니다.", e);
+		}
+
 	}
 
 
