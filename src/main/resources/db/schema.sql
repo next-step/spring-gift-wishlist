af9ce11fe0e8399c889752fcc10b40385da2455a
HJH <jijinny26@gmail.com>
2025-07-07 16:48:50 +0900

    refactor: product 테이블 컬럼 크기 조정

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price BIGINT NOT NULL,
-    image_url VARCHAR(255),
-    md_approved BOOLEAN NOT NULL DEFAULT FALSE COMMENT '담당 MD 협의 여부'
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        price BIGINT NOT NULL,
+        image_url VARCHAR(1000),
+        md_approved BOOLEAN NOT NULL DEFAULT FALSE COMMENT '담당 MD 협의 여부'
 );
