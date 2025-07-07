fec461f9751b6d22e259433a6f5bbfa4df96bbc8
Lee Si Woong <silverttthin@naver.com>
2025-07-08 00:23:39 +0900

    fix: schema 및 초기화 로직 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,22 @@
-INSERT INTO item (name, price, image_url) VALUES
-('item1', 1000, 'url1'),
-('item2', 2000, 'url2'),
-('item3', 3000, 'url3'),
-('item4', 4000, 'url4'),
-('item5', 5000, 'url5'),
-('item6', 6000, 'url6'),
-('item7', 7000, 'url7'),
-('item8', 8000, 'url8'),
-('item9', 9000, 'url9'),
-('item10', 10000, 'url10'),
-('item11', 11000, 'url11'),
-('item12', 12000, 'url12'),
-('item13', 13000, 'url13'),
-('item14', 14000, 'url14'),
-('item15', 15000, 'url15');
-
+-- users 데이터
 INSERT INTO users (email, password, nickname) VALUES
-('s@s', '1533', '이시웅'),
-('t@t', '1533', '트랄라렐로 트랄랄라');
+('s@s', '1', '이시웅'),
+('t@t', '1', '카카오');
+
+-- item 데이터 (user_id 포함)
+INSERT INTO item (name, price, image_url, user_id) VALUES
+('item1',  1000,  'url1',  1),
+('item2',  2000,  'url2',  1),
+('item3',  3000,  'url3',  1),
+('item4',  4000,  'url4',  1),
+('item5',  5000,  'url5',  1),
+('item6',  6000,  'url6',  1),
+('item7',  7000,  'url7',  1),
+('item8',  8000,  'url8',  1),
+('item9',  9000,  'url9',  2),
+('item10',10000, 'url10', 2),
+('item11',11000, 'url11', 2),
+('item12',12000, 'url12', 2),
+('item13',13000, 'url13', 2),
+('item14',14000, 'url14', 2),
+('item15',15000, 'url15', 2);
