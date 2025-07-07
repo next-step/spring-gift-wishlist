a6054e8add84c86d8dc4e5a256aabd4f1bf1c43a
LeeJaeRyun <pinixs3@naver.com>
2025-07-07 16:09:48 +0900

    feat: member 엔티티 추가

---
+++ src/main/java/gift/member/entity/Member.java
@@ -0,0 +1,31 @@
+package gift.member.entity;
+
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+
+    public Member(Long id, String email, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
