5103f0ed73b6b108526178237b23990b82df62d7
BSY3510 <baeksy3510@gmail.com>
2025-07-07 17:21:11 +0900

    feat(repo): implement MemberRepository

---
+++ src/main/java/gift/repository/MemberRepositoryImpl.java
@@ -0,0 +1,114 @@
+package gift.repository;
+
+import gift.entity.Member;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepositoryImpl implements MemberRepository {
+
+    private final JdbcClient jdbcClient;
+
+    public MemberRepositoryImpl(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    private static final RowMapper<Member> MEMBER_ROW_MAPPER = (rs, rowNum) -> new Member(
+        rs.getLong("id"),
+        rs.getString("email"),
+        rs.getString("password"),
+        rs.getString("role")
+    );
+
+    @Override
+    public Member save(Member member) {
+        if (member == null) {
+            throw new IllegalArgumentException("member cannot be null");
+        }
+
+        if (member.getEmail() == null || member.getPassword() == null || member.getRole() == null) {
+            throw new IllegalArgumentException("Required fields are missing");
+        }
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sql = "INSERT INTO members (email, password, role) VALUES (?, ?, ?)";
+        jdbcClient.sql(sql)
+            .param(1, member.getEmail())
+            .param(2, member.getPassword())
+            .param(3, member.getRole())
+            .update(keyHolder, new String[]{"id"});
+
+        Number key = keyHolder.getKey();
+        if (key == null) {
+            throw new IllegalArgumentException("Failed to get id");
+        }
+        Long id = key.longValue();
+
+        return new Member(
+            id,
+            member.getEmail(),
+            member.getPassword(),
+            member.getRole()
+        );
+    }
+
+    @Override
+    public Member update(Member member) {
+        if (member == null) {
+            throw new IllegalArgumentException("member cannot be null");
+        }
+
+        if (member.getId() == null || member.getEmail() == null || member.getPassword() == null
+            || member.getRole() == null) {
+            throw new IllegalArgumentException("Required fields are missing");
+        }
+
+        String sql = "UPDATE members SET email = ?, password = ?, role = ? WHERE id = ?";
+        int rowsCount = jdbcClient.sql(sql)
+            .param(1, member.getEmail())
+            .param(2, member.getPassword())
+            .param(3, member.getRole())
+            .param(4, member.getId())
+            .update();
+        if (rowsCount == 0) {
+            throw new IllegalArgumentException("Member(id: " + member.getId() + ") not found");
+        }
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "SELECT id, email, password, role FROM members WHERE id = ?";
+        return jdbcClient.sql(sql)
+            .param(1, id)
+            .query(Member.class)
+            .optional();
+    }
+
+    @Override
+    public void delete(Long id) {
+        String sql = "DELETE FROM members WHERE id = ?";
+        int rowsCount = jdbcClient.sql(sql)
+            .param(1, id)
+            .update();
+        if (rowsCount == 0) {
+            throw new IllegalArgumentException("Member(id: " + id + " ) not found");
+        }
+    }
+
+    @Override
+    public List<Member> findAll() {
+        String sql = "SELECT id, email, password, role FROM members";
+        return jdbcClient.sql(sql)
+            .query(Member.class)
+            .list();
+    }
+
+}
