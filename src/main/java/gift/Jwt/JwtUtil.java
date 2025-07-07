e17e8794e4c42873db9cb8c2111e2bd09b9c060e
xodbs126 <dltvmsskd@naver.com>
2025-07-07 14:29:15 +0900

    feat:jwt 로직 추가

--- src/main/java/gift/Jwt/JwtUtil.java
+++ src/main/java/gift/Jwt/JwtUtil.java
@@ -1,4 +1,43 @@
 package gift.Jwt;
 
+import gift.entity.User;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    private static final String SECRET = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    private static final long EXPIRATION_MS = 1000 * 60 * 60 * 24;
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .subject(user.id().toString())
+                .claim("email", user.email())
+                .claim("role", "USER")
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
 }
