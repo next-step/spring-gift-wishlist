724bdb752167ce31e1558ee1304e689d7f78149f
uvrvuoon <0524yar@gmail.com>
2025-07-07 16:17:13 +0900

    feat: UserRepository에게 id 생성 책임 맡김

--- src/main/java/gift/repository/UserRepository.java
+++ src/main/java/gift/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package gift.repository;
 
+import gift.entity.Product;
 import gift.entity.User;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 
@@ -16,10 +21,26 @@ public class UserRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void save(User user) {
-        String sql = "insert into users (email, password) values (?, ?)";
+    public Product save(Product product) {
 
-        jdbcTemplate.update(sql, user.getEmail(), user.getPassword());
+        String sql = "insert into product (name, imageUrl) values (?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, product.getName());
+            ps.setString(2, product.getImageUrl());
+            return ps;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+
+        if(key != null) {
+            product.setId(key.longValue());
+        }
+
+        return product;
     }
 
     public Optional<User> findByEmail(String email) {
