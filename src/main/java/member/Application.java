57bb402fc9e71da613ef9b4d659fa9f7804e83c0
gosurim <su020900@gmail.com>
2025-07-07 17:37:11 +0900

    feat: 회원 서비스 로직 구현

---
+++ src/main/java/member/Application.java
@@ -0,0 +1,12 @@
+package member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
