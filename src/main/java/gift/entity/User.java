9176a977f41b4e026060722da16008db36b6b447
uvrvuoon <0524yar@gmail.com>
2025-07-07 16:19:17 +0900

    feat: User Entity에 생성자 추가

--- src/main/java/gift/entity/User.java
+++ src/main/java/gift/entity/User.java
@@ -6,6 +6,12 @@ public class User {
     private String email;
     private String password;
 
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getEmail() {return email;}
     public String getPassword() {return password;}
 }
