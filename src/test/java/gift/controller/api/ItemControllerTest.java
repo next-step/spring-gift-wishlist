8fef2a5b30f35514ca73b2a89e165293d6a21696
KIYEONG <rldud1237@naver.com>
2025-07-07 17:17:00 +0900

    test: 회원가입 및 로그인 API 통합 테스트 작성

--- src/test/java/gift/controller/api/ItemControllerTest.java
+++ src/test/java/gift/controller/api/ItemControllerTest.java
@@ -1,4 +1,4 @@
-package gift.controller;
+package gift.controller.api;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
--- src/test/java/gift/controller/web/AdminItemControllerTest.java
+++ src/test/java/gift/controller/web/AdminItemControllerTest.java
@@ -1,4 +1,4 @@
-package gift.controller;
+package gift.controller.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
