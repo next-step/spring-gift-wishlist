bdfa8db88a127b1c5593949fa42eb497558b020e
sinsehwan <sk124590@gmail.com>
2025-07-07 16:03:00 +0900

    feat(member): add rest api membercontroller

---
+++ src/main/java/gift/member/controller/MemberController.java
@@ -0,0 +1,39 @@
+package gift.member.controller;
+
+import gift.member.domain.Member;
+import gift.member.dto.MemberLoginRequest;
+import gift.member.dto.MemberLoginResponse;
+import gift.member.dto.MemberRegisterRequest;
+import gift.member.service.MemberService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Member> register(@Valid @RequestBody MemberRegisterRequest request) {
+        Member member = memberService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(member);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MemberLoginResponse> login(@RequestBody MemberLoginRequest request) {
+        MemberLoginResponse response = memberService.login(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+}
