dbd06a05e1ff08938890031f81017107df9f2cdf
BSY3510 <baeksy3510@gmail.com>
2025-07-07 17:20:17 +0900

    feat(repo): add MemberRepository interface

---
+++ src/main/java/gift/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package gift.repository;
+
+import gift.entity.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member update(Member member);
+
+    Optional<Member> findById(Long id);
+
+    void delete(Long id);
+
+    List<Member> findAll();
+
+}
