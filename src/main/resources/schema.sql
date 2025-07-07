d842647053fd62e5ab757c30b9ba61922aebbfcb
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-07 14:13:36 +0900

    feat: wishlist table 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,4 +12,16 @@ create table members
     email varchar(255) not null,
     password varchar(255) not null,
     role varchar(10) not null
-)
+);
+
+create table wishlist
+(
+    id bigint AUTO_INCREMENT PRIMARY KEY,
+    member_id bigint not null,
+    product_id bigint not null,
+
+    foreign key (member_id) references members(id) on delete cascade,
+    foreign key (product_id) references products(id) on delete cascade ,
+
+    unique(member_id, product_id)
+);
