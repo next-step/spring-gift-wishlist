ccc8c663f81ae979a6568f4e52b8d4f8b7773fd1
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-07-07 16:01:56 +0900

    feat : 로그인 API 앤드포인트 추가

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,5 +1,6 @@
 package gift.controller;
 
+import gift.dto.MemberLoginRequest;
 import gift.dto.MemberRegisterRequest;
 import gift.service.AuthService;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,12 @@ public class MemberController {
     public ResponseEntity<TokenResponse> register(@RequestBody MemberRegisterRequest request) {
         String token = authService.registerAndIssueToken(request);
         return ResponseEntity.status(201).body(new TokenResponse(token));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody MemberLoginRequest request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(new TokenResponse(token));
     }
 
     private record TokenResponse(String token) {}
