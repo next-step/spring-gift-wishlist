fc9e79025c92c3795633cfb7f458e3f56ac707b0
hydrationn <suhwaparkk1031@gmail.com>
2025-07-07 13:37:29 +0900

    test: ProductRequestDtoTest DTO 생성 공통 로직 리팩토링

--- src/test/java/gift/dto/ProductRequestDtoTest.java
+++ src/test/java/gift/dto/ProductRequestDtoTest.java
@@ -25,10 +25,14 @@ public class ProductRequestDtoTest {
         url = "http://localhost:" + port + "/api/products";
     }
 
+    private ProductRequestDto createProductRequestDto(String name, Integer price, String imageUrl) {
+        return new ProductRequestDto(name, price, imageUrl);
+    }
+
     @Test
     @DisplayName("Product 생성 시, 검증된 input을 넣으면 201(Created)이 반환된다. ")
     void createProduct_withValidInput_returns201() {
-        ProductRequestDto productRequestDto = new ProductRequestDto(
+        ProductRequestDto productRequestDto = createProductRequestDto(
                 "하리보 젤리(콜라맛)",
                 2000,
                 "http://img.url/test.png"
@@ -53,7 +57,7 @@ public class ProductRequestDtoTest {
     @DisplayName("Product 생성 시, 상품명이 15자를 초과한 경우 400(Bad Request)를 반환한다. ")
     void createProduct_withNameTooLong_returns400() {
         String name = "하리보맛있네또사먹을게요많이파세요"; // 총 17자
-        ProductRequestDto productRequestDto = new ProductRequestDto(name, 50, null);
+        ProductRequestDto productRequestDto = createProductRequestDto(name, 50, null);
 
         assertThatExceptionOfType(HttpClientErrorException.BadRequest.class)
                 .isThrownBy(() ->
@@ -72,7 +76,7 @@ public class ProductRequestDtoTest {
     @Test
     @DisplayName("Product 생성 시, 허용되지 않은 특수문자를 포함한 경우 400(Bad Request)를 반환한다. ")
     void createProduct_withBadCharacters_returns400() {
-        ProductRequestDto productRequestDto = new ProductRequestDto("하리보!@", 20, null);
+        ProductRequestDto productRequestDto = createProductRequestDto("하리보!@", 20, null);
 
         assertThatExceptionOfType(HttpClientErrorException.BadRequest.class)
                 .isThrownBy(() ->
@@ -91,7 +95,7 @@ public class ProductRequestDtoTest {
     @Test
     @DisplayName("Product 생성 시, 음수 가격을 입력한 경우 400(Bad Request)를 반환한다. ")
     void createProduct_withNegativePrice_returns400() {
-        ProductRequestDto productRequestDto = new ProductRequestDto("하리보 젤리", -2000, null);
+        ProductRequestDto productRequestDto = createProductRequestDto("하리보 젤리", -2000, null);
 
         assertThatExceptionOfType(HttpClientErrorException.BadRequest.class)
                 .isThrownBy(() ->
@@ -110,7 +114,7 @@ public class ProductRequestDtoTest {
     @Test
     @DisplayName("Product 생성 시, '카카오'가 포함된 문구를 입력한 경우 400(Bad Request)를 반환한다. ")
     void createProduct_withKakaoInName_returns400() {
-        ProductRequestDto productRequestDto = new ProductRequestDto("카카오 젤리", 3000, null);
+        ProductRequestDto productRequestDto = createProductRequestDto("카카오 젤리", 3000, null);
 
         assertThatExceptionOfType(HttpClientErrorException.BadRequest.class)
                 .isThrownBy(() ->
