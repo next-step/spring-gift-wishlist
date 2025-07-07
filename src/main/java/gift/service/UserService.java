fcccea43bc6a304e74fa0d65f254f8d4992defc6
uvrvuoon <0524yar@gmail.com>
2025-07-07 18:07:16 +0900

    feat: UserService 구현

---
+++ src/main/java/gift/service/UserService.java
@@ -0,0 +1,38 @@
+package gift.service;
+
+import gift.JwtTokenProvider;
+import gift.dto.UserDto;
+import gift.entity.User;
+import gift.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public UserService(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
+        this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public UserDto register(UserDto dto) {
+        User user = new User(null, dto.getEmail(), dto.getPassword());
+        User saved = userRepository.save(user);
+
+        return new UserDto(saved);
+    }
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()->new IllegalArgumentException("가입되지 않은 이메일입니다."));
+
+        if(!user.getPassword().equals(password)) {
+            throw new IllegalAccessException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return jwtTokenProvider.createToken(user.getEmail());
+    }
+
+}
