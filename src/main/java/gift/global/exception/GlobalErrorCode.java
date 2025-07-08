12519a1a89bec8b57e076b5998d2bea3986795ac
lwj0831 <wjl0831@gmail.com>
2025-07-08 22:55:54 +0900

    chore: GlobaleErrorCode, ProductErrorCode내 서비스 자체 에러코드명 명확하게 도메인이 드러나도록 수정

--- src/main/java/gift/global/exception/GlobalErrorCode.java
+++ src/main/java/gift/global/exception/GlobalErrorCode.java
@@ -3,8 +3,8 @@ package gift.global.exception;
 import org.springframework.http.HttpStatus;
 
 public enum GlobalErrorCode implements ErrorCode {
-  INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "G001", "요청 값이 유효하지 않습니다"),
-  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G002", "내부 서버 오류가 발생했습니다");
+  INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL-001", "요청 값이 유효하지 않습니다"),
+  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLOBAL-002", "내부 서버 오류가 발생했습니다");
 
   private final HttpStatus status;
   private final String errorCode;
--- src/main/java/gift/product/exception/ProductErrorCode.java
+++ src/main/java/gift/product/exception/ProductErrorCode.java
@@ -4,9 +4,9 @@ import gift.global.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum ProductErrorCode implements ErrorCode {
-  PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "상품 정보를 찾을 수 없습니다"),
-  INVALID_PRODUCT_NAME(HttpStatus.BAD_REQUEST, "P002", "상품명에 포함할 수 없는 키워드가 존재합니다"),
-  INVALID_SORT_FIELD_ERROR(HttpStatus.BAD_REQUEST, "P003", "상품 페이징 정렬 필드 값이 올바르지 않습니다");
+  PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT-001", "상품 정보를 찾을 수 없습니다"),
+  INVALID_PRODUCT_NAME(HttpStatus.BAD_REQUEST, "PRODUCT-002", "상품명에 포함할 수 없는 키워드가 존재합니다"),
+  INVALID_SORT_FIELD_ERROR(HttpStatus.BAD_REQUEST, "PRODUCT-003", "상품 페이징 정렬 필드 값이 올바르지 않습니다");
 
   private final HttpStatus status;
   private final String errorCode;
