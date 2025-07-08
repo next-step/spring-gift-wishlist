645fb7b436559a0cf585e7573cf8a19922a7c53a
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-07-08 16:22:14 +0900

    fix: Wishlist delete 오류 수정

--- src/main/java/gift/controller/wishlist/WishlistController.java
+++ src/main/java/gift/controller/wishlist/WishlistController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +47,7 @@ public class WishlistController {
     @DeleteMapping("/{productId}")
     public ResponseEntity<Void> deleteToMyWishlist(
         @CurrentUser Long userId,
-        @RequestParam(name = "productId") Long id
+        @PathVariable(name = "productId") Long id
     ) {
         wishlistService.deleteFromMyWishlist(userId, id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
