9a879a6f7842e165b47cba17ac960caccf100aba
sungchan <awdr1057@naver.com>
2025-07-09 03:20:03 +0900

    feat: User관리를 위한 UserRepo 구현

---
+++ src/main/java/com/example/demo/repository/UserJdbcClientRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.User;
+import java.util.Optional;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserJdbcClientRepositoryImpl implements UserJdbcClientRepository{
+
+  private final JdbcClient jdbcClient;
+
+  public UserJdbcClientRepositoryImpl(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
+
+  @Override
+  public Optional<User> findByEmail(String email) {
+    String sql = "SELECT id, email, password, role FROM users WHERE email = :email";
+    return jdbcClient.sql(sql)
+        .param("email", email)
+        .query(User.class)
+        .optional();
+  }
+
+  @Override
+  public void saveUser(User user) {
+    String sql = "INSERT INTO users (email, password, role) VALUES (:email, :password:, :role)";
+
+    jdbcClient.sql(sql)
+        .param("email", user.getEmail())
+        .param("password", user.getPassword())
+        .param("role", user.getRole())
+        .update();
+  }
+
+  @Override
+  public void deleteByEmail(String email) {
+    String sql = "DELETE FROM users WHERE email = :email";
+
+    jdbcClient.sql(sql)
+        .param("email", email)
+        .update();
+  }
+}
