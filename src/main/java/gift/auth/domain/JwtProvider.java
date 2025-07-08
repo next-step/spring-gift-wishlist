3fdc448a07c92dcc7d8661c4b446a4b01ca296e7
lwj0831 <wjl0831@gmail.com>
2025-07-08 16:25:20 +0900

    feat(auth): JwtProvider 유틸클래스 등록

---
+++ src/main/java/gift/auth/domain/JwtProvider.java
@@ -0,0 +1,92 @@
+package gift.auth.domain;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Collection;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+  private final JwtProperties jwtProperties;
+  private final SecretKey key;
+  private final long ACCESS_TOKEN_VALIDITY;
+  private final long REFRESH_TOKEN_VALIDITY;
+  private final String ISSUER;
+
+  public JwtProvider(JwtProperties jwtProperties) {
+    this.jwtProperties = jwtProperties;
+    this.key = Keys.hmacShaKeyFor(jwtProperties.secretKey().getBytes());
+    this.ACCESS_TOKEN_VALIDITY = jwtProperties.accessTokenValidity();
+    this.REFRESH_TOKEN_VALIDITY= jwtProperties.refreshTokenValidity();
+    this.ISSUER = jwtProperties.issuer();
+  }
+
+  public String createToken(Long userId, String email, Collection<String> roles) {
+    Claims claims = Jwts.claims()
+        .add("email", email)
+        .add("roles", roles.stream().toList())
+        .build();
+
+    Date now = new Date();
+    Date expiredAt = new Date(now.getTime() + ACCESS_TOKEN_VALIDITY);
+
+    return Jwts.builder()
+        .issuer(ISSUER)
+        .subject(userId.toString())
+        .claims(claims)
+        .issuedAt(now)
+        .expiration(expiredAt)
+        .signWith(key)
+        .compact();
+  }
+
+  public String createRefreshToken(Long userId) {
+    Date now = new Date();
+    Date expiredDate = new Date(now.getTime() + REFRESH_TOKEN_VALIDITY);
+
+    return Jwts.builder()
+        .issuer(ISSUER)
+        .subject(userId.toString())
+        .issuedAt(now)
+        .expiration(expiredDate)
+        .signWith(key)
+        .compact();
+  }
+
+  public Long getUserId(String token) {
+    return Long.parseLong(
+        Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getSubject()
+    );
+  }
+
+  public String getEmail(String token) {
+    return Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .get("email",String.class);
+  }
+
+  public boolean validateToken(String token) {
+    try {
+      Jwts.parser()
+          .verifyWith(key)
+          .build()
+          .parseSignedClaims(token);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+}
