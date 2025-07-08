68e86f51926fc78a3dbbccddaf3d62f91bbbd020
doorcs <itsdoorcs@gmail.com>
2025-07-09 02:27:12 +0900

    refactor: 정규표현식을 통한 상품명 검증 기능을 커스텀 어노테이션으로 분리

--- src/main/java/gift/dto/UpdateProductRequest.java
+++ src/main/java/gift/dto/UpdateProductRequest.java
@@ -2,19 +2,16 @@ package gift.dto;
 
 import org.hibernate.validator.constraints.Length;
 
+import gift.validation.ProductNamePattern;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record UpdateProductRequest(
 
     @NotBlank(message = "상품명이 입력되지 않았습니다.")
     @Length(max = 15, message = "상품명은 최대 15자까지 입력 가능합니다.")
-    @Pattern(
-        regexp = "^[0-9a-zA-Z가-힣()\\s\\[\\]+\\-&/_]*$",
-        message = "( ), [ ], +, -, &, /, _ 외의 특수문자는 사용하실 수 없습니다."
-    )
+    @ProductNamePattern(message = "( ), [ ], +, -, &, /, _ 외의 특수문자는 사용하실 수 없습니다.")
     String name,
 
     @NotNull(message = "상품 가격이 입력되지 않았습니다.")
