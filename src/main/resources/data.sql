23a4aa87c5d07e68485ec2ef095f42a49dae2272
KKPASII <kkongpasi1005@gmail.com>
2025-07-08 19:19:18 +0900

    feat(db): member 스키마 정의

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO product (name, price, image_url) VALUES ('hamburger', 4500, 'https://media.istockphoto.com/id/182744943/ko/%EC%82%AC%EC%A7%84/burger.jpg?s=2048x2048&w=is&k=20&c=Phev16dEc55V8v9t7_ty9fk7T-yq_Fs9Q7wMnPmhMJg=');
 INSERT INTO product (name, price, image_url) VALUES ('egg', 2000, 'https://cdn.pixabay.com/photo/2018/07/23/18/43/hens-egg-3557544_1280.jpg');
+
+INSERT INTO member (email, password, role) VALUES ('ham@email.com', '$2a$10$nGlxOnVYmiKQD0pGZUzWzO.UkoKI5fIvSO905blP/kxqNMzZ59KMO', 'ADMIN');
+INSERT INTO member (email, password, role) VALUES ('dam@email.com', '$2a$10$nGlxOnVYmiKQD0pGZUzWzO.UkoKI5fIvSO905blP/kxqNMzZ59KMO', 'USER');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE product
     price     INT           NOT NULL CHECK (price >= 0),
     image_url VARCHAR(1024) NOT NULL
 );
+
+CREATE TABLE member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(10) NOT NULL
+);
