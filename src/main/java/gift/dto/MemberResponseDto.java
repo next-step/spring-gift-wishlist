9c9102bd3076765bb4a6ccb7d76572c2462e6381
hhseo9519 <hhseo9519@naver.com>
2025-07-08 17:44:30 +0900

    feat : MemberResponseDto 작성

--- src/main/java/gift/dto/MemberResponseDto.java
+++ src/main/java/gift/dto/MemberResponseDto.java
@@ -2,4 +2,22 @@ package gift.dto;
 
 public class MemberResponseDto {
 
+    private String token;
+
+
+    public MemberResponseDto() {
+    }
+
+    public MemberResponseDto(String token) {
+        this.token = token;
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
