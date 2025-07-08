5c49c155001b95a1b77dd8bb894ec8c418791b22
milk-stone <ywshero@naver.com>
2025-07-08 01:01:23 -0700

    style: 폴더 구조 변경

--- src/main/java/gift/domain/admin/controller/AdminController.java
+++ src/main/java/gift/domain/admin/controller/AdminController.java
@@ -1,4 +1,4 @@
-package gift.admin.controller;
+package gift.domain.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
