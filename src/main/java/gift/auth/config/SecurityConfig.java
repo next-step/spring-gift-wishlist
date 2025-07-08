24ca2418e639a151f3de957b30f8a313f47295f9
lwj0831 <wjl0831@gmail.com>
2025-07-08 18:39:34 +0900

    config: H2 DB 접근을 위한 설정 추가 및 data.sql 수정
    - memberRepository내 jdbcInsert 전략으로 id 1부터 생성되어 테스트 회원 id와 충돌 일어나는 현상 해결
    - SpringSecurityFilterChain에 header관련 사항 추가

--- src/main/java/gift/auth/config/SecurityConfig.java
+++ src/main/java/gift/auth/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,9 +31,13 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
+        .headers(headers -> headers
+            .defaultsDisabled()
+            .frameOptions(FrameOptionsConfig::sameOrigin)
+        )
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh").permitAll()
+            .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh","/h2-console/**").permitAll()
             .anyRequest().authenticated()
         )
         .addFilterBefore(jwtValidationFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,13 @@
 INSERT INTO product (name, price, description, image_url)
-VALUES ('테스트 상품', 1000, '설명', 'https://img.url');
+VALUES ('테스트 상품1', 1000, '설명1', 'https://img1.url');
+INSERT INTO product (name, price, description, image_url)
+VALUES ('테스트 상품2', 2000, '설명2', 'https://img2.url');
+
+INSERT INTO member (id, name) VALUES (100, 'kim');
+INSERT INTO member (id, name) VALUES (101, 'park');
+
+INSERT INTO member_auth (member_id, email, password, refresh_token)
+VALUES (100, 'kim@example.com', '$2a$10$C9fK7K1QZUXmCqZsMR8VdupxCBFe2g7kWyWeLb9bPP1WjAzBtHSSK', NULL);
+
+INSERT INTO member_auth (member_id, email, password, refresh_token)
+VALUES (101, 'park@example.com', '$2a$10$7jxHkc3x2P8ZbmHcDxOXke95hWuHx6cLG3axrGgVqLEK3KLS/ZUQ6', 'abcd');
