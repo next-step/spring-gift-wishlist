1f334c900df679a4160c30ff233c61a426542613
hhseo9519 <hhseo9519@naver.com>
2025-07-08 17:44:43 +0900

    feat : 로그인 기능 구현

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -26,15 +26,26 @@ public class MemberService {
                     HttpStatus.FORBIDDEN, "이미 등록된 이메일입니다.");
         }
 
-
         Member member = new Member(requestDto.getEmail(), requestDto.getPassword());
         memberRepository.save(member);
-
 
         Member saved = memberRepository.findByEmail(member.getEmail())
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.INTERNAL_SERVER_ERROR, "회원 저장 후 조회 실패"));
 
         return jwtProvider.createToken(saved);
+    }
+
+    public String login(MemberRequestDto requestDto) {
+        Member member = memberRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.FORBIDDEN, "이메일이 존재하지 않습니다."));
+
+        if (!member.getPassword().equals(requestDto.getPassword())) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN, "비밀번호가 일치하지 않습니다.");
+        }
+
+        return jwtProvider.createToken(member);
     }
 }
