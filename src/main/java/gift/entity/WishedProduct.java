b198061a76faf3a57a3477a4686a8b6d70a26202
Swallow Lee <lmh157@outlook.kr>
2025-07-08 23:56:40 +0900

    feat : WishedProduct의 entity 설계

---
+++ src/main/java/gift/entity/WishedProduct.java
@@ -0,0 +1,61 @@
+package gift.entity;
+
+public class WishedProduct {
+    private Long id;
+    private String name;
+    private Long price;
+    private String imageUrl;
+    private Integer quantity;
+
+    public WishedProduct(Long id, String name, Long price, String imageUrl, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getSubtotal() {
+        return price * quantity;
+    }
+}
