d2ce236c92807d2294f689758a6b4e8ca49a5ce0
uvrvuoon <0524yar@gmail.com>
2025-07-08 16:29:58 +0900

    feat: 와일드카드 안 쓰도록 변경

--- src/main/java/gift/JwtTokenProvider.java
+++ src/main/java/gift/JwtTokenProvider.java
@@ -1,6 +1,9 @@
 package gift;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
