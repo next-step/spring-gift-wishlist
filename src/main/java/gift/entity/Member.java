5ee5923516cbf7dba5521e648c69c85ccae64ad6
hhseo9519 <hhseo9519@naver.com>
2025-07-08 17:26:06 +0900

    feat : Member entity 추가

---
+++ src/main/java/gift/entity/Member.java
@@ -0,0 +1,47 @@
+package gift.entity;
+
+public class Member {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    public Member() {
+    }
+
+    public Member(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // getter & setter
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
