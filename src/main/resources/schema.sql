a75f24e3f3cd8516b3f6ac7f21fc7295b5af9dc7
Junhee <dkfl7190@naver.com>
2025-07-09 02:11:38 +0900

    feat: member 테이블 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE product
     image_url VARCHAR(2048) NOT NULL,
     status    VARCHAR(30)   NOT NULL
 );
+
+CREATE TABLE member
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email     VARCHAR(255)  NOT NULL UNIQUE,
+    password  VARCHAR(255)  NOT NULL,
+    role      VARCHAR(20)   NOT NULL
+);
