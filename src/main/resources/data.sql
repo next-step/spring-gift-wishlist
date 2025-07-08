9677c510b1e9f9ec27a3e270b47886d6eb9b1a78
Hyun <hyun_7586@naver.com>
2025-07-08 14:53:59 +0900

    chore: members table의 dummy data도 encoded password가 들어가도록 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO products(name, price, image_url) VALUES('p3', 3000, 'url3');
 INSERT INTO products(name, price, image_url) VALUES('p4', 4000, 'url4');
 INSERT INTO products(name, price, image_url) VALUES('p5', 5000, 'url5');
 
-INSERT INTO members(email, password) VALUES('member1', 'password1');
-INSERT INTO members(email, password) VALUES('member2', 'password2');
-INSERT INTO members(email, password) VALUES('member3', 'password3');
-INSERT INTO members(email, password) VALUES('member4', 'password4');
-INSERT INTO members(email, password) VALUES('member5', 'password5');
+INSERT INTO members(email, password) VALUES('member1', '$2a$10$141vsuswYHZmSMgqvTUP/u2sfY1881BOE32M7n8UdlHG/2XZ7IEuO');
+INSERT INTO members(email, password) VALUES('member2', '$2a$10$.2HSQpYl0kRbMqUrnb7V9eGJOimko1SIP0w.1TCN37LW0VNxbdzLq');
+INSERT INTO members(email, password) VALUES('member3', '$2a$10$Sy9sN2w9D66Tsdm3flXjQeEmb/6QhIZlSiKykUMZ9lLJrcgG/IdJ.');
+INSERT INTO members(email, password) VALUES('member4', '$2a$10$Xp52WG3ZkvthBuYk3r0Q6.58UAyX5qCoFymPG4.UYl6kQpxWSHWr2');
+INSERT INTO members(email, password) VALUES('member5', '$2a$10$cVqSwvAyY2P1RlJMyj3ZJeqcMHzpJPvky8PLf0n1qjyy9zzu6FLk.');
