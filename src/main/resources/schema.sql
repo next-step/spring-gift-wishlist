26c921238da9b3ab36026150088c0a21fdfc01bf
eastgyu11 <eastgyu11>
2025-07-08 15:35:03 +0900

    feat: 이메일, 비밀번호 길이 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE products (
 
 CREATE TABLE members (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
 );
