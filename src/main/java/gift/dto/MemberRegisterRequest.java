894c8320ce4021ee56a6a7c70eb6b685c48a5cd1
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-07-08 19:03:22 +0900

    feat(member): 회원가입 요청 DTO 추가 및 등록 로직 구현

---
+++ src/main/java/gift/dto/MemberRegisterRequest.java
@@ -0,0 +1,17 @@
+package gift.dto;
+
+public class MemberResgisterRequest {
+    private String email;
+    private String password;
+
+    public MemberResgisterRequest(){
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
