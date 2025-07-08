05a0c0309a97b91120b01463158d5fe890ae57a0
JAEIL1999 <pkyj040410@gmail.com>
2025-07-08 11:12:11 +0900

    step-02 user model, product model 구성

--- src/main/java/gift/model/product.java
+++ src/main/java/gift/model/product.java
@@ -1,4 +1,22 @@
 package gift.model;
 
 public class product {
+    private Long id;
+    private String name;
+    private int price;
+    private boolean usableKakao;
+    private String imageUrl;
+
+    //Getters and Setters
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getPrice() {return price;}
+    public void setPrice(int price) {this.price = price;}
+    public boolean isUsableKakao() {return usableKakao;}
+    public void setUsableKakao(boolean usableKakao) {this.usableKakao = usableKakao;}
+    public String getImageUrl() {return imageUrl;}
+    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+
 }
--- src/main/java/gift/model/user.java
+++ src/main/java/gift/model/user.java
@@ -1,4 +1,26 @@
 package gift.model;
 
+import java.time.LocalDateTime;
+
 public class user {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    //Getters and Setters
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
+    public void setUpdatedAt(LocalDateTime updatedAt) {this.updatedAt = updatedAt;}
 }
