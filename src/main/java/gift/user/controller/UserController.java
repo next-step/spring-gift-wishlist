859dd2b6457ac2d170019665e1b9eb8faceeb0bb
Dohyun <sks020k@naver.com>
2025-07-08 16:09:14 +0900

    feat : 비밀번호 validation을 위한 custom validator 추가

--- src/main/java/gift/user/controller/UserController.java
+++ src/main/java/gift/user/controller/UserController.java
@@ -5,9 +5,11 @@ import gift.user.dto.LoginResponseDto;
 import gift.user.dto.RegisterRequestDto;
 import gift.user.dto.RegisterResponseDto;
 import gift.user.service.UserService;
+import gift.validation.PasswordValidator;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService userService;
+  private final PasswordValidator passwordValidator;
 
-  public UserController(UserService userService) {
+  public UserController(UserService userService, PasswordValidator passwordValidator) {
     this.userService = userService;
+    this.passwordValidator = passwordValidator;
   }
 
   @PostMapping("/register")
   public ResponseEntity<RegisterResponseDto> registerUser(
-      @Valid @RequestBody RegisterRequestDto registerRequestDto) {
+      @Valid @RequestBody RegisterRequestDto registerRequestDto,
+      BindingResult bindingResult) {
+
+    // HACK : Controller에서 Getter을 사용하고 있다.
+    passwordValidator.validate(registerRequestDto.password(), bindingResult);
 
     return new ResponseEntity<RegisterResponseDto>(userService.registerUser(registerRequestDto),
         HttpStatus.CREATED);
--- src/main/java/gift/user/dto/LoginRequestDto.java
+++ src/main/java/gift/user/dto/LoginRequestDto.java
@@ -6,7 +6,6 @@ public record LoginRequestDto(
     @NotBlank(message = "이메일은 반드시 입력되어야 합니다.")
     String email,
 
-    @NotBlank(message = "비밀번호는 반드시 입력되어야 합니다.")
     String password
 ) {
 
