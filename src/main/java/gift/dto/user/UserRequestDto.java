04e51ce894bf6c4a488f0f85ca7ae6f612b1ebd9
JAEIL1999 <pkyj040410@gmail.com>
2025-07-08 11:40:00 +0900

    step-02 User DTO 구성

--- src/main/java/gift/dto/user/UserRequestDto.java
+++ src/main/java/gift/dto/user/UserRequestDto.java
@@ -1,4 +1,25 @@
 package gift.dto.user;
 
+import gift.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserRequestDto {
+
+    @NotBlank(message = "이메일 입력은 필수입니다.")
+    @Email(message = "이메일 형식을 맞춰주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자리를 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "사용자 이름 값 입력은 필수입니다.")
+    private String name;
+
+    public User toEntity() {
+        return new User(this.email, this.password, this.name);
+    }
+
 }
--- src/main/java/gift/dto/user/UserResponseDto.java
+++ src/main/java/gift/dto/user/UserResponseDto.java
@@ -1,4 +1,21 @@
 package gift.dto.user;
 
+import gift.model.User;
+
+import java.time.LocalDateTime;
+
 public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+
+    public static UserResponseDto fromEntity(User user) {
+        UserResponseDto dto = new UserResponseDto();
+        dto.id = user.getId();
+        dto.name = user.getName();
+        dto.email = user.getEmail();
+        dto.createdAt = user.getCreatedAt();
+        return dto;
+    }
 }
