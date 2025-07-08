56b68bbff556d2cece3f0bf83baa5506cd12c9f9
WonJuN Sun <swj010324@naver.com>
2025-07-08 14:56:39 +0900

    feat(service) Test code class for context loading and delete existing GiftTest to split existing test:

---
+++ src/test/java/gift/GiftApplicationTest.java
@@ -0,0 +1,12 @@
+package gift;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // Application 전체 context 로딩 확인
+  }
+}
