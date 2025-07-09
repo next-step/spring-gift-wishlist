aac230012f107037935e17467330f35843d21b7e
hopencode <newalfo1006@gmail.com>
2025-07-09 13:38:26 +0900

    refactor: RequestDto에 대해 setter 대신 @InitBinder를 사용해 불변성 보장

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
