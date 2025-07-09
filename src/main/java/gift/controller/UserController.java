ef4e492efc8087a0abb0a006920020aca1ca0dc5
uvrvuoon <0524yar@gmail.com>
2025-07-09 21:53:20 +0900

    feat: 회원가입의 상태코드를 201 CREATED로 변경, 컨트롤러 포맷을 Producdt와 같게 변경

--- src/main/java/gift/controller/UserController.java
+++ src/main/java/gift/controller/UserController.java
@@ -2,6 +2,7 @@ package gift.controller;
 
 import gift.dto.UserDto;
 import gift.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,16 +19,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    // 1. 회원 가입
     @PostMapping("/register")
-    public ResponseEntity<String> register(UserDto userDto) {
-        userService.register(userDto);
+    public ResponseEntity<UserDto> register(@RequestBody UserDto userDto) {
 
-        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+        UserDto response = userService.register(userDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    // 2. 로그인
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserDto user) throws IllegalAccessException {
-    // 요청으로 들어온 이메일, 비밀번호가 일치하면 JWT 토큰 발급
+
+        // 요청으로 들어온 이메일, 비밀번호가 일치하면 JWT 토큰 발급
         String token = userService.login(user.getEmail(), user.getPassword());
 
         return ResponseEntity.ok(token);
