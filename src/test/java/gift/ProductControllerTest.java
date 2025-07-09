b0814abd73974926552435e3a0511ca4470a9e6c
Lmmhhhh <lmh10087215@gmail.com>
2025-07-09 23:24:29 +0900

    test: ProductControllerTest WebMvcTest로 전환

--- src/test/java/gift/ProductControllerTest.java
+++ src/test/java/gift/ProductControllerTest.java
@@ -1,100 +1,76 @@
 package gift;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.controller.ProductController;
 import gift.dto.request.ProductRequest;
-import org.junit.jupiter.api.BeforeEach;
+import gift.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestClient;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(ProductController.class)
 public class ProductControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private RestClient.Builder builder;
+    private MockMvc mockMvc;
 
-    private RestClient client;
+    @MockitoBean
+    private ProductService productService;
 
-    @BeforeEach
-    void setUp() {
-        this.client = builder.defaultStatusHandler(response -> {
-                                return response.getStatusCode().is4xxClientError();
-                             })
-                             .build();
-    }
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final String PRODUCT_API = "/api/products";
-
-    private ProductRequest createProductRequest(String name,Integer price, String imageUrl){
-        return new ProductRequest(name,price,imageUrl);
-    }
-
-    private ResponseEntity<String> postProduct(ProductRequest request){
-        String url = "http://localhost:" + port + PRODUCT_API;
-        return client.post()
-                .uri(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(request)
-                .retrieve()
-                .toEntity(String.class);
-    }
-
-    private void assertBadRequest(ProductRequest request){
-        ResponseEntity<String> response = postProduct(request);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-    }
-
+    @DisplayName("상품명 15자 초과 시 400BAD_REQUEST 에러가 발생한다.")
     @Test
-    void 상품명_15자_초과시_400이_반환된다() throws Exception{
-        String url = "http://localhost:" + port + "/api/products";
+    void 상품명_15자_초과시_400이_반환된다() throws Exception {
 
-        ProductRequest request = createProductRequest(
+        ProductRequest request = new ProductRequest(
                 " 0123456789abcdef",
                 1000,
                 "http://image.jpg"
         );
 
-        ResponseEntity<String> response = postProduct(request);
-        assertBadRequest(request);
-        assertThat(response.getBody()).contains("상품명은 공백 포함 최대 15자까지 입력할 수 있습니다.");
+        assertBadRequest(request, "상품명은 공백 포함 최대 15자까지 입력할 수 있습니다.");
     }
 
+    @DisplayName("허용되지 않은 특수문자 입력 시 400BAD_REQUEST 에러가 발생한다.")
     @Test
-    void 허용되지않은_특수문자_입력시_400이_반환된다() throws Exception{
-        String url = "http://localhost:" + port + "/api/products";
+    void 허용되지_않은_특수문자_입력시_400이_반환된다() throws Exception{
 
-        ProductRequest request = createProductRequest(
-                "9abcdef!!!",
+        ProductRequest request = new ProductRequest(
+                " 012345!!!!",
                 1000,
-                "http://"
+                "http://image.jpg"
         );
 
-        ResponseEntity<String> response = postProduct(request);
-        assertBadRequest(request);
-        assertThat(response.getBody()).contains("상품명에는 특수문자 (),[],+,-,&,/,_ 만 포함될 수 있습니다.");
+        assertBadRequest(request,"상품명에는 특수문자 (),[],+,-,&,/,_ 만 포함될 수 있습니다.");
     }
 
+    @DisplayName("상품명에 '카카오' 포함 시 400BAD_REQUEST 에러가 발생한다.")
     @Test
-    void 상품명에_카카오_포함시_400이_반환된다() throws Exception {
-        String url = "http://localhost:" + port + "/api/products";
+    void 상품명에_카카오_포함시_400이_반환된다() throws Exception{
 
-        ProductRequest request = createProductRequest(
+        ProductRequest request = new ProductRequest(
                 "카카오97 초콜릿",
                 1000,
-                "http://"
+                "http://image.jpg"
         );
 
-        ResponseEntity<String> response = postProduct(request);
-        assertBadRequest(request);
-        assertThat(response.getBody()).contains("상품명에 '카카오'를 포함할 수 없습니다. 담당자에게 문의하세요.");
+        assertBadRequest(request,"상품명에 '카카오'를 포함할 수 없습니다. 담당자에게 문의하세요.");
+    }
+
+    private void assertBadRequest(ProductRequest request, String expectedMessage) throws Exception {
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.name[0]").value(expectedMessage));
     }
 }
