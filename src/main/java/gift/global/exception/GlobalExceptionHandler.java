b3b6722f2eadb44f434812b7021bf0020c5365b8
Hyun <hyun_7586@naver.com>
2025-07-10 00:50:25 +0900

    refactor: jwt 관련 예외 핸들러 추가

--- src/main/java/gift/global/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package gift.global.exception;
 
+import io.jsonwebtoken.JwtException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.dao.DataAccessException;
@@ -18,6 +19,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
             .body(new ErrorResponse(e.getErrorCode(), e.getMessage()));
     }
+
     @ExceptionHandler(InvalidMemberException.class)
     public ResponseEntity<?> handleInvalidMemberException(InvalidMemberException e) {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
@@ -26,22 +28,31 @@ public class GlobalExceptionHandler {
 
     // jdbcTemplate.query()의 결과값이 없을 때
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<?> handleDataAccessException(EmptyResultDataAccessException e){
+    public ResponseEntity<?> handleDataAccessException(EmptyResultDataAccessException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(new ErrorResponse(ErrorCode.NOT_EXISTS, ErrorCode.NOT_EXISTS.getErrorMessage()));
     }
 
     // validation 관련 예외
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
         List<ErrorResponse> errors = new ArrayList<>();
 
         e.getBindingResult().getAllErrors().forEach(error -> {
-            errors.add(new ErrorResponse(ErrorCode.INVALID_FORM_REQUEST, error.getDefaultMessage()));
+            errors.add(
+                new ErrorResponse(ErrorCode.INVALID_FORM_REQUEST, error.getDefaultMessage()));
         });
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(errors);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<?> handleJwtException(JwtException e) {
+        return ResponseEntity.status(ErrorCode.INVALID_TOKEN_REQUEST.getHttpStatus())
+            .body(new ErrorResponse(ErrorCode.INVALID_TOKEN_REQUEST,
+                ErrorCode.INVALID_TOKEN_REQUEST.getErrorMessage()));
     }
 
     @ExceptionHandler(Exception.class)
