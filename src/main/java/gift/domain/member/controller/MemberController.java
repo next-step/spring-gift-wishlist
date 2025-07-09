54974618ed846ff073d9ef43e48b7e7530480b21
milk-stone <ywshero@naver.com>
2025-07-09 15:52:02 -0700

    feat: 관리자 페이지에서 사용할 유저 정보 열람, 수정, 삭제 API 작성

--- src/main/java/gift/domain/member/controller/MemberController.java
+++ src/main/java/gift/domain/member/controller/MemberController.java
@@ -1,7 +1,15 @@
 package gift.domain.member.controller;
 
+import gift.domain.member.dto.MemberInfoListResponse;
+import gift.domain.member.dto.MemberInfoResponse;
+import gift.domain.member.dto.MemberInfoUpdateRequest;
 import gift.domain.member.service.MemberService;
+import gift.global.exception.TokenExpiredException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/members")
@@ -12,4 +20,33 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberInfoResponse> getMemberInfo(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String accessToken) throws TokenExpiredException {
+        System.out.println("accessToken: " + accessToken);
+        return new ResponseEntity<>(memberService.getMemberInfo(id, accessToken), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateMemberInfo(
+            @PathVariable Long id,
+            @RequestBody MemberInfoUpdateRequest request,
+            @RequestHeader("Authorization") String accessToken) throws TokenExpiredException {
+        memberService.updateMemberInfo(id, accessToken, request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMemberInfo(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String accessToken) throws TokenExpiredException {
+        memberService.deleteMemberInfo(id, accessToken);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<MemberInfoListResponse> getMemberInfos(@RequestHeader("Authorization") String accessToken) {
+        return new ResponseEntity<>(new MemberInfoListResponse(memberService.getMembers(accessToken)), HttpStatus.OK);
+    }
 }
--- src/main/java/gift/domain/member/repository/MemberRepository.java
+++ src/main/java/gift/domain/member/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package gift.domain.member.repository;
 
 import gift.domain.member.Member;
+import gift.domain.product.Product;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -34,11 +36,45 @@ public class MemberRepository {
     }
 
     public boolean existsByEmail(String email) {
-        String sql = "SELECT COUNT(*) FROM users WHERE email = :email";
+        String sql = "SELECT COUNT(*) FROM member WHERE email = :email";
         Integer count = jdbcClient.sql(sql)
                 .param("email", email)
                 .query(Integer.class)
                 .single();
         return count > 0;
+    }
+
+    public int update(Member member) {
+        String updateSql = "UPDATE member SET email = :email, password = :password, name = :name, role = :role WHERE id = :id";
+
+        return jdbcClient.sql(updateSql)
+                .param("id", member.getId())
+                .param("email", member.getEmail())
+                .param("password", member.getPassword())
+                .param("name", member.getName())
+                .param("role", member.getRole())
+                .update();
+    }
+
+    public Optional<Member> findById(Long id) {
+        String sql = "SELECT * FROM member WHERE id = :id";
+        return jdbcClient.sql(sql)
+                .param("id", id)
+                .query(Member.class)
+                .optional();
+    }
+
+    public void delete(Long id){
+        String sql = "DELETE FROM member WHERE id = :id";
+        jdbcClient.sql(sql)
+                .param("id", id)
+                .update();
+    }
+
+    public List<Member> getAll(){
+        String sql = "SELECT * FROM member";
+        return jdbcClient.sql(sql)
+                .query(Member.class)
+                .list();
     }
 }
