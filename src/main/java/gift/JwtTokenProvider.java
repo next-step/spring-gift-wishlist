d0bf6d25fdf842aece4ecba3658a5519c00e3d12
uvrvuoon <0524yar@gmail.com>
2025-07-10 01:10:37 +0900

    feat: Duration 적용

--- src/main/java/gift/JwtTokenProvider.java
+++ src/main/java/gift/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import java.time.Duration;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -14,13 +15,15 @@ import java.util.Date;
 public class JwtTokenProvider {
 
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 랜덤으로 비밀 키 생성
-    private final long validityInMilliseconds = 3600000; // 유효시간 1시간으로 설정
+
+    // 유효시간 1시간으로 설정
+    private final Duration validityInMilliseconds = Duration.ofHours(1);
 
     // 토큰 생성
     public String createToken(String email) {
         Claims claims = Jwts.claims().setSubject(email);
         Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        Date validity = new Date(now.getTime() + validityInMilliseconds.toMillis());
 
         return Jwts.builder()
                 .setClaims(claims)
