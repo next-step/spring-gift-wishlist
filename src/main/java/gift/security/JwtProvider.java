80fa783a97ed739a05ed8deb2a9887f680457f1c
sylimi2r2 <sylimi2r2@naver.com>
2025-07-09 15:59:45 +0900

    feat: JwtProvider 토큰 만료 시간 추가, 토큰 검증, 만료 여부 확인 기능 추가

--- src/main/java/gift/security/JwtProvider.java
+++ src/main/java/gift/security/JwtProvider.java
@@ -1,25 +1,66 @@
 package gift.security;
 
 import gift.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
+
 @Component
 public class JwtProvider {
 
-    private final String secretKey;
+    private static final long EXPIRE_TIME = 1000 * 60 * 30;
+
+    private final SecretKey secretKey;
 
     public JwtProvider(@Value("${jwt.secret.key}") String secretKey) {
-        this.secretKey = secretKey;
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
     public String generateToken(User user) {
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + EXPIRE_TIME);
+
         return Jwts.builder()
-                .setSubject(user.getId().toString())
+                .subject(user.getId().toString())
                 .claim("email", user.getEmail())
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .expiration(expireDate)
+                .signWith(secretKey)
                 .compact();
     }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Long getUserId(String token) {
+        return Long.getLong(getClaims(token).getSubject());
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expiration = getClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
 }
