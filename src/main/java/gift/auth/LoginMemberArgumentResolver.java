e9ec0b114668bdfd80f0027851220620ffd64cc7
sanghyeon1225 <oopt1234@naver.com>
2025-07-10 04:27:25 +0900

    feat: api 요청시 JWT 유효성 검사를 하도록 Interceptor, Resolver, Login 어노테이션 구현

---
+++ src/main/java/gift/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,31 @@
+package gift.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+
+        boolean isLongType = Long.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasLoginAnnotation && isLongType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        return request.getAttribute("memberId");
+    }
+}
