2102162c773d70381dfc19c45601daa4a5d10867
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-09 16:45:28 +0900

    feat: jwt 생성 기능 추가

---
+++ src/main/java/gift/jwt/JwtProvider.java
@@ -0,0 +1,23 @@
+package gift.jwt;
+
+import gift.member.entity.Member;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+    private final String secretKey = "내가하고싶은시크릿키step2를완료하기위해오늘도달린다화이팅";
+
+    public String generateToken(Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getEmail())
+                .claim("role", member.getRole())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+
+
+    }
+
+}
