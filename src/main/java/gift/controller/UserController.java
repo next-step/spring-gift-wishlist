d8ff56ae2f089232d752442f35adae8def410957
uvrvuoon <0524yar@gmail.com>
2025-07-09 16:14:14 +0900

    feat: '/' 추가, 예외 처리 변경

--- src/main/java/gift/controller/UserController.java
+++ src/main/java/gift/controller/UserController.java
@@ -25,8 +25,8 @@ public class UserController {
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
 
-    @PostMapping("login")
-    public ResponseEntity<String> login(@RequestBody UserDto user) {
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDto user) throws IllegalAccessException {
     // 요청으로 들어온 이메일, 비밀번호가 일치하면 JWT 토큰 발급
         String token = userService.login(user.getEmail(), user.getPassword());
 
