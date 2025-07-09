983ff85462d27895081bc6920c744bfb3011c06a
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-07-10 00:52:48 +0900

    feat(db) : 초기 데이터 삽입용 data.sql 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO products (name, price, image_url) VALUES ('사과', 3000, 'apple.jpg');
 INSERT INTO products (name, price, image_url) VALUES ('바나나', 1500, 'banana.jpg');
 INSERT INTO products (name, price, image_url) VALUES ('포도', 4000, 'grape.jpg');
+
+INSERT INTO products (name, price, image_url,status) VALUES ('딸기', 10000, 'grape.jpg','INACTIVE');
+
+INSERT INTO products (name, price, image_url,status) VALUES ('골든키위', 6000, 'grape.jpg','DISCONTINUED');
