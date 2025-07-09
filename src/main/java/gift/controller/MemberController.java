39255e0002c5ac7a59a3e0c91d14100e41e3fa29
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-10 03:55:43 +0900

    refactor(member): 회원 가입 api 응답 변경

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -5,6 +5,7 @@ import gift.dto.MemberLoginRequestDTO;
 import gift.dto.MemberRequestDTO;
 import gift.service.MemberService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class MemberController {
     @PostMapping("/register")
     public ResponseEntity<AuthTokenResponseDTO> register(@Valid @RequestBody MemberRequestDTO member) {
         AuthTokenResponseDTO token = memberService.register(member);
-        return ResponseEntity.status(201)
+        return ResponseEntity.status(HttpStatus.CREATED)
                 .body(token);
     }
 
