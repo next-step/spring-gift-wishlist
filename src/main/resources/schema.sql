806b80eaf0640b5c360795a9ef35b9a19ef9066f
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-09 16:44:44 +0900

    feat: member 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE product (
      price INT NOT NULL,
      image_url VARCHAR(255)
 );
+
+CREATE TABLE member(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(100) NOT NULL
+);
