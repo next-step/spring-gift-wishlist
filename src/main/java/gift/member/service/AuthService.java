7659fc20d43ca433204a6955291861d4431c3bbf
jihwan38 <sss203800@gmail.com>
2025-07-09 18:26:55 +0900

    refactor: 비밀번호 검증 로직 Member에게 위임

--- src/main/java/gift/member/service/AuthService.java
+++ src/main/java/gift/member/service/AuthService.java
@@ -1,34 +1,36 @@
 package gift.member.service;
 
 import gift.exception.InvalidLoginException;
+import gift.exception.MemberNotFoundByEmailException;
 import gift.member.dto.request.LoginRequestDto;
 import gift.member.dto.response.MemberResponseDto;
 import gift.member.dto.request.RegisterRequestDto;
 import gift.member.entity.Member;
 import gift.member.token.TokenProvider;
+import gift.member.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private final MemberService memberService;
     private final TokenProvider tokenProvider;
+    private final PasswordUtil passwordUtil;
 
-    public AuthService(MemberService memberService, TokenProvider tokenProvider) {
+    public AuthService(MemberService memberService, TokenProvider tokenProvider, PasswordUtil passwordUtil) {
         this.memberService = memberService;
         this.tokenProvider = tokenProvider;
+        this.passwordUtil = passwordUtil;
     }
 
     public String login(LoginRequestDto loginRequestDto) {
         try{
             Member member = memberService.getMemberByEmail(loginRequestDto.email());
 
-            if(!member.getPassword().equals(loginRequestDto.password())) {
-                throw new IllegalArgumentException();
-            }
+            member.verifyPassword(loginRequestDto.password(), passwordUtil);
 
             return tokenProvider.generateToken(MemberResponseDto.from(member));
 
-        }catch(IllegalArgumentException e){
+        }catch(IllegalArgumentException | MemberNotFoundByEmailException e){
             throw new InvalidLoginException();
         }
     }
