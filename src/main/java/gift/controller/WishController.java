fa956434419df529e219bed22af47d1b87b1d9b7
5seonjae <5seonjae.dev@gmail.com>
2025-07-09 21:55:52 +0900

    feat(wish): WishController REST API 엔드포인트 스켈레톤 추가
    - gift.controller 패키지에 WishController 클래스 생성 및 @RestController 적용
    - 클래스 레벨에 @RequestMapping("/api/wishes") 설정으로 기본 URL 경로 지정
    - 생성자 주입 방식으로 WishService 의존성 주입 구현

---
+++ src/main/java/gift/controller/WishController.java
@@ -0,0 +1,17 @@
+package gift.controller;
+
+import gift.service.WishService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/wishes")
+public class WishController {
+
+    private final WishService wishService;
+
+    public WishController(WishService wishService) {
+        this.wishService = wishService;
+    }
+
+}
