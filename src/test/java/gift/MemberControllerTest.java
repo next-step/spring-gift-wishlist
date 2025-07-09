0607fba126b4e1b23c08bc4625dbf5b3840ce907
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-07-09 15:32:24 +0900

    [Step2:feat] 로그인 관련 TestCase 추가

--- src/test/java/gift/MemberControllerTest.java
+++ src/test/java/gift/MemberControllerTest.java
@@ -62,4 +62,55 @@ public class MemberControllerTest {
       assertThat(ex.getResponseBodyAsString()).contains("이미 존재하는 이메일");
     }
   }
+
+  @Test
+  @DisplayName("[3] 정상 로그인 Test")
+  @Order(3)
+  void ValidTestLogin() {
+    var url = "http://localhost:" + port + "/api/members/login";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    String formData = "email=abc123@gmail.com&password=qwer1234!@";
+
+    ResponseEntity<String> response = client.post()
+        .uri(url)
+        .headers(h -> h.addAll(headers))
+        .body(formData)
+        .retrieve()
+        .toEntity(String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    String authHeader = response.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+    assertThat(authHeader).isNotNull();
+
+    assertThat(authHeader).startsWith("Bearer ");
+
+    String token = authHeader.substring(7); // "Bearer " 제거
+    assertThat(token).isNotBlank();
+  }
+
+  @Test
+  @DisplayName("[4] 비정상 로그인 Test")
+  @Order(4)
+  void inValidTestLogin() {
+    var url = "http://localhost:" + port + "/api/members/login";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    String formData = "email=abc123@gmail.com&password=qwer12345!@";
+
+    try{
+      client.post()
+          .uri(url)
+          .headers(h -> h.addAll(headers))
+          .body(formData)
+          .retrieve()
+          .toEntity(String.class);
+
+    }catch(RestClientResponseException ex){
+      assertThat(ex.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+  }
 }
