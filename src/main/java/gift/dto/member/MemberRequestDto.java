4f2dbaebbaf81f5286f71ed12ef360b4249473a4
eastgyu11 <eastgyu11>
2025-07-10 16:26:00 +0900

    refactor: requestDto에 필요 없는 setter 제거

--- src/main/java/gift/dto/member/MemberRequestDto.java
+++ src/main/java/gift/dto/member/MemberRequestDto.java
@@ -17,14 +17,6 @@ public class MemberRequestDto {
     this.password = password;
   }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
   public String getEmail() {
     return email;
   }
