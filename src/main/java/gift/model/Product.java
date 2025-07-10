0e266dc794f5c171393433ea6f032e94b6d4b021
JAEIL1999 <pkyj040410@gmail.com>
2025-07-10 23:07:30 +0900

    STEP-02 생성날짜, 업데이트날짜 생성

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,12 +1,27 @@
 package gift.model;
 
+import java.time.LocalDateTime;
+
 public class Product {
     private Long id;
     private String name;
     private int price;
     private boolean usableKakao;
     private String imageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public Product(String name, int price, boolean usableKakao, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.usableKakao = usableKakao;
+        this.imageUrl = imageUrl;
+    }
+    public Product(String name, int price, boolean usableKakao) {
+        this.name = name;
+        this.price = price;
+        this.usableKakao = usableKakao;
+    }
     //Getters and Setters
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
@@ -18,5 +33,9 @@ public class Product {
     public void setUsableKakao(boolean usableKakao) {this.usableKakao = usableKakao;}
     public String getImageUrl() {return imageUrl;}
     public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
+    public void setUpdatedAt(LocalDateTime updatedAt) {this.updatedAt = updatedAt;}
 
 }
