d7ef09c7fdddd259e73cc3aa887f7c468ca63206
박신영 <park_new0@naver.com>
2025-07-10 18:29:41 +0900

    feat(security): security filter chain 설정을 통해 API 엔드포인트 인증 필요유무 세팅

--- src/main/java/gift/config/SecurityConfig.java
+++ src/main/java/gift/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package gift.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/members/register", "/api/members/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
