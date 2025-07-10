5edd69755076d00cb6a84e449b9832c2b349125d
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-10 16:47:32 +0900

    feat(dto): DTO 필드 유효성 검증 메세지 추가

--- src/main/java/gift/dto/MemberLoginRequestDTO.java
+++ src/main/java/gift/dto/MemberLoginRequestDTO.java
@@ -1,6 +1,10 @@
 package gift.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record MemberLoginRequestDTO(
+        @NotBlank(message = "Email은 필수 입력 항목입니다.")
         String email,
+        @NotBlank(message = "Password는 필수 입력 항목입니다.")
         String password
 ) {}
--- src/main/java/gift/dto/MemberRequestDTO.java
+++ src/main/java/gift/dto/MemberRequestDTO.java
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberRequestDTO(
-        @NotBlank
+        @NotBlank(message = "Email은 필수 입력 항목입니다.")
         String email,
-        @NotBlank
+        @NotBlank(message = "Password는 필수 입력 항목입니다.")
         String password,
         @NotNull
         Role role
