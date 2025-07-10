5d8f8791511a02b03564c593ee7cd194dd144591
sanghyeon1225 <oopt1234@naver.com>
2025-07-10 23:06:05 +0900

    refactor: ArgumentResolver가 Member 객체를 반환하도록 수정

--- src/main/java/gift/auth/LoginMemberArgumentResolver.java
+++ src/main/java/gift/auth/LoginMemberArgumentResolver.java
@@ -1,5 +1,8 @@
 package gift.auth;
 
+import gift.entity.Member;
+import gift.exception.ResourceNotFoundException;
+import gift.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -11,13 +14,19 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
+    private final MemberRepository memberRepository;
+
+    public LoginMemberArgumentResolver(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
 
-        boolean isLongType = Long.class.isAssignableFrom(parameter.getParameterType());
+        boolean isMemberClass = Member.class.isAssignableFrom(parameter.getParameterType());
 
-        return hasLoginAnnotation && isLongType;
+        return hasLoginAnnotation && isMemberClass;
     }
 
     @Override
@@ -25,7 +34,9 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
             NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Long memberId = (Long) request.getAttribute("memberId");
 
-        return request.getAttribute("memberId");
+        return memberRepository.findMemberById(memberId)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 ID의 사용자를 찾을 수 없습니다: " + memberId));
     }
 }
