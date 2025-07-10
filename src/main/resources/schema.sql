7ff18d4d3cebdc7c19730550010b1cc1bcb20442
Junyoung Yang <y@jun0.dev>
2025-07-11 02:20:26 +0900

    feat: wishlist_item 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,7 +20,21 @@ CREATE TABLE refresh_token
 (
     token           CHAR(32) PRIMARY KEY,
     member_uuid     CHAR(36) NOT NULL,
-    created_at      DATETIME   NOT NULL,
-    expiration_date DATETIME   NOT NULL,
+    created_at      DATETIME NOT NULL,
+    expiration_date DATETIME NOT NULL,
     FOREIGN KEY (member_uuid) REFERENCES member (uuid) ON DELETE CASCADE
 );
+
+
+CREATE TABLE wishlist_item
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_uuid CHAR(36) NOT NULL,
+    product_id  BIGINT   NOT NULL,
+    quantity    INT      NOT NULL DEFAULT 1,
+    added_at    TIMESTAMP         DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_uuid) REFERENCES member (uuid) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE SET NULL,
+    UNIQUE (member_uuid, product_id)
+);
+//FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
