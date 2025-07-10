9781cc9cf880ff2b6bae49eab81577d5f9b357f3
kimpro4214 <qnfghk555@naver.com>
2025-07-10 18:23:58 +0900

    refactor: 전역 예외 처리 일관성을 위해 예외 클래스의 @ResponseStatus 제거

--- src/main/java/gift/exception/forbidden/EmailDuplicateException.java
+++ src/main/java/gift/exception/forbidden/EmailDuplicateException.java
@@ -1,9 +1,5 @@
 package gift.exception.forbidden;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class EmailDuplicateException extends RuntimeException {
     public EmailDuplicateException() {
         super("이미 존재하는 이메일입니다.");
--- src/main/java/gift/exception/forbidden/EmailNotFoundException.java
+++ src/main/java/gift/exception/forbidden/EmailNotFoundException.java
@@ -1,9 +1,5 @@
 package gift.exception.forbidden;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class EmailNotFoundException extends RuntimeException {
     public EmailNotFoundException() {
         super("존재하지 않는 이메일입니다.");
