8e65ae391fedda0a5d14cf52145e5f7415131c5c
YUNJAEGOONER <coygyj@gmail.com>
2025-07-11 01:49:42 +0900

    test: 장바구니에 대한 테스트 코드 작성

--- src/test/java/gift/controller/WishListControllerTest.java
+++ src/test/java/gift/controller/WishListControllerTest.java
@@ -1,13 +1,13 @@
 package gift.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import gift.dto.JwtResponseDto;
 import gift.dto.MemberRequestDto;
 import gift.dto.wish.WishRequestDto;
+import gift.dto.wish.WishResponseDto;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.http.client.HttpClientProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -22,35 +22,156 @@ class WishListControllerTest {
     private RestClient restClient = RestClient.builder().build();
 
     @Test
-    void 회원_가입_성공시_토큰이_반환() {
-        var url = "http://localhost:" + port + "/api/members/register";
+    void 장바구니에_상품을_추가하는_기능() {
+        String token = createMemberAndReturnToken("abc123@gmail.com", "12345678");
+        var url = "http://localhost:" + port + "/api/wishlist/add";
 
-        MemberRequestDto member1 = new MemberRequestDto("abc123@gmail.com", "password");
+        WishRequestDto wishRequestDto = new WishRequestDto(1L, 50);
 
-        var response = restClient.post()
+       var response = restClient.post()
                 .uri(url)
-                .body(member1)
+                .header("Authorization", token)
+                .body(wishRequestDto)
                 .retrieve()
-                .toEntity(String.class);
+               .toEntity(WishResponseDto.class);
 
-        System.out.println("createdToken = " + response.getBody());
+        System.out.println(response.toString());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
     @Test
-    void 장바구니에_상품을_추가하는_기능() {
-        var url = "http://localhost:" + port + "/api/wishList";
+    void 장바구니의_상품들을_조회하는_기능(){
+        String token = loginAndReturnToken("testuser1@kakao.com" , "12345678");
+        addProduct(1L, 5, token);
+        addProduct(3L, 10, token);
 
-        WishRequestDto wishRequestDto = new WishRequestDto(1L, 5);
+        var url = "http://localhost:" + port + "/api/wishlist";
 
+        var response = restClient.get()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        System.out.println(response.toString());
+        assertThat(response.getBody().size()).isEqualTo(2);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void 장바구니에_있는_상품을_삭제하는_기능(){
+        String token = loginAndReturnToken("testuser2@naver.com" , "12345678");
+
+        addProduct(1L, 5, token);
+        addProduct(3L, 10, token);
+        Long wishListId = addProduct(2L, 5, token);
+
+        var url = "http://localhost:" + port + "/api/wishlist";
+
+        var response = restClient.get()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        System.out.println("response = " + response);
+        assertThat(response.getBody().size()).isEqualTo(3);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        url = "http://localhost:" + port + "/api/wishlist/remove/" + wishListId.toString();
+
+        var deleteResponse = restClient.delete()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(Void.class);
+
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        url = "http://localhost:" + port + "/api/wishlist";
+
+        response = restClient.get()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        System.out.println("response = " + response);
+        assertThat(response.getBody().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 장바구니에서_상품을_추가하고_제거하는_기능(){
+        String token = loginAndReturnToken("testuser3@apple.com" , "12345678");
+        //상품 100개를 추가
+        Long wishlistId = addProduct(3L, 100, token);
+
+        var url = "http://localhost:" + port + "/api/wishlist/add/" + wishlistId.toString();
+
+        var response = restClient.patch()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        response = restClient.patch()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        System.out.println(response.getBody());
+        assertThat(response.getBody().size()).isEqualTo(1);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        url = "http://localhost:" + port + "/api/wishlist/subtract/" + wishlistId.toString();
+
+        response = restClient.patch()
+                .uri(url)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(List.class);
+
+        System.out.println(response.getBody());
+        assertThat(response.getBody().size()).isEqualTo(1);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    //회원등록
+    public String createMemberAndReturnToken(String email, String password){
+        var url = "http://localhost:" + port + "/api/members/register";
+        MemberRequestDto memberRequestDto = new MemberRequestDto(email, password);
         var response = restClient.post()
                 .uri(url)
-                .header("Authorization", "Bearer eyJhbGciOiJIUzM4NCJ9.eyJlbWFpbCI6ImFiYzEyM0BnbWFpbC5jb20iLCJtZW1iZXJJZCI6NH0.Dp-5yh3qQVTn7oLGwMnaVI4dLV5h9M5UG8aZBvMPXn0CWKu9U-XClswgiwy5xTCT")
-                .body(wishRequestDto)
+                .body(memberRequestDto)
                 .retrieve()
-                .toEntity(String.class);
-        System.out.println(response.getBody().toString());
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+                .toEntity(JwtResponseDto.class);
+        return response.getBody().token();
+    }
+
+    //로그인
+    public String loginAndReturnToken(String email, String password){
+        var url = "http://localhost:" + port + "/api/members/login";
+        MemberRequestDto memberRequestDto = new MemberRequestDto(email, password);
+        var response = restClient.post()
+                .uri(url)
+                .body(memberRequestDto)
+                .retrieve()
+                .toEntity(JwtResponseDto.class);
+        return response.getBody().token();
+    }
+
+    //상품추가
+    public Long addProduct(Long proudctId, Integer quantity, String token){
+        var url = "http://localhost:" + port + "/api/wishlist/add";
+        WishRequestDto wishRequestDto = new WishRequestDto(proudctId, quantity);
+        var response = restClient.post()
+                .uri(url)
+                .body(wishRequestDto)
+                .header("Authorization", token)
+                .retrieve()
+                .toEntity(WishResponseDto.class);
+        return response.getBody().wishListId();
     }
 
 
