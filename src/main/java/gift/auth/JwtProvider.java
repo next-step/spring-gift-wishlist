a197790312dfed589eb42c338394e2c11c5baae4
oh010 <oh010224@gmail.com>
2025-07-11 01:20:39 +0900

    refactor: 생성자 주입을 통한 비밀키 주입

--- src/main/java/gift/auth/JwtProvider.java
+++ src/main/java/gift/auth/JwtProvider.java
@@ -3,14 +3,18 @@ package gift.auth;
 import gift.user.domain.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 
 @Component
 public class JwtProvider {
-    private final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
-    private final SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private final SecretKey secretKey;
+
+    public JwtProvider(@Value("${jwt.secret}") String keyString) {
+        this.secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
+    }
     public String createToken(User user) {
         return Jwts.builder()
                 .claim("sub", user.getEmail())
