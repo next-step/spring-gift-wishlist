b667d40939eafec61ec1a2ad6f0389c8123060a9
sungchan <awdr1057@naver.com>
2025-07-10 16:07:34 +0900

    refactor: 외부 로직을 사용하는 getter 지양을 하기 위한 코드 리팩토링

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -4,25 +4,25 @@ import com.example.demo.dto.UserRequestDto;
 import com.example.demo.entity.User;
 import com.example.demo.exception.InvalidLoginException;
 import com.example.demo.exception.UserNotFoundException;
-import com.example.demo.jwt.PasswordHasher;
-import com.example.demo.repository.UserJdbcClientRepository;
+import com.example.demo.security.PasswordHasher;
+import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
-  private final UserJdbcClientRepository userJdbcClientRepository;
+  private final UserRepository userRepository;
 
-  public UserServiceImpl(UserJdbcClientRepository userJdbcClientRepository) {
-    this.userJdbcClientRepository = userJdbcClientRepository;
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
   @Override
   public User login(UserRequestDto dto) {
-    User user = userJdbcClientRepository.findByEmail(dto.getEmail())
-        .orElseThrow(() -> new InvalidLoginException("이메일이 일치하지 않습니다."));
+    User user = userRepository.findByEmail(dto.getEmail())
+                              .orElseThrow(() -> new InvalidLoginException("이메일이 일치하지 않습니다."));
 
     String hashedInput = PasswordHasher.hash(dto.getPassword());
-    if(!user.getPassword().equals(hashedInput)){
+    if(!user.isPasswordMatch(dto.getPassword())){
       throw new InvalidLoginException("비밀번호가 일치하지 않습니다.");
     }
     return user;
@@ -30,15 +30,15 @@ public class UserServiceImpl implements UserService{
 
   @Override
   public User findByEmail(String email) {
-    return userJdbcClientRepository.findByEmail(email)
-        .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+    return userRepository.findByEmail(email)
+                         .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
   }
 
   @Override
   public void deleteByEmail(String email) {
-    if(userJdbcClientRepository.findByEmail(email).isEmpty()) {
+    if(userRepository.findByEmail(email).isEmpty()) {
       throw new UserNotFoundException("사용자를 찾을 수 없습니다.");
     }
-    userJdbcClientRepository.deleteByEmail(email);
+    userRepository.deleteByEmail(email);
   }
 }
