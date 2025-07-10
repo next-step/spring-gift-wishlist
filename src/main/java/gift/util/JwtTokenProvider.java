14335102ab25adbe99c2208d602eede1647bda4d
Yunseong <dev.yunseong@gmail.com>
2025-07-10 12:07:44 +0900

    refactor secretKey를 객체 형태로 저장하도록 수정

--- src/main/java/gift/util/JwtTokenProvider.java
+++ src/main/java/gift/util/JwtTokenProvider.java
@@ -2,34 +2,36 @@ package gift.util;
 
 import gift.entity.Member;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+
 @Component
 public class JwtTokenProvider {
 
-    private final String secretKey;
+    private final SecretKey secretKey;
 
 
     public JwtTokenProvider(@Value("${jwt.secretKey}") String secretKey) {
-        this.secretKey = secretKey;
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+
     }
 
     public String makeJwtToken(Member member) {
         return Jwts.builder()
                 .subject(Long.toString(member.id()))
                 .claim("email", member.email())
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .signWith(secretKey)
                 .compact();
     }
 
     public JwtPayload parseJwtToken(String jwtToken) {
         JwtParser parser = Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .verifyWith(secretKey)
                 .build();
         Claims claims = (Claims) parser.parse(jwtToken).getPayload();
 
