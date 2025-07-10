1bc7ca37cdc163af4d29067d8f49f42e899c7df7
sungchan <awdr1057@naver.com>
2025-07-10 16:04:22 +0900

    refactor: refreshToken의 ServiceLayer 추가로 인한 코드 변경

--- src/main/java/com/example/demo/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/demo/repository/RefreshTokenRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import org.springframework.stereotype.Repository;
-
 public interface RefreshTokenRepository {
-  void saveRefreshToken(String email, String refreshToken);
-  String refreshTokenFindByEmail(String email);
-  void refreshTokenDeleteByEmail(String email);
+
+  void saveRefreshToken(Long id, String refreshToken);
+  boolean existsByUserId(Long id);
+  String refreshTokenFindByUserId(Long id);
+  void refreshTokenDeleteByUserId(Long id);
 }
