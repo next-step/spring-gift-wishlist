33cad2179632f391ba7d9230a6a1fd80ebe66be2
JAEIL1999 <pkyj040410@gmail.com>
2025-07-10 23:06:45 +0900

    STEP-02 setter 생성

--- src/main/java/gift/dto/product/ProductRequestDto.java
+++ src/main/java/gift/dto/product/ProductRequestDto.java
@@ -37,13 +37,13 @@ public class ProductRequestDto {
     }
 
     public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
+    public  void setId(Long id) {this.id = id;}
     public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public  void setName(String name) {this.name = name;}
     public int getPrice() {return price;}
-    public void setPrice(int price) {this.price = price;}
+    public  void setPrice(int price) {this.price = price;}
     public String getImageUrl() {return imageUrl;}
-    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+    public  void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
     public boolean getUsableKakao() {return usableKakao;}
     public void setUsableKakao(boolean usableKakao) {this.usableKakao = usableKakao;}
 }
