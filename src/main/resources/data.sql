faa56fc7c811a41f5d713ec218558c40d48a10cf
sungchan <awdr1057@naver.com>
2025-07-10 16:08:06 +0900

    feat: insert에 refreshtoken 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO product (name, price, image_url)
 VALUES ('딸기 케이크', 5500, 'https://example.com/strawberry.jpg');
 
-INSERT INTO users (email, password, role)
-VALUES ('example@example.com', 'f47c4b720cd560809b27592e4933f897170e7cfe999a3f80d04c7b2887aa8843', 'admin');
+INSERT INTO users (id, email, password, role)
+VALUES (1, 'example@example.com', 'f47c4b720cd560809b27592e4933f897170e7cfe999a3f80d04c7b2887aa8843', 'admin');
+
+INSERT INTO refresh_token (user_id, refresh_token)
+VALUES (1, 'some-refresh-token-value');
