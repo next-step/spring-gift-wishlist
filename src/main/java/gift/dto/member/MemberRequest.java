b048baaa88a5c73c2ca480a91d714878f92e214b
Hyun <hyun_7586@naver.com>
2025-07-10 22:01:51 +0900

    refactor: MemberRequest에 id 필드 추가

--- src/main/java/gift/dto/member/MemberRequest.java
+++ src/main/java/gift/dto/member/MemberRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record MemberRequest(
+    Long id,
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수값입니다.")
@@ -12,4 +13,9 @@ public record MemberRequest(
 
     @NotBlank(message = "비밀번호는 필수값입니다.")
     String password
-) {}
+) {
+
+    public static MemberRequest createForNewMemberForm(){
+        return new MemberRequest(null, null, null);
+    }
+}
