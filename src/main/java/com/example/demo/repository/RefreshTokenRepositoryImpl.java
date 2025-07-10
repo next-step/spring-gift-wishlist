12c4cdfab4ed54204f3d2d11aa9ef4785560d080
sungchan <awdr1057@naver.com>
2025-07-10 16:05:11 +0900

    refactor: refreshToken의 ServiceLayer 추가로 인한 코드 변경

--- src/main/java/com/example/demo/repository/RefreshTokenRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/RefreshTokenRepositoryImpl.java
@@ -4,52 +4,63 @@ import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RefreshTokenRepositoryImpl implements RefreshTokenRepository{
+public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
 
   private final JdbcClient jdbcClient;
 
   public RefreshTokenRepositoryImpl(JdbcClient jdbcClient) {
     this.jdbcClient = jdbcClient;
   }
-
   @Override
-  public void saveRefreshToken(String email, String refreshToken) {
-    String selectSql = "SELECT COUNT(*) FROM refresh_token WHERE email = :email";
-    Integer count = jdbcClient.sql(selectSql)
-                              .param("email", email)
+  public boolean existsByUserId(Long id) {
+    String sql = "SELECT COUNT(*) FROM refresh_token WHERE user_id = :userId";
+    Integer count = jdbcClient.sql(sql)
+                              .param("userId", id)
                               .query(Integer.class)
                               .single();
+    return count != null && count > 0;
+  }
 
-    if (count != null && count > 0) {
-      String updateSql = "UPDATE refresh_token SET refresh_token = :refresh_token WHERE email = :email";
-      jdbcClient.sql(updateSql)
-                .param("refresh_token", refreshToken)
-                .param("email", email)
-                .update();
+  @Override
+  public void saveRefreshToken(Long id, String refreshToken) {
+    if (existsByUserId(id)) {
+      updateRefreshToken(id, refreshToken);
     } else {
-      String insertSql = "INSERT INTO refresh_token(email, refresh_token) VALUES (:email, :refresh_token);";
-      jdbcClient.sql(insertSql)
-                .param("refresh_token", refreshToken)
-                .param("email", email)
-                .update();
+      insertRefreshToken(id, refreshToken);
     }
   }
 
-  @Override
-  public String refreshTokenFindByEmail(String email) {
-    String sql = "SELECT refresh_token FROM refresh_token WHERE email = :email";
+  private void updateRefreshToken(Long id, String refreshToken) {
+    String sql = "UPDATE refresh_token SET refresh_token = :refresh_token WHERE user_id = :userId";
+    jdbcClient.sql(sql)
+              .param("refresh_token", refreshToken)
+              .param("userId", id)
+              .update();
+  }
+
+  private void insertRefreshToken(Long id, String refreshToken) {
+    String sql = "INSERT INTO refresh_token(user_id, refresh_token) VALUES (:userId, :refresh_token)";
+    jdbcClient.sql(sql)
+              .param("userId", id)
+              .param("refresh_token", refreshToken)
+              .update();
+  }
+
+    @Override
+  public String refreshTokenFindByUserId(Long id) {
+    String sql = "SELECT refresh_token FROM refresh_token WHERE user_id = :userId";
     return jdbcClient.sql(sql)
-        .param("email", email)
-        .query(String.class)
-        .optional()
-        .orElse(null);
+                     .param("userId", id)
+                     .query(String.class)
+                     .optional()
+                     .orElse(null);
   }
 
   @Override
-  public void refreshTokenDeleteByEmail(String email) {
-    String sql = "DELETE FROM refresh_token WHERE email = :email";
+  public void refreshTokenDeleteByUserId(Long id) {
+    String sql = "DELETE FROM refresh_token WHERE user_id = :userId";
     jdbcClient.sql(sql)
-        .param("email", email)
-        .update();
+              .param("userId", id)
+              .update();
   }
 }
