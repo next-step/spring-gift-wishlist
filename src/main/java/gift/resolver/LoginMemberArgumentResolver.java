99477308eb94264234f83f5bf33b36860855e9e1
jnujh <jnu21jihoon@gmail.com>
2025-07-10 20:33:22 +0900

    feat: 인증된 사용자 정보를 주입하는 @LoginMember 기능 구현

---
+++ src/main/java/gift/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,45 @@
+package gift.resolver;
+
+import gift.annotation.LoginMember;
+import gift.domain.Member;
+import gift.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+
+    public LoginMemberArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginMember.class)
+                && parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = ((ServletWebRequest) webRequest).getRequest();
+        Object memberIdAttr = request.getAttribute("memberId");
+
+        if (memberIdAttr == null) {
+            throw new IllegalArgumentException("로그인 정보가 존재하지 않습니다.");
+        }
+
+        Long memberId = (Long) memberIdAttr;
+        return memberService.findById(memberId);
+    }
+}
