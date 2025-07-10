2d599ba61a2e03a27878297ab8764141789bd845
Hyun <hyun_7586@naver.com>
2025-07-10 22:07:58 +0900

    chore: HttpServletRequest, RequestAttribute에 사용할 상수 정의

--- src/main/java/gift/global/interceptor/LoginInterceptor.java
+++ src/main/java/gift/global/interceptor/LoginInterceptor.java
@@ -3,6 +3,7 @@ package gift.global.interceptor;
 import gift.global.exception.ErrorCode;
 import gift.global.exception.InvalidTokenException;
 import gift.global.jwt.JwtUtil;
+import gift.global.util.RequestAttributes;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
@@ -35,7 +36,7 @@ public class LoginInterceptor implements HandlerInterceptor {
         // 원래 member 객체나 memberId등 controller에서 필요한 정보들을 httpServlet에 넣어두고,
         // controller에서 @RequestAttribute("")로 전달받아서 사용하려고 했습니다.
         // 참고로 토큰 자체에 대한 유효성 검증은 jwtUtil.getSubject()에서 수행됩니다.
-        request.setAttribute("memberId", Long.parseLong(jwtUtil.getSubject(token)));
+        request.setAttribute(RequestAttributes.MEMBER_ID, Long.parseLong(jwtUtil.getSubject(token)));
 
         return true;
     }
---
+++ src/main/java/gift/global/util/RequestAttributes.java
@@ -0,0 +1,11 @@
+package gift.global.util;
+
+public final class RequestAttributes {
+
+    private RequestAttributes(){};
+
+    public static final String MEMBER_ID = "memberId";
+    // 이후 memberRole 필드 추가하면 사용 예정
+    public static final String MEMBER_ROLE="memberRole";
+
+}
