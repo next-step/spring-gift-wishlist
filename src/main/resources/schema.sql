3b92452b0ba776a65c2b399346f88d0e2cf6eba3
JAEIL1999 <pkyj040410@gmail.com>
2025-07-10 23:04:25 +0900

    STEP-02 테이블 이름 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE productDto (
+CREATE TABLE products (
                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(255),
                          price INT,
