1ad9fca4dad15d59d08eb5a0b33296a33f06313d
sungchan <awdr1057@naver.com>
2025-07-10 16:05:58 +0900

    refactor: 코드정리

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import java.util.Optional;
 
-public interface UserJdbcClientRepository {
+public interface UserRepository {
   Optional<User> findByEmail(String email);
   void saveUser(User user);
   void deleteByEmail(String email);
