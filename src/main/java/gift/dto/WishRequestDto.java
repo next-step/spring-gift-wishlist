ab339d6d5f0019a264b7e6a11f035cb69cc0b64c
seongwon02 <seongwon2303@gmail.com>
2025-07-11 16:45:33 +0900

    feat: 위시 상품 요청 시 유효성 검사 추가

--- src/main/java/gift/dto/WishRequestDto.java
+++ src/main/java/gift/dto/WishRequestDto.java
@@ -1,7 +1,10 @@
 package gift.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public class WishRequestDto {
 
+    @NotNull(message = "상품 ID는 필수입니다.")
     private final Long productId;
 
     public WishRequestDto(Long productId) {
