c42185ec5632fc536ed3713420c07565c45adbe4
KIYEONG <rldud1237@naver.com>
2025-07-11 14:25:59 +0900

    refactor: jbcrypt 라이브러리를 이용한 비밀번호 암호화 적용

--- src/main/resources/sql/dummy.sql
+++ src/main/resources/sql/dummy.sql
@@ -2,5 +2,5 @@ INSERT INTO products (name, price, image_url) VALUES ('라이언 인형', 25000,
 INSERT INTO products (name, price, image_url) VALUES ('춘식이 필통', 12000, 'https://example.com/choonsik-case.jpg');
 INSERT INTO products (name, price, image_url) VALUES ('어피치 머그컵', 15000, 'https://example.com/apeach-mug.jpg');
 
-INSERT INTO members (email, password, role) VALUES ('user@example.com', 'user1234', 'USER');
-INSERT INTO members (email, password, role) VALUES ('admin@example.com', 'admin1234', 'ADMIN');
+INSERT INTO members (email, password, role) VALUES ('user@example.com', '$2a$10$/yZS/3sclhWloFTyogEIY.GUv3mKAytUmyr29/5rtltVVCAbR.sTK', 'USER');
+INSERT INTO members (email, password, role) VALUES ('admin@example.com', '$2a$10$5QrNbYuaqZQgiRl50deLLOqIzpmWuiZM1YK2aaj9i6ooL7.JseKua', 'ADMIN');
