1a73f1fd8eeeaf95d1703ba8b5d658b65f912bff
sungchan <awdr1057@naver.com>
2025-07-11 19:05:37 +0900

    test: 패키지 구조 변경에 따른 테스트 코드 경로 및 의존성 수정

--- src/test/java/com/example/demo/user/UserLoginTest.java
+++ src/test/java/com/example/demo/user/UserLoginTest.java
@@ -3,11 +3,10 @@ package com.example.demo.user;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import com.example.demo.dto.UserDataInfo;
-import com.example.demo.dto.UserRequestDto;
+import com.example.demo.dto.user.UserDataInfo;
+import com.example.demo.dto.user.UserRequestDto;
 import com.example.demo.jwt.Jwt;
-import com.example.demo.security.PasswordHasher;
-import com.example.demo.service.UserService;
+import com.example.demo.service.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,6 +32,9 @@ class UserLoginTest {
   private UserService userService;
   @Test
   void 로그인_후_me_요청시_회원정보가_반환된다() {
+    // example@example.com 유저는 DB에 미리 insert되어 있다고 가정
+
+    // 1. 로그인 요청
     UserRequestDto dto = new UserRequestDto("example@example.com", "abcd1234!", "admin");
     String loginUrl = "http://localhost:" + port + "/login";
     ResponseEntity<Jwt> loginResponse = client.post()
@@ -43,7 +45,9 @@ class UserLoginTest {
                                               .toEntity(Jwt.class);
 
     String accessToken = loginResponse.getBody().getAccessToken();
+    System.out.println("AccessToken: " + accessToken);
 
+    // 2. /users/me 요청
     String meUrl = "http://localhost:" + port + "/users/me";
     ResponseEntity<UserDataInfo> meResponse = client.get()
                                                     .uri(meUrl)
@@ -51,9 +55,12 @@ class UserLoginTest {
                                                     .retrieve()
                                                     .toEntity(UserDataInfo.class);
 
+    // 3. 응답 검증
     assertThat(meResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(meResponse.getBody().email()).isEqualTo("example@example.com");
   }
+
+
 
 
   @Test
