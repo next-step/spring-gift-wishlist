0e753f1914988dababf16ce85528d31e25f06719
jihwan38 <sss203800@gmail.com>
2025-07-11 13:05:45 +0900

    fix: LoginMemberArgumentResolver Bean으로 등록하기 위해 @Component를 붙여줌

--- src/main/java/gift/member/config/LoginMemberArgumentResolver.java
+++ src/main/java/gift/member/config/LoginMemberArgumentResolver.java
@@ -6,11 +6,13 @@ import gift.member.repository.MemberRepository;
 import gift.member.token.LoginMember;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
     private final MemberRepository memberRepository;
 
@@ -27,7 +29,7 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        Long memberId = Long.parseLong(request.getParameter("memberId"));
+        Long memberId = (Long) request.getAttribute("memberId");
 
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundByIdException(memberId));
