8c3d2ac9daf5bab01bb33bc90e34791da514bd97
seongwon02 <seongwon2303@gmail.com>
2025-07-11 16:19:26 +0900

    feat: 토큰을 생성, 검증, 추출하는 클래스 생성, 비밀키와 토큰 만료 시간을 properties로 이동

---
+++ src/main/java/gift/util/JwtTokenProvider.java
@@ -0,0 +1,71 @@
+package gift.util;
+
+import gift.entity.Member;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey key;
+    private final long expirationTime;
+
+    public JwtTokenProvider(@Value("${jwt.secret.key}") String secretKey,
+                            @Value("${jwt.expiration.ms}") long expirationTime) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expirationTime = expirationTime;
+    }
+
+    public String getAccessToken(Member member) {
+
+        Date now = new Date(System.currentTimeMillis());
+        Date expiry = new Date(System.currentTimeMillis() + expirationTime);
+
+        return Jwts.builder()
+                .subject(member.getId().toString())
+                .claim("email", member.getEmail())
+                .claim("role", member.getRole().name())
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+    public String resolveAccessToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring((7));
+        }
+
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getIdFromToken(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
