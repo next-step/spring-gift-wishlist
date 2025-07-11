3eec2235c99bd978f30a7ed28e926048d586a8b8
xqqldir <2jihye10@kangwon.ac.kr>
2025-07-11 14:33:16 +0900

    [Feat] wishlist 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,13 @@ CREATE TABLE member
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE wishlist
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    UNIQUE (member_id, product_id)
+);
