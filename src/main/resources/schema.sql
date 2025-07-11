73775385203d8df9d50bb246d606fcbe82fbf49e
lwj0831 <wjl0831@gmail.com>
2025-07-11 16:23:02 +0900

    feat(wishlist): WishItem관련 SQL추가 및 memberAuth내 email UNIQUE 처리
    - memberAuth내 email 중복 허용하지 않도록 DB level에서 유니크 제약 조건
      추가
    - WishItem 테이블 생성 DDL, 더미 데이터 추가 SQL작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,7 +13,16 @@ CREATE TABLE member (
 
 CREATE TABLE member_auth (
     member_id BIGINT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     refresh_token VARCHAR(512)
+);
+
+CREATE TABLE wish_item (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id BIGINT,
+    product_id BIGINT,
+    CONSTRAINT uq_member_product UNIQUE (member_id, product_id),
+    CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member(id),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
 );
