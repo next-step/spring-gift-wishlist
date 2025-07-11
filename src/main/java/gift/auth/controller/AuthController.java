3cdd2c9f39390248575456d3ebf127ff4f4367d6
lwj0831 <wjl0831@gmail.com>
2025-07-11 17:20:05 +0900

    chore(auth): AuthController에서 '@Controller' -> '@RestController'로 변경

--- src/main/java/gift/auth/controller/AuthController.java
+++ src/main/java/gift/auth/controller/AuthController.java
@@ -8,12 +8,12 @@ import gift.auth.dto.TokenResponseDto;
 import gift.auth.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
