05c212529fc03cbfbeeb02fdcc5412d42408845e
seongwon02 <seongwon2303@gmail.com>
2025-07-11 16:24:09 +0900

    feat: 위시 컨트롤러 구현

---
+++ src/main/java/gift/controller/WishController.java
@@ -0,0 +1,50 @@
+package gift.controller;
+
+import gift.annotation.LoginMember;
+import gift.dto.WishRequestDto;
+import gift.dto.WishResponseDto;
+import gift.entity.Member;
+import gift.service.WishService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/wishes")
+public class WishController {
+
+    private final WishService wishService;
+
+    public WishController(WishService wishService) {
+        this.wishService = wishService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WishResponseDto>> findWishList(@LoginMember Member member) {
+
+        return ResponseEntity.ok(wishService.findWishList(member.getId()));
+    }
+
+    @PostMapping
+    public ResponseEntity<WishResponseDto> createWish(
+            @LoginMember Member member,
+            @RequestBody WishRequestDto dto
+            ) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(wishService.saveWish(member.getId(), dto));
+    }
+
+    @DeleteMapping("/{wishId}")
+    public ResponseEntity<Void> deleteWish(
+            @PathVariable Long wishId,
+            @LoginMember Member member
+    ) {
+        wishService.deleteWish(wishId, member);
+        return ResponseEntity.noContent().build();
+    }
+
+}
