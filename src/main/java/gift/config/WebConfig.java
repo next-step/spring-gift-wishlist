59c452758a176200011bedab43cedfb72f5ef149
thdgmlwns1 <xhrl233@naver.com>
2025-07-11 13:28:22 +0900

    Feat: HandlerMethodArgumentResolver 설정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -2,21 +2,30 @@ package gift.config;
 
 import gift.interceptor.AdminCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final AdminCheckInterceptor adminCheckInterceptor;
+    private final LoginMemberArgumentResolver resolver;
 
-    public WebConfig(AdminCheckInterceptor adminCheckInterceptor) {
+    public WebConfig(AdminCheckInterceptor adminCheckInterceptor, LoginMemberArgumentResolver resolver) {
         this.adminCheckInterceptor = adminCheckInterceptor;
+        this.resolver = resolver;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminCheckInterceptor)
                 .addPathPatterns("/admin/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(resolver);
     }
 }
