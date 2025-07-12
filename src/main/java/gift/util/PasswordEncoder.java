97314dc5d3c011715810e8aabe634519e87c3e5c
KNU-K <knukang334@gmail.com>
2025-07-13 02:42:55 +0900

    feat: PasswordEncoder 클래스 추가 - 비밀번호 해시 및 검증 기능 구현

--- src/main/java/gift/util/PasswordEncoder.java
+++ src/main/java/gift/util/PasswordEncoder.java
@@ -2,7 +2,7 @@ package gift.util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-public class PasswordUtil {
+public class PasswordEncoder {
     public static String hashPassword(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
