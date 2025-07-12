c9110f79bf48d6db67d901a70ccbb50696ced9e0
Junhee <dkfl7190@naver.com>
2025-07-13 01:02:26 +0900

    refactor: Wish 테이블 개선

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,6 +20,7 @@ CREATE TABLE wish
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     member_id   BIGINT NOT NULL,
     product_id  BIGINT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    UNIQUE (member_id, product_id)
 );
