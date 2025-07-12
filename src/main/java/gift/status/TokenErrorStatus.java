989e95652613a47951688f16899e8e01b76c87a3
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-12 11:48:48 +0900

    Feat: Token 이 없을 경우를 대비한 Exception 메세지 설정

--- src/main/java/gift/status/TokenErrorStatus.java
+++ src/main/java/gift/status/TokenErrorStatus.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum TokenErrorStatus {
     TOKEN_EXPIRED("TE001", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
-    INVALID_TOKEN_TYPE("TE002", "토큰 타입이 올바르지 않습니다.", HttpStatus.BAD_REQUEST);
+    INVALID_TOKEN_TYPE("TE002", "토큰 타입이 올바르지 않습니다.", HttpStatus.BAD_REQUEST),
+    NO_TOKEN("TE003", "토큰이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED);
 
     private final String code;
     private final String message;
