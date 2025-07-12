f020eb924b8155a03d2afdc694cb2f00cbc37187
milk-stone <ywshero@naver.com>
2025-07-12 11:49:20 -0700

    feat: Wish 엔티티 구현 및 schema.sql에 테이블 생성 쿼리 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 -- 기존에 테이블이 있다면 삭제
+DROP TABLE IF EXISTS wish;
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS member;
 
 -- product 테이블 생성
 CREATE TABLE product (
@@ -16,4 +18,14 @@ CREATE TABLE member (
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     role VARCHAR(10) NOT NULL
+);
+
+-- wish 테이블 생성
+CREATE TABLE wish (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
