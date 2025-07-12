7af501aff9b7bd2d2d6c61f0492dd0b6aa3ac3e6
happyGogildong <rbgus630@gmail.com>
2025-07-12 16:34:03 +0900

    <feat>(domain): wish_list 테이블 ddl 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS "user";
 
 CREATE TABLE product
 (
-    productId BIGINT PRIMARY KEY,
-    name      VARCHAR(255) NOT NULL,
-    price     INTEGER      NOT NULL,
-    imageURL  VARCHAR(500)
+    product_id BIGINT PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    price      INTEGER      NOT NULL,
+    image_url  VARCHAR(500)
 
 );
 
@@ -18,4 +18,11 @@ CREATE TABLE users
     password  VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE wish_list
+(
+    wish_id    BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id    BIGINT REFERENCES users (user_id),
+    product_id BIGINT REFERENCES product (product_id)
+);
 
+CREATE INDEX user_idx ON wish_list (user_id);
