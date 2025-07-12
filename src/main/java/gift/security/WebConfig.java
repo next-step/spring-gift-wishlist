2c01825d30716128ed73629b21a2b5612397eed1
WonJuN Sun <swj010324@naver.com>
2025-07-13 02:17:00 +0900

    feat(security) : create a configuration class that enable spring mvc to utilize LoginMemberIdArgumentResolver

---
+++ src/main/java/gift/security/WebConfig.java
@@ -0,0 +1,21 @@
+package gift.security;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final LoginMemberIdArgumentResolver loginMemberIdArgumentResolver;
+
+  public WebConfig(LoginMemberIdArgumentResolver loginMemberIdArgumentResolver) {
+    this.loginMemberIdArgumentResolver = loginMemberIdArgumentResolver;
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(loginMemberIdArgumentResolver);
+  }
+}
