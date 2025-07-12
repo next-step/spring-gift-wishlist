b65f9a5fb3160cfba94dbe0cf83512b72acad508
kimpro4214 <qnfghk555@naver.com>
2025-07-12 18:07:17 +0900

    refactor: record 변경으로 인한 코드 수정

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -23,20 +23,20 @@ public class MemberService {
     }
 
     public void register(MemberRequestDto dto) {
-        if (memberRepository.findByEmail(dto.getEmail()).isPresent()) {
+        if (memberRepository.findByEmail(dto.email()).isPresent()) {
             throw new EmailDuplicateException();
         }
 
-        String hashedPassword = BCrypt.hashpw(dto.getPassword(), BCrypt.gensalt());
-        Member member = new Member(null, dto.getEmail(), hashedPassword, "USER");
+        String hashedPassword = BCrypt.hashpw(dto.password(), BCrypt.gensalt());
+        Member member = new Member(null, dto.email(), hashedPassword, "USER");
         memberRepository.save(member);
     }
 
     public String login(MemberRequestDto dto) {
-        Member member = memberRepository.findByEmail(dto.getEmail())
+        Member member = memberRepository.findByEmail(dto.email())
                 .orElseThrow(EmailNotFoundException::new);
 
-        if (!BCrypt.checkpw(dto.getPassword(), member.getPassword())) {
+        if (!BCrypt.checkpw(dto.password(), member.getPassword())) {
             throw new WrongPasswordException();
         }
 
