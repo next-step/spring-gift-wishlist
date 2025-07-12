dc7db48b318e195c0f4a93590be5942277dabba8
YoonGangHoon <yghun021007@naver.com>
2025-07-12 23:16:13 +0900

    refactor: 사용되지 않는 코드 제거

--- src/main/java/gift/config/LoginMemberArgumentResolver.java
+++ src/main/java/gift/config/LoginMemberArgumentResolver.java
@@ -1,9 +1,7 @@
 package gift.config;
 
-import gift.dto.MemberResponseDto;
 import gift.entity.Member;
 import gift.exception.UnauthorizedAccessException;
-import gift.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.NonNull;
@@ -13,12 +11,6 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
-
-    private final MemberService memberService;
-
-    public LoginMemberArgumentResolver(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -29,6 +29,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver(memberService));
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
