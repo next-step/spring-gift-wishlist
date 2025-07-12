ba1d7da683551bac68e8e6a00ef546c69968493a
KNU-K <knukang334@gmail.com>
2025-07-13 02:35:15 +0900

    feat: 회원 등록 응답 클래스 추가 - 토큰 및 사용자 정보를 포함하는 응답 구조 구현

---
+++ src/main/java/gift/controller/MemberRegisterResponse.java
@@ -0,0 +1,15 @@
+package gift.controller;
+
+import gift.domain.Member;
+
+public record MemberRegisterResponse(
+        String token,
+        MemberResponse user
+) {
+
+
+    public static MemberRegisterResponse of(String token, Member member) {
+        MemberResponse memberResponse = MemberResponse.from(member);
+        return new MemberRegisterResponse(token,memberResponse);
+    }
+}
