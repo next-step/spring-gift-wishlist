769eb663594cae061f820f715ec352cf12e38090
KNU-K <knukang334@gmail.com>
2025-07-13 02:35:46 +0900

    feat: MemberService 클래스 수정 - 비밀번호 해시 처리 및 JWT 토큰 생성 로직 통합

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,56 +1,53 @@
 package gift.service;
 
+import gift.controller.MemberRegisterResponse;
+import gift.controller.MemberResponse;
+import gift.domain.Member;
 import gift.dto.AuthorizationRequest;
 import gift.dto.AuthorizationResponse;
-import gift.domain.Member;
 import gift.exception.BusinessException;
 import gift.exception.ErrorCode;
 import gift.repository.MemberRepository;
-import gift.util.JwtUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import gift.util.JwtProvider;
+import gift.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
+    private final JwtProvider jwtProvider;
+    public MemberService(MemberRepository memberRepository, JwtProvider jwtProvider) {
         this.memberRepository = memberRepository;
+        this.jwtProvider = jwtProvider;
     }
 
-    public AuthorizationResponse register(AuthorizationRequest request) {
+    public MemberRegisterResponse register(AuthorizationRequest request) {
+        String hashedPassword = PasswordUtil.hashPassword(request.password());
         Member member = Member.of(
                 request.email(),
-                request.password()
+                hashedPassword
         );
         Member savedMember = memberRepository.save(member);
 
-        String token = generateToken(savedMember.email());
-
-        return AuthorizationResponse.of(token);
+        String token = jwtProvider.generateToken(savedMember.email());
+        return MemberRegisterResponse.of(token, member);
     }
 
     public AuthorizationResponse login(AuthorizationRequest request) {
         String email = request.email();
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(()-> new BusinessException(ErrorCode.USER_EMAIL_NOT_FOUND));
-        if(!member.isMatchPassword(request.password())){
+        if(!PasswordUtil.checkPassword(request.password(), member.password())){
             throw new BusinessException(ErrorCode.USER_PASSWORD_MISMATCH);
         }
 
-        String token = generateToken(member.email());
+        String token = jwtProvider.generateToken(member.email());
         return AuthorizationResponse.of(token);
     }
 
-    private String generateToken(String email){
-        Claims claims = generateClaims(email);
-        return JwtUtil.generateToken(claims);
-    }
-
-    private Claims generateClaims(String email){
-        return Jwts.claims()
-                .subject(email)
-                .build();
+    public MemberResponse getById(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(()-> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        return MemberResponse.from(member);
     }
 }
