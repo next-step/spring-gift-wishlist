6a41db53d07548b552b1ad06d2b7e3795ec3f952
KNU-K <knukang334@gmail.com>
2025-07-13 02:35:10 +0900

    feat: 회원 조회 기능 추가 - 특정 회원 정보를 가져오는 API 구현

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -3,7 +3,10 @@ package gift.controller;
 import gift.dto.AuthorizationRequest;
 import gift.dto.AuthorizationResponse;
 import gift.service.MemberService;
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +20,19 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberResponse> getById(@PathVariable("memberId") Long memberId){
+        MemberResponse response = memberService.getById(memberId);
 
+        return ResponseEntity.ok()
+                .body(response);
+    }
     @PostMapping("/register")
-    public ResponseEntity<AuthorizationResponse> register(@RequestBody AuthorizationRequest request){
-        AuthorizationResponse response = memberService.register(request);
+    public ResponseEntity<MemberRegisterResponse> register(@RequestBody AuthorizationRequest request) {
+        MemberRegisterResponse response = memberService.register(request);
 
-        return ResponseEntity.created(null)
+        URI location = URI.create("/api/v1/members/" + response.user().id());
+        return ResponseEntity.created(location)
                 .body(response);
     }
 
