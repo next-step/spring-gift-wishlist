4c93716cbe4ffb3186bc3197bebcdea9827f0539
uvrvuoon <0524yar@gmail.com>
2025-07-14 01:23:06 +0900

    feat: HttpHeaders에 정의되어 있는 값 사용하도록 변경

--- src/main/java/gift/jwt/JwtAuthenticationFilter.java
+++ src/main/java/gift/jwt/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -28,7 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain)
             throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7); // "Bearer " 제거
