9d152ad4bc773f83d2e45372c16a6609e89e8287
uvrvuoon <0524yar@gmail.com>
2025-07-14 01:14:20 +0900

    feat: wish dto에 생성자, getter 메서드 추가

--- src/main/java/gift/dto/CreateWishRequest.java
+++ src/main/java/gift/dto/CreateWishRequest.java
@@ -2,6 +2,19 @@ package gift.dto;
 
 public class CreateWishRequest {
 
-    public Long productId;
+    Long productId;
+    Long memberId;
 
+    public CreateWishRequest(Long productId, Long memberId) {
+        this.productId = productId;
+        this.memberId = memberId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
 }
--- src/main/java/gift/dto/CreateWishResponse.java
+++ src/main/java/gift/dto/CreateWishResponse.java
@@ -4,5 +4,13 @@ public class CreateWishResponse {
 
     Long wishId;
     Long productId;
-    Long memberId;
+
+    public CreateWishResponse(Long wishId, Long productId) {
+        this.wishId = wishId;
+        this.productId = productId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
 }
