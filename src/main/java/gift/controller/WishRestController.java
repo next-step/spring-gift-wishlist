6a6ab38425f0e7bc3e15bb5ccbdef151bc794ac0
uvrvuoon <0524yar@gmail.com>
2025-07-14 01:38:53 +0900

    feat: wish를 path를 통해 받은 id로 삭제하도록 변경 & addWish의 반환 타입을 wish dto로 변경

--- src/main/java/gift/controller/WishRestController.java
+++ src/main/java/gift/controller/WishRestController.java
@@ -1,6 +1,7 @@
 package gift.controller;
 
 import gift.dto.CreateWishRequest;
+import gift.dto.CreateWishResponse;
 import gift.dto.LoginMember;
 import gift.entity.Product;
 import gift.service.WishService;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,17 +27,17 @@ public class WishRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> addWish(LoginMember member,
+    public ResponseEntity<CreateWishResponse> addWish(LoginMember member,
             @RequestBody CreateWishRequest request) {
         wishService.addWish(member.getId(), request.getProductId());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Object> deleteWish(LoginMember member,
-            @RequestBody CreateWishRequest request) {
-        wishService.removeWish(member.getId(), request.getProductId());
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWish(LoginMember member,
+            @PathVariable Long productId) {
+        wishService.removeWish(member.getId(), productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
