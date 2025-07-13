af20cdd4609a04b34b0e3e1f67644e55d0d71c7f
sungchan <awdr1057@naver.com>
2025-07-13 18:53:07 +0900

    refactor: 기본생성자 제거

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -23,7 +23,6 @@ public class User {
     this.role = role;
   }
 
-
   public static User createWithRawPassword(Long id, String email, String rawPassword, String role) {
     return new User(id, email, PasswordHasher.hash(rawPassword), role);
   }
@@ -31,7 +30,6 @@ public class User {
   public void changePassword(String rawPassword) {
     this.password = PasswordHasher.hash(rawPassword);
   }
-
 
   public boolean isPasswordMatch(String rawInput) {
     return this.password.equals(PasswordHasher.hash(rawInput));
@@ -60,12 +58,4 @@ public class User {
   public void setEmail(String email) {
     this.email = email;
   }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
 }
