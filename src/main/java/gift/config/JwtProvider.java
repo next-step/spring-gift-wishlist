9ca9068a327dc9f74ced9d9065262c5dac8b2bda
Junhee <dkfl7190@naver.com>
2025-07-13 12:27:53 +0900

    feat: JWT에 발급자, 만료 시간 추가

--- src/main/java/gift/config/JwtProvider.java
+++ src/main/java/gift/config/JwtProvider.java
@@ -5,20 +5,29 @@ import gift.exception.UnAuthenticationException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.time.Instant;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProvider {
 
+    private static final long EXPIRE_TIME = 60 * 60 * 1000L;
+
     @Value("${jwt.secret.key}")
     private String secretKey;
 
     public String generateToken(Long id, MemberRole role) {
 
+        Instant now = Instant.now();
+        Date expirationDate = Date.from(now.plusMillis(EXPIRE_TIME));
+
         return Jwts.builder()
                    .subject(id.toString())
                    .claim("role", role.name())
+                   .issuer("spring-gift")
+                   .expiration(expirationDate)
                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
                    .compact();
     }
@@ -27,6 +36,7 @@ public class JwtProvider {
         try {
             return Jwts.parser()
                        .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                       .requireIssuer("spring-gift")
                        .build()
                        .parseSignedClaims(token)
                        .getPayload();
