3b01d64ae036aa6a9b20cba9f6c05ed71c8d1e25
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-13 15:37:49 +0900

    feat: 수량 추가

--- src/main/java/gift/domain/Wish.java
+++ src/main/java/gift/domain/Wish.java
@@ -4,13 +4,15 @@ public class Wish {
     private Long id;
     private Long memberId;
     private Long productId;
+    private int quantity;
 
     public Wish() {}
 
-    public Wish(Long id, Long memberId, Long productId) {
+    public Wish(Long id, Long memberId, Long productId, int quantity) {
         this.id = id;
         this.memberId = memberId;
         this.productId = productId;
+        this.quantity = quantity;
     }
 
     public Long getId() { return id; }
@@ -19,4 +21,12 @@ public class Wish {
     public void setMemberId(Long memberId) { this.memberId = memberId; }
     public Long getProductId() { return productId; }
     public void setProductId(Long productId) { this.productId = productId; }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
