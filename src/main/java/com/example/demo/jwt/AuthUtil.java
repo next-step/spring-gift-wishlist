a84444a02c49abbb8c5e5923982ee06c79b4b906
sungchan <awdr1057@naver.com>
2025-07-13 18:54:07 +0900

    refactor: try-catch 문 보완

--- src/main/java/com/example/demo/jwt/AuthUtil.java
+++ src/main/java/com/example/demo/jwt/AuthUtil.java
@@ -2,27 +2,28 @@ package com.example.demo.jwt;
 
 import com.example.demo.entity.User;
 import com.example.demo.exception.UnauthorizedException;
-import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.service.user.UserService;
 import org.springframework.http.HttpHeaders;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 public class AuthUtil {
 
-  public static User extractUserFromRequest(HttpServletRequest request, JwtProvider jwtProvider, UserService userService) {
+  public static User extractUserFromRequest(HttpServletRequest request, JwtProvider jwtProvider,
+      UserService userService) {
     String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       throw new UnauthorizedException("AccessToken이 없습니다.");
     }
 
+    String token = authHeader.replace("Bearer ", "").trim();
+    String email = extractEmailFromToken(token, jwtProvider);
+    return userService.findByEmail(email);
+  }
+
+  private static String extractEmailFromToken(String token, JwtProvider jwtProvider) {
     try {
-      String token = authHeader.replace("Bearer ", "").trim();
-      String email = jwtProvider.getClaims(token).get("email", String.class);
-      return userService.findByEmail(email);
-    } catch (UserNotFoundException e) {
-      throw e;
+      return jwtProvider.getClaims(token).get("email", String.class);
     } catch (Exception e) {
       throw new UnauthorizedException("AccessToken이 유효하지 않습니다.");
     }
