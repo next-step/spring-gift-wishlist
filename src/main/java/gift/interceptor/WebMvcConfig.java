1c043d7d21833046a5f3308b817751c26fdfca94
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-14 16:23:32 +0900

    feat: argumentResolver 설정 추가

--- src/main/java/gift/interceptor/WebMvcConfig.java
+++ src/main/java/gift/interceptor/WebMvcConfig.java
@@ -1,20 +1,34 @@
 package gift.interceptor;
 
+import gift.auth.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final Interceptor interceptor;
-    public WebMvcConfig(Interceptor interceptor) {
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    public WebMvcConfig(Interceptor interceptor,
+                        LoginMemberArgumentResolver loginMemberArgumentResolver) {
         this.interceptor = interceptor;
+        this.loginMemberArgumentResolver = loginMemberArgumentResolver;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/api/products/**",
                 "/api/wishlists/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
     }
 
 }
