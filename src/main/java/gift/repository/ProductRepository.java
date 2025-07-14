c28060355ff34990821a30a131e537ba3fe6de8a
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-14 15:51:21 +0900

    refactor(repository): 상품 저장 시 KeyHolder로 생성 ID 반환

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -2,9 +2,14 @@ package gift.repository;
 
 import gift.entity.Product;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -16,7 +21,16 @@ public class ProductRepository {
     }
 
     public Product save(Product product) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         String sql = "INSERT INTO products(name, price, image_url) VALUES (?, ?, ?)";
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, product.getName());
+            ps.setLong(2, product.getPrice().longValue());
+            ps.setString(3, product.getImageUrl());
+            return ps;
+        });
         jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
         return product;
     }
