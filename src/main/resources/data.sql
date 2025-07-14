359bc1f68421ba260310cc7c84eebb2745b8ccad
최효은 <choehyoeun@choehyoeun-ui-MacBookAir.local>
2025-07-14 18:18:22 +0900

    test: 예외처리/시나리오 테스트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
-MERGE INTO product (name, price, image_url) KEY(name) VALUES
-('아이스 아메리카노', 4000,'https://example.com/images/americano.jpg'),
-('뜨거운 아메리카노', 4000, 'https://example.com/images/cafelatte.jpg'),
-('바닐라라떼', 5000, 'https://example.com/images/vanillalatte.jpg');
+INSERT INTO product (id, name, price, image_url) VALUES 
+(1, '테스트상품1', 10000, 'test1.jpg'),
+(2, '테스트상품2', 10000, 'test2.jpg'),
+(3, '테스트상품3', 10000, 'test3.jpg'),
+(4, '테스트상품4', 10000, 'test4.jpg'),
+(5, '테스트상품5', 10000, 'test5.jpg');
