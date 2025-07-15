cd4ef84317c5b0afad7872648f6612f157b94349
gary5876 <gjw0622@gmail.com>
2025-07-16 06:38:47 +0900

    step2

--- src/main/java/gift/wish/dto/WishRequest.java
+++
@@ -1,9 +0,0 @@
-package gift.wish.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record WishRequest(
-        @NotNull(message = "상품 ID는 비워둘 수 없습니다.")
-        Long productId
-) {
-}
